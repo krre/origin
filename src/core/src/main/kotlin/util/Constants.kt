fa87c8a516a0106789cc9cbd9e38e46e9eda2878
Vladimir Zarypov <krre31@gmail.com>
2016-05-02 12:13:32 +0300

    Add const to fields in Constants class

--- src/core/src/main/kotlin/util/Constants.kt
+++ src/core/src/main/kotlin/util/Constants.kt
@@ -1,8 +1,8 @@
 package org.gagarin.utils
 
 object Constants {
-    val virtualWidth = 800f
-    val virtualHeight = 480f
+    const val virtualWidth = 800f
+    const val virtualHeight = 480f
 }
 
 enum class File {

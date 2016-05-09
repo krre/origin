c81184ebd99aaa93e26d7079cd6967afb4b590ec
Vladimir Zarypov <krre31@gmail.com>
2016-05-09 09:14:36 +0300

    Cleanup comments

--- src/Game.cpp
+++ src/Game.cpp
@@ -4,7 +4,7 @@
 #include "SDL/include/SDL_version.h"
 #include "SDL/include/SDL.h"
 
-// Screen dimension constants
+// Default screen dimension
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 480;
 
@@ -17,39 +17,23 @@ int Game::run() {
     SDL_VERSION(&compiled);
     printf("SDL %d.%d.%d\n", compiled.major, compiled.minor, compiled.patch);
 
-    // The window we'll be rendering to
-    SDL_Window* window = nullptr;
-
-    // The surface contained by the window
-    SDL_Surface* screenSurface = nullptr;
-
-    // Initialize SDL
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
         printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        return EXIT_FAILURE;
     } else {
-        // Create window
-        window = SDL_CreateWindow("Gagarin", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        SDL_Window* window = SDL_CreateWindow("Gagarin", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
         if (window == nullptr) {
             printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+            return EXIT_FAILURE;
         } else {
-            // Get window surface
-            screenSurface = SDL_GetWindowSurface(window);
-
-            // Fill the surface white
+            SDL_Surface* screenSurface = SDL_GetWindowSurface(window);
             SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 40, 40, 40));
-
-            // Update the surface
             SDL_UpdateWindowSurface(window);
-
-            // Wait two seconds
             SDL_Delay(2000);
+            SDL_DestroyWindow(window);
+            SDL_Quit();
+
+            return EXIT_SUCCESS;
         }
     }
-
-    // Destroy window
-    SDL_DestroyWindow(window);
-
-    SDL_Quit();
-
-    return EXIT_SUCCESS;
 }

c2751845d5743c70edbfe58cc0bb3a8eceea9ca6
Vladimir Zarypov <krre31@gmail.com>
2016-05-14 10:26:44 +0300

    Replace SDL by SFML

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,18 @@
-﻿#include "Game.h"
-#include <stdlib.h>
-#undef main // fix error: undefined reference to `WinMain@16'
+﻿#include <stdlib.h>
+#include <SFML/Window.hpp>
 
-int main() {
-    Game game;
-    game.init();
-    game.run();
-    game.clean();
+int main(int argc, char* argv[]) {
+    sf::Window App(sf::VideoMode(800, 600), "Gagarin");
+
+    while (App.isOpen()) {
+        sf::Event Event;
+        while (App.pollEvent(Event)) {
+            if (Event.type == sf::Event::Closed) {
+                App.close();
+            }
+        }
+        App.display();
+    }
+
     return EXIT_SUCCESS;
 }

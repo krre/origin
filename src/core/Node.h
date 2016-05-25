5946aa781c29dacd760ad79e927b52a417a2389b
Vladimir Zarypov <krre31@gmail.com>
2016-05-25 12:42:12 +0300

    Add nodes list to Node class

--- src/core/Node.h
+++ src/core/Node.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <memory>
+#include <list>
 
 class Node {
 
@@ -7,4 +8,7 @@ public:
     Node();
     virtual void addChild(std::shared_ptr<Node> child) = 0;
     virtual void removeChild(int id) = 0;
+
+private:
+    std::list<std::shared_ptr<Node>> nodes;
 };

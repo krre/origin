863bd9a888fe619c0d57076d035b7f2fdfec9e42
Vladimir Zarypov <krre31@gmail.com>
2016-05-28 07:37:57 +0300

    Add PlayerView class

---
+++ Source/UI/PlayerView.cpp
@@ -0,0 +1,5 @@
+#include "PlayerView.h"
+
+PlayerView::PlayerView() {
+
+}
---
+++ Source/UI/PlayerView.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "../Core/View.h"
+
+class PlayerView : public View {
+
+public:
+    PlayerView();
+};

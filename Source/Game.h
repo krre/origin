87d76de241e1ab2b4f1688824c0085d7dafe4ae6
Vladimir Zarypov <krre31@gmail.com>
2016-05-28 06:48:45 +0300

    Rename core directory to Core

--- Source/Game.h
+++ Source/Game.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <SDL.h>
-#include "core/screens/MainScreen.h"
-#include "core/screens/GameScreen.h"
+#include "Core/screens/MainScreen.h"
+#include "Core/screens/GameScreen.h"
 
 class Game {
 

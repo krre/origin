96d56b1457eaa742ec6e358c3286e4f1ceccacde
Vladimir Zarypov <krre31@gmail.com>
2016-05-31 10:49:03 +0300

    Add camera and scene members to PlayerView class

--- Source/UI/PlayerView.h
+++ Source/UI/PlayerView.h
@@ -1,8 +1,15 @@
 #pragma once
 #include "View.h"
+#include "../Graphics3D/Camera.h"
+#include "../Graphics3D/Scene.h"
+#include <memory>
 
 class PlayerView : public View {
 
 public:
     PlayerView();
+
+private:
+    std::shared_ptr<Camera> camera;
+    std::shared_ptr<Scene> scene;
 };

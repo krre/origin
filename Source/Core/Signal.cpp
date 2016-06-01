a5ca836be7f466f3bcce1917e0bf6ddbc432201a
Vladimir Zarypov <krre31@gmail.com>
2016-06-01 15:22:35 +0300

    Add implementation of Signal class

--- Source/Core/Signal.cpp
+++
@@ -1,5 +0,0 @@
-#include "Signal.h"
-
-Signal::Signal() {
-
-}
--- Source/Core/Signal.h
+++ Source/Core/Signal.h
@@ -1,7 +1,40 @@
 #pragma once
+#include <functional>
+#include <map>
 
+// Based on http://simmesimme.github.io/tutorials/2015/09/20/signal-slot/
+template <typename... Args>
 class Signal {
 
 public:
-    Signal();
+    Signal() {}
+
+    int connect(std::function<void(Args...)> const& slot) const {
+        slots.insert(std::make_pair(++currentId, slot));
+        return currentId;
+    }
+
+    template <typename F, typename... A>
+    int connectMember(F&& f, A&& ... a) const {
+        slots.insert({ ++currentId, std::bind(f, a...) });
+        return currentId;
+    }
+
+    void disconnect(int id) const { slots.erase(id); }
+
+    void disconnectAll() const { slots.clear(); }
+
+    void emit(Args... p) {
+        for (auto it : slots) {
+            it.second(p...);
+        }
+    }
+
+    Signal& operator=(Signal const& other) {
+        disconnectAll();
+    }
+
+private:
+    mutable std::map<int, std::function<void(Args...)>> slots;
+    mutable int currentId = 0;
 };

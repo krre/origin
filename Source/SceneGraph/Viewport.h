9387a3299a4e5cca9337a4b4753d3a461eaf79c0
Vladimir Zarypov <krre31@gmail.com>
2016-06-02 20:36:06 +0300

    Add scene property to Viewport class

--- Source/SceneGraph/Viewport.h
+++ Source/SceneGraph/Viewport.h
@@ -1,8 +1,15 @@
 #pragma once
 #include "../Core/Object.h"
+#include "Scene.h"
 
 class Viewport : public Object {
 
 public:
     Viewport();
+
+    void setScene(Scene* const scene) { this->scene = scene; }
+    Scene* getScene() { return scene; }
+
+private:
+    Scene* scene;
 };

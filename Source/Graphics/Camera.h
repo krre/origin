667f457133ca67dbbea63f4dd0721d789104bf12
Vladimir Zarypov <krre31@gmail.com>
2016-06-08 06:26:53 +0300

    Add near, far, fov and aspect members to Camera class

--- Source/Graphics/Camera.h
+++ Source/Graphics/Camera.h
@@ -5,4 +5,14 @@ class Camera : public Node {
 
 public:
     Camera();
+    void setNear(float near) { this->near = near; }
+    void setFar(float far) { this->far = far; }
+    void setFov(float fov) { this->fov = fov; }
+    void setAspect(float aspect) { this->aspect = aspect; }
+
+private:
+    float near;
+    float far;
+    float fov;
+    float aspect;
 };

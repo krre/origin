574cedea470e276ce730987c4f62a066c9d967bc
Vladimir Zarypov <krre31@gmail.com>
2016-06-08 06:38:40 +0300

    Add projection member to Camera class

--- Source/Graphics/Camera.cpp
+++ Source/Graphics/Camera.cpp
@@ -3,3 +3,19 @@
 Camera::Camera() {
 
 }
+
+void Camera::setNear(float near) {
+    this->near = near;
+}
+
+void Camera::setFar(float far) {
+    this->far = far;
+}
+
+void Camera::setFov(float fov) {
+    this->fov = fov;
+}
+
+void Camera::setAspect(float aspect) {
+    this->aspect = aspect;
+}
--- Source/Graphics/Camera.h
+++ Source/Graphics/Camera.h
@@ -1,18 +1,21 @@
 #pragma once
 #include "../SceneGraph/Node.h"
+#include <glm/glm.hpp>
 
 class Camera : public Node {
 
 public:
     Camera();
-    void setNear(float near) { this->near = near; }
-    void setFar(float far) { this->far = far; }
-    void setFov(float fov) { this->fov = fov; }
-    void setAspect(float aspect) { this->aspect = aspect; }
+    void setNear(float near);
+    void setFar(float far);
+    void setFov(float fov);
+    void setAspect(float aspect);
+    const glm::mat4& getProjection() const { return projection; }
 
 private:
     float near;
     float far;
     float fov;
     float aspect;
+    glm::mat4 projection;
 };

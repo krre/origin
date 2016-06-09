c61d11bc0238547ddbcf2f31de02eee23ae433d6
Vladimir Zarypov <krre31@gmail.com>
2016-06-09 10:44:03 +0300

    Disable debug console output

--- Source/Event/Event.cpp
+++ Source/Event/Event.cpp
@@ -17,7 +17,7 @@ void Event::handleEvents() {
             switch (event.window.event) {
             case SDL_WINDOWEVENT_RESIZED:
                 windowResize.emit(event.window.data1, event.window.data2);
-                print("window resize: " << event.window.data1 << " " << event.window.data2);
+//                print("window resize: " << event.window.data1 << " " << event.window.data2);
                 break;
             default:
                 break;
@@ -26,19 +26,19 @@ void Event::handleEvents() {
 
         case SDL_MOUSEMOTION:
             mouseMove.emit(event.motion.x, event.motion.y);
-            print("mouse move: " << event.motion.x << " " << event.motion.y);
+//            print("mouse move: " << event.motion.x << " " << event.motion.y);
             break;
 
         case SDL_MOUSEBUTTONDOWN:
-            print("mouse button down: " << event.button.button << " " << event.button.x << " " << event.button.y);
+//            print("mouse button down: " << event.button.button << " " << event.button.x << " " << event.button.y);
             break;
 
         case SDL_MOUSEBUTTONUP:
-            print("mouse button up: " << event.button.button << " " << event.button.x << " " << event.button.y);
+//            print("mouse button up: " << event.button.button << " " << event.button.x << " " << event.button.y);
             break;
 
         case SDL_MOUSEWHEEL:
-            print("mouse wheel: " << event.wheel.x << " " << event.wheel.y);
+//            print("mouse wheel: " << event.wheel.x << " " << event.wheel.y);
 
         default:
             break;

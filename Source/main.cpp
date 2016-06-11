1a94f267ea1fc3d1d07347998596a4e004bc07e1
Vladimir Zarypov <krre31@gmail.com>
2016-06-11 09:05:11 +0300

    Add Input class and init it global object

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,17 +1,21 @@
 ﻿#include <stdlib.h>
 #include "Core/App.h"
+#include "Event/Input.h"
 #undef main // fix SDL error: undefined reference to `WinMain@16'
 
 App* app;
 Event* event;
+Input* input;
 
 int main(int argc, char* argv[]) {
     ::event = new Event();
+    ::input = new Input();
     ::app = new App(argc, argv);
     ::app->init();
     ::app->run();
     ::app->clean();
     delete ::event;
+    delete ::input;
     delete ::app;
     return EXIT_SUCCESS;
 }

69475cae0dcb531fd10ea56198296e3896001afc
Vladimir Zarypov <krre31@gmail.com>
2016-06-24 12:22:39 +0300

    Add entities list to System class

--- Source/ECS/System.cpp
+++ Source/ECS/System.cpp
@@ -3,3 +3,15 @@
 System::System() {
 
 }
+
+void System::addEntity(Entity* entity) {
+    entities.push_back(entity);
+}
+
+void System::removeEntity(Entity* entity) {
+    entities.remove(entity);
+}
+
+void System::clearEntities() {
+    entities.clear();
+}
--- Source/ECS/System.h
+++ Source/ECS/System.h
@@ -1,9 +1,17 @@
 #pragma once
 #include "../Core/Object.h"
+#include "Entity.h"
+#include <list>
 
 class System : public Object {
 
 public:
     System();
+    void addEntity(Entity* entity);
+    void removeEntity(Entity* entity);
+    void clearEntities();
     virtual void update(float dt) = 0;
+
+private:
+    list<Entity*> entities;
 };

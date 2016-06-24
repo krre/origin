457324306c7706afc1f67fbef88adbd944a72a17
Vladimir Zarypov <krre31@gmail.com>
2016-06-24 16:00:17 +0300

    Add parent and children members to NodeComponent

--- Source/ECS/Components/NodeComponent.h
+++ Source/ECS/Components/NodeComponent.h
@@ -1,8 +1,12 @@
 #pragma once
 #include "../Component.h"
+#include "../Entity.h"
+#include <list>
 
 class NodeComponent : public Component {
 
 public:
     NodeComponent();
+    EntityId parent;
+    list<EntityId> children;
 };

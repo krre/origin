e28417f84d5d84aaab15ebe31bf87978959c55d4
Vladimir Zarypov <krre31@gmail.com>
2016-06-24 16:02:52 +0300

    Add localMatrix and worldMatrix members to TransformComponent

--- Source/ECS/Components/TransformComponent.h
+++ Source/ECS/Components/TransformComponent.h
@@ -1,8 +1,11 @@
 #pragma once
 #include "../Component.h"
+#include <glm/glm.hpp>
 
 class TransformComponent : public Component {
 
 public:
     TransformComponent();
+    glm::mat4 localMatrix;
+    glm::mat4 worldMatrix;
 };

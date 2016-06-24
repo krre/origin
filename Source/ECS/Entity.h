b9d5b8e02b9ee737fd58bfc8e1e6196f0899411a
Vladimir Zarypov <krre31@gmail.com>
2016-06-24 14:37:19 +0300

    Add id member to Entity class

--- Source/ECS/Entity.h
+++ Source/ECS/Entity.h
@@ -7,11 +7,15 @@ class Entity : Object {
 
 public:
     Entity();
+
     void addComponent(const Component& component);
     void removeComponent(const Component::Type& type);
     const Component& getComponent(const Component::Type& type);
     void clear();
 
+    unsigned int getId() { return id; }
+
 private:
     map<Component::Type, Component> components;
+    unsigned int id;
 };

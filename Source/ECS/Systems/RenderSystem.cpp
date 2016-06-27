99bbd925906966d6ddeb5ac864b976f870e2cac7
Vladimir Zarypov <krre31@gmail.com>
2016-06-27 08:59:22 +0300

    Add RenderSystem class

---
+++ Source/ECS/Systems/RenderSystem.cpp
@@ -0,0 +1,5 @@
+#include "RenderSystem.h"
+
+RenderSystem::RenderSystem() {
+
+}
---
+++ Source/ECS/Systems/RenderSystem.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "../System.h"
+
+class RenderSystem : public System {
+
+public:
+    RenderSystem();
+};

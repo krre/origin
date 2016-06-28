629c4391b76007995eae73d7998ffafa7aac4525
Vladimir Zarypov <krre31@gmail.com>
2016-06-28 10:55:14 +0300

    Set Camera to CameraComponent in Player entity

--- Source/ECS/Entities/Player.cpp
+++ Source/ECS/Entities/Player.cpp
@@ -1,7 +1,11 @@
 #include "Player.h"
+#include "../ECS/Components/CameraComponent.h"
+#include "../Graphics/Camera.h"
 
 Player::Player() {
     addComponent(ComponentType::Node);
-    addComponent(ComponentType::Camera);
     addComponent(ComponentType::Transform);
+
+    CameraComponent* cameraComponent = static_cast<CameraComponent*>(addComponent(ComponentType::Camera));
+    cameraComponent->camera = new Camera();
 }

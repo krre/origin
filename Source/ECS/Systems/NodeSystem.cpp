bc8f43320fa4750ad8b568a2f271607dd617ee89
Vladimir Zarypov <krre31@gmail.com>
2016-06-30 15:09:04 +0300

    Add NodeSystem::process() blank

--- Source/ECS/Systems/NodeSystem.cpp
+++ Source/ECS/Systems/NodeSystem.cpp
@@ -1,9 +1,16 @@
 #include "NodeSystem.h"
+#include "../Components/NodeComponent.h"
+#include "../Engine.h"
 
 NodeSystem::NodeSystem() {
     type = SystemType::Node;
 }
 
 void NodeSystem::process(float dt) {
-
+    for (auto entity: Engine::getInstance()->getEntities()) {
+        NodeComponent* nc = static_cast<NodeComponent*>(entity->getComponent(ComponentType::Node));
+        if (nc) {
+//            print(nc);
+        }
+    }
 }

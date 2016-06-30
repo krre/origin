ee8bd2dd504e003de60b2a10242c8aae0052037e
Vladimir Zarypov <krre31@gmail.com>
2016-06-30 11:07:44 +0300

    Add position/rotation/scale members to TransformComponent.h

--- Source/ECS/Components/TransformComponent.h
+++ Source/ECS/Components/TransformComponent.h
@@ -1,13 +1,19 @@
 #pragma once
 #include "../Component.h"
-#include <glm/glm.hpp>
+#include <glm/ext.hpp>
+
+using namespace glm;
 
 class TransformComponent : public Component {
 
 public:
     TransformComponent();
-    glm::mat4 localMatrix;
-    glm::mat4 worldMatrix;
+    mat4 localMatrix;
+    mat4 worldMatrix;
+    vec3 position;
+    quat rotation;
+    float scale = 1.0;
     float yaw = 0;
     float pitch = 0;
+    bool dirty = false;
 };

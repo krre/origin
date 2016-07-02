d27ce8ddf9a727ed92fd90389aef4d2f3726c0b0
Vladimir Zarypov <krre31@gmail.com>
2016-07-02 20:29:49 +0300

    Add color member to Color class

--- Source/Graphics/Color.cpp
+++ Source/Graphics/Color.cpp
@@ -1,5 +1,9 @@
 #include "Color.h"
 
-Color::Color() {
+Color::Color() : color(vec4(1.0, 1.0, 1.0, 1.0)) {
+
+}
+
+Color::Color(int r, int g, int b, int a) : color(vec4(r, g, b, a)) {
 
 }
--- Source/Graphics/Color.h
+++ Source/Graphics/Color.h
@@ -1,8 +1,16 @@
 #pragma once
 #include "../Core/Object.h"
+#include <glm/glm.hpp>
+
+using namespace glm;
 
 class Color : public Object {
 
 public:
     Color();
+    Color(int r, int g, int b, int a = 255);
+    vec4 getRgba() const { return color; }
+
+private:
+    vec4 color;
 };

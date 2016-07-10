2c6c1c15fa3442afd3af1e99b4337e81bf1542e2
Vladimir Zarypov <krre31@gmail.com>
2016-07-10 09:21:49 +0300

    Change font for Console on inconsolatalgc.ttf

--- Source/UI/Text.cpp
+++ Source/UI/Text.cpp
@@ -18,7 +18,8 @@ Text::Text() {
 
     FT_Face face;
 
-    string fontPath = App::getAbsolutePath() + "/Data/Fonts/arial.ttf";
+//    string fontPath = App::getAbsolutePath() + "/Data/Fonts/arial.ttf";
+    string fontPath = App::getAbsolutePath() + "/Data/Fonts/inconsolatalgc.ttf";
     if (FT_New_Face(ft, fontPath.c_str(), 0, &face)) {
         fprintf(stderr, "Could not open font\n");
     }

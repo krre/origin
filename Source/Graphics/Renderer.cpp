00688c7023cc3331cbd3ac2ef04dc6a2fd8e4b19
Vladimir Zarypov <krre31@gmail.com>
2016-07-13 13:57:40 +0300

    Add Renderer::setEntities() method

--- Source/Graphics/Renderer.cpp
+++ Source/Graphics/Renderer.cpp
@@ -1,10 +1,18 @@
 #include "Renderer.h"
 #include <SDL_opengl.h>
+#include <assert.h>
 
 Renderer::Renderer() {
 
 }
 
 void Renderer::render(float dt) {
+    assert(entities);
+    for (auto entity : (*entities)) {
 
+    }
+}
+
+void Renderer::setEntities(std::vector<Entity*>* entities) {
+    this->entities = entities;
 }
--- Source/Graphics/Renderer.h
+++ Source/Graphics/Renderer.h
@@ -1,9 +1,15 @@
 #pragma once
 #include "../Core/Object.h"
+#include "../ECS/Entity.h"
+#include <vector>
 
 class Renderer : public Object {
 
 public:
     Renderer();
     void render(float dt);
+    void setEntities(std::vector<Entity*>* entities);
+
+private:
+    std::vector<Entity*>* entities = nullptr;
 };

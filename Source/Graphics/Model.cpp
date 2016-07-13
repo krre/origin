0cccdb3037b2618a62db8ab0e6e8b2a63f24634c
Vladimir Zarypov <krre31@gmail.com>
2016-07-13 15:43:44 +0300

    Add mesh and material members to Model class

--- Source/Graphics/Model.cpp
+++ Source/Graphics/Model.cpp
@@ -7,3 +7,11 @@ Model::Model() {
 void Model::draw(float dt) {
 
 }
+
+void Model::setMaterial(Material* material) {
+    this->material = material;
+}
+
+void Model::setMesh(Mesh* mesh) {
+    this->mesh = mesh;
+}
--- Source/Graphics/Model.h
+++ Source/Graphics/Model.h
@@ -1,9 +1,21 @@
 #pragma once
 #include "Drawable.h"
+#include "Mesh.h"
+#include "Material.h"
 
 class Model : public Drawable {
 
 public:
     Model();
     void draw(float dt) override;
+
+    Material* getMaterial() const { return material; }
+    void setMaterial(Material* material);
+
+    Mesh* getMesh() const { return mesh; }
+    void setMesh(Mesh* mesh);
+
+private:
+    Mesh* mesh;
+    Material* material;
 };

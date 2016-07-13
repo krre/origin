858f98e70b25b39b9aee05f3f2254718f027ffe2
Vladimir Zarypov <krre31@gmail.com>
2016-07-13 13:35:35 +0300

    Add verticles and indicies members to Mesh class

--- Source/Graphics/Mesh.h
+++ Source/Graphics/Mesh.h
@@ -2,6 +2,7 @@
 #include "../Core/Object.h"
 #include "OpenGL/VertexArray.h"
 #include "OpenGL/Buffer.h"
+#include <vector>
 
 class Mesh : public Object {
 
@@ -9,6 +10,12 @@ public:
     Mesh();
     VertexArray* getVao() { return &vao; }
     Buffer* getVbo() { return &vbo; }
+    const std::vector<float>& getVerticles() const { return verticles; }
+    const std::vector<int>& getIndices() const { return indices; }
+
+protected:
+    std::vector<float> verticles;
+    std::vector<int> indices;
 
 private:
     VertexArray vao;

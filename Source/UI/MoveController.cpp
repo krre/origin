9185b9853f28aa44d827a3224fbc42aecddeb82b
Vladimir Zarypov <krre31@gmail.com>
2016-07-16 06:31:07 +0300

    Add MoveController class

---
+++ Source/UI/MoveController.cpp
@@ -0,0 +1,5 @@
+#include "MoveController.h"
+
+MoveController::MoveController() {
+
+}
---
+++ Source/UI/MoveController.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "../Core/Object.h"
+
+class MoveController : public Object {
+
+public:
+    MoveController();
+};

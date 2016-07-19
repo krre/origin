ba8330f43da5e38ce986bba706031072aad9ff24
Vladimir Zarypov <krre31@gmail.com>
2016-07-19 16:14:06 +0300

    Add base virtual methods to GameState class

--- Source/State/GameState.h
+++ Source/State/GameState.h
@@ -5,4 +5,13 @@ class GameState : public Object {
 
 public:
     GameState();
+    virtual void init() = 0;
+    virtual void cleanup() = 0;
+
+    virtual void pause() = 0;
+    virtual void resume() = 0;
+
+    virtual void handleEvents() = 0;
+    virtual void update(float dt) = 0;
+    virtual void draw(float dt) = 0;
 };

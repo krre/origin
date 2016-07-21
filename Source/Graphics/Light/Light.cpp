c29b64f74a901390b923bf8e6f1197da1a78b019
Vladimir Zarypov <krre31@gmail.com>
2016-07-21 14:35:12 +0300

    Add getter and setter for Light::enable property

--- Source/Graphics/Light/Light.cpp
+++ Source/Graphics/Light/Light.cpp
@@ -3,3 +3,9 @@
 Light::Light() {
 
 }
+
+
+
+void Light::setEnable(bool enable) {
+    this->enable = enable;
+}
--- Source/Graphics/Light/Light.h
+++ Source/Graphics/Light/Light.h
@@ -6,6 +6,8 @@ class Light : public Object {
 public:
     Light();
 
+    bool getEnable() const { return enable; }
+    void setEnable(bool enable);
 
 protected:
     bool enable = true;

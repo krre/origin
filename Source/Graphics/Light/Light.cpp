ffad33c283401fd1cf479b8217b2b5d756526de9
Vladimir Zarypov <krre31@gmail.com>
2016-07-21 14:33:06 +0300

    Add base Light class

---
+++ Source/Graphics/Light/Light.cpp
@@ -0,0 +1,5 @@
+#include "Light.h"
+
+Light::Light() {
+
+}
---
+++ Source/Graphics/Light/Light.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "../../Core/Object.h"
+
+class Light : public Object {
+
+public:
+    Light();
+
+
+protected:
+    bool enable = true;
+
+};

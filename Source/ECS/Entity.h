7c000ea5f5162db439c2210c4cf961f31a910e9c
Vladimir Zarypov <krre31@gmail.com>
2016-07-22 06:07:15 +0300

    Add Entity::enable member

--- Source/ECS/Entity.h
+++ Source/ECS/Entity.h
@@ -11,6 +11,7 @@ public:
     Entity(EntityId id = 0);
     EntityId getId() { return id; }
     std::map<Component::Type, std::shared_ptr<Component>> components;
+    bool enable = true;
 
 private:
     EntityId id;

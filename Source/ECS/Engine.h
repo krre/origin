254f235067fe1d872a85e99d246b3f1496026d8a
Vladimir Zarypov <krre31@gmail.com>
2016-07-22 20:50:50 +0300

    Add comments to Engine header

--- Source/ECS/Engine.h
+++ Source/ECS/Engine.h
@@ -8,6 +8,7 @@ class Engine : public Singleton<Engine> {
 public:
     Engine();
 
+    // System
     template <typename T> void addSystem() {
         auto system = std::make_shared<T>();
         systems[system->getType()] = system;
@@ -17,19 +18,20 @@ public:
     std::shared_ptr<System> getSystem(SystemType type);
     void clearSystems();
 
+    // Entity
     void addEntity(std::shared_ptr<Entity> entity);
     void removeEntity(std::shared_ptr<Entity> entity);
     std::shared_ptr<Entity> getEntity(EntityId id);
     void clearEntities();
-
     std::vector<std::shared_ptr<Entity>> getEntities() { return entities; }
 
-    void process(float dt);
-
+    // Component
     Component* createComponent(Entity* entity, ComponentType type);
     void createComponents(Entity* entity, std::vector<ComponentType> types);
     void addComponent(Entity* entity, std::shared_ptr<Component> component);
     void removeComponent(Entity* entity, ComponentType type);
+
+    void process(float dt);
 
 private:
     std::map<SystemType, std::shared_ptr<System>> systems;

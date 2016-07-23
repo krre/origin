07d1a79b73f71b2b16d0fa838558ddd1d5b30eed
Vladimir Zarypov <krre31@gmail.com>
2016-07-23 11:19:07 +0300

    Add color property to LightComponent

--- Source/ECS/Components/LightComponent.h
+++ Source/ECS/Components/LightComponent.h
@@ -1,9 +1,10 @@
 #pragma once
 #include "../Component.h"
+#include <glm/glm.hpp>
 
 class LightComponent : public Component {
 
 public:
     LightComponent() { type = ComponentType::Light; }
-
+    glm::vec3 color = glm::vec3(1.0, 1.0, 1.0);
 };

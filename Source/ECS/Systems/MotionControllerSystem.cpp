f09007c077ea20a352881bd5988a17453aa2dd04
Vladimir Zarypov <krre31@gmail.com>
2016-07-23 18:31:28 +0300

    Fix camera rotation direction

--- Source/ECS/Systems/MotionControllerSystem.cpp
+++ Source/ECS/Systems/MotionControllerSystem.cpp
@@ -12,7 +12,7 @@ void MotionControllerSystem::process(float dt) {
     TransformComponent* tc = static_cast<TransformComponent*>(rotateEntity->components[ComponentType::Transform].get());
 
     glm::ivec2 relMousePos = Input::getInstance()->getRelMousePos();
-    tc->yaw += rotateSpeed * relMousePos.x;
+    tc->yaw -= rotateSpeed * relMousePos.x;
     tc->yaw = fmod(tc->yaw, 360.0f);
 
     tc->pitch -= rotateSpeed * relMousePos.y;
@@ -33,7 +33,7 @@ void MotionControllerSystem::process(float dt) {
     }
 
     TransformComponent* mtc = static_cast<TransformComponent*>(moveEntity->components[ComponentType::Transform].get());
-    // Track to floor position
+    // Track to floor pos
     mtc->position.y = 0.1; // TODO: take from height map
 }
 

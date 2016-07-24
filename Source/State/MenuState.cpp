c7984245bc4ec873cc1f72dc0a85fb645853275b
Vladimir Zarypov <krre31@gmail.com>
2016-07-24 09:22:10 +0300

    Add MenuState class

---
+++ Source/State/MenuState.cpp
@@ -0,0 +1,33 @@
+#include "MenuState.h"
+
+MenuState::MenuState() {
+
+}
+
+void MenuState::init() {
+
+}
+
+void MenuState::cleanup() {
+
+}
+
+void MenuState::pause() {
+
+}
+
+void MenuState::resume() {
+
+}
+
+void MenuState::handleEvents() {
+
+}
+
+void MenuState::update(float dt) {
+
+}
+
+void MenuState::draw(float dt) {
+
+}
---
+++ Source/State/MenuState.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "GameState.h"
+
+class MenuState : public GameState {
+
+public:
+    MenuState();
+    void init()  override;
+    virtual void cleanup() override;
+
+    virtual void pause() override;
+    virtual void resume() override;
+
+    virtual void handleEvents() override;
+    virtual void update(float dt) override;
+    virtual void draw(float dt) override;
+};

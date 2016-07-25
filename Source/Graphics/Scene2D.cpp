290c26b07e19e534a3026b61896f7209f1bfc1fb
Vladimir Zarypov <krre31@gmail.com>
2016-07-25 14:31:51 +0300

    Add Scene2D::onWindowResize() slot

--- Source/Graphics/Scene2D.cpp
+++ Source/Graphics/Scene2D.cpp
@@ -1,5 +1,10 @@
 #include "Scene2D.h"
+#include "../Event/Event.h"
 #include <algorithm>
+
+Scene2D::Scene2D() {
+    Event::getInstance()->windowResize.connect<Scene2D, &Scene2D::onWindowResize>(this);
+}
 
 void Scene2D::draw(float dt) {
     for (auto control : controls) {
@@ -17,4 +22,8 @@ void Scene2D::removeControl(std::shared_ptr<Control> control) {
 
 void Scene2D::clearControls() {
     controls.clear();
+}
+
+void Scene2D::onWindowResize(int width, int height) {
+    print(width << " " << height);
 }
--- Source/Graphics/Scene2D.h
+++ Source/Graphics/Scene2D.h
@@ -6,12 +6,14 @@
 class Scene2D : public Drawable {
 
 public:
-    Scene2D() {}
+    Scene2D();
     void draw(float dt) override;
     void addControl(std::shared_ptr<Control> control);
     void removeControl(std::shared_ptr<Control> control);
     void clearControls();
 
 private:
+    void onWindowResize(int width, int height);
+
     std::vector<std::shared_ptr<Control>> controls;
 };

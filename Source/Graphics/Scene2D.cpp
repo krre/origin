ad0790a65a8f2355ece7101f42afd3c2701636e1
Vladimir Zarypov <krre31@gmail.com>
2016-07-25 14:24:31 +0300

    Add methods for operations with controls in Scene2D

--- Source/Graphics/Scene2D.cpp
+++ Source/Graphics/Scene2D.cpp
@@ -1,7 +1,20 @@
 #include "Scene2D.h"
+#include <algorithm>
 
 void Scene2D::draw(float dt) {
     for (auto control : controls) {
         control->draw(dt);
     }
+}
+
+void Scene2D::addControl(std::shared_ptr<Control> control) {
+    controls.push_back(control);
+}
+
+void Scene2D::removeControl(std::shared_ptr<Control> control) {
+    controls.erase(std::remove(controls.begin(), controls.end(), control), controls.end());
+}
+
+void Scene2D::clearControls() {
+    controls.clear();
 }
--- Source/Graphics/Scene2D.h
+++ Source/Graphics/Scene2D.h
@@ -8,6 +8,9 @@ class Scene2D : public Drawable {
 public:
     Scene2D() {}
     void draw(float dt) override;
+    void addControl(std::shared_ptr<Control> control);
+    void removeControl(std::shared_ptr<Control> control);
+    void clearControls();
 
 private:
     std::vector<std::shared_ptr<Control>> controls;

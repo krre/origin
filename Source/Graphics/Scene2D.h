c31651cbbd401a9b712f64d964a9789990e29cfa
Vladimir Zarypov <krre31@gmail.com>
2016-07-25 14:42:38 +0300

    Add operations with layouts in Scene2D

--- Source/Graphics/Scene2D.h
+++ Source/Graphics/Scene2D.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Drawable.h"
 #include "../UI/Control.h"
+#include "../UI/Layout.h"
 #include <vector>
 
 class Scene2D : public Drawable {
@@ -8,12 +9,18 @@ class Scene2D : public Drawable {
 public:
     Scene2D();
     void draw(float dt) override;
+
     void addControl(std::shared_ptr<Control> control);
     void removeControl(std::shared_ptr<Control> control);
     void clearControls();
+
+    void addLayout(std::shared_ptr<Layout> layout);
+    void removeLayout(std::shared_ptr<Layout> layout);
+    void clearLayouts();
 
 private:
     void onWindowResize(int width, int height);
 
     std::vector<std::shared_ptr<Control>> controls;
+    std::vector<std::shared_ptr<Layout>> layouts;
 };

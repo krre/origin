5c85f9603979e8a28fec598d4f41141e3ce34ae9
Vladimir Zarypov <krre31@gmail.com>
2016-07-25 08:27:27 +0300

    Add color property to Rectangle class

--- Source/UI/Rectangle.cpp
+++ Source/UI/Rectangle.cpp
@@ -7,3 +7,7 @@ Rectangle::Rectangle() {
 void Rectangle::draw(float dt) {
 
 }
+
+void Rectangle::setColor(const glm::vec3& color) {
+    this->color = color;
+}
--- Source/UI/Rectangle.h
+++ Source/UI/Rectangle.h
@@ -1,9 +1,16 @@
 #pragma once
 #include "Control.h"
+#include <glm/glm.hpp>
 
 class Rectangle : Control {
 
 public:
     Rectangle();
     void draw(float dt) override;
+
+    glm::vec3 getColor() const { return color; }
+    void setColor(const glm::vec3& color);
+
+protected:
+    glm::vec3 color;
 };

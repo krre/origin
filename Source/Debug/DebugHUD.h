aff23823c2f07484e456473681ff25d17cad394f
Vladimir Zarypov <krre31@gmail.com>
2016-07-25 14:32:10 +0300

    DebugHUD inherits Scene2D

--- Source/Debug/DebugHUD.h
+++ Source/Debug/DebugHUD.h
@@ -1,9 +1,9 @@
 #pragma once
 #include "../Core/Singleton.h"
-#include "../Graphics/Drawable.h"
+#include "../Graphics/Scene2D.h"
 #include "../UI/Text.h"
 
-class DebugHUD : public Singleton<DebugHUD>, public Drawable {
+class DebugHUD : public Singleton<DebugHUD>, public Scene2D {
 
 public:
     DebugHUD();

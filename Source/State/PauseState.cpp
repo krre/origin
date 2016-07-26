d370845e5270f149fc5026d443923110877dc73f
Vladimir Zarypov <krre31@gmail.com>
2016-07-26 10:19:33 +0300

    Add PauseState class

---
+++ Source/State/PauseState.cpp
@@ -0,0 +1,33 @@
+#include "PauseState.h"
+
+PauseState::PauseState() {
+
+}
+
+void PauseState::init() {
+
+}
+
+void PauseState::cleanup() {
+
+}
+
+void PauseState::pause() {
+
+}
+
+void PauseState::resume() {
+
+}
+
+void PauseState::handleEvents() {
+
+}
+
+void PauseState::update(float dt) {
+
+}
+
+void PauseState::draw(float dt) {
+
+}

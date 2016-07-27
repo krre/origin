fa9a46e5651af27523d2a30a0983a8fcbee4d288
Vladimir Zarypov <krre31@gmail.com>
2016-07-27 14:13:15 +0300

    Add Source/UI/Scenes directory with PauseScene class

---
+++ Source/UI/Scenes/PauseScene.cpp
@@ -0,0 +1,5 @@
+#include "PauseScene.h"
+
+PauseScene::PauseScene() {
+
+}
---
+++ Source/UI/Scenes/PauseScene.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "../Control.h"
+
+class PauseScene : public Control {
+
+public:
+    PauseScene();
+};

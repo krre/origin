1e0e866057bb54cdf7c0df1aa04b263f08fc5304
Vladimir Zarypov <krre31@gmail.com>
2016-07-27 10:49:58 +0300

    Add stack operations with game states

--- Source/GameState/GameStateManager.cpp
+++ Source/GameState/GameStateManager.cpp
@@ -3,3 +3,16 @@
 GameStateManager::GameStateManager() {
 
 }
+
+void GameStateManager::push(std::shared_ptr<GameState> gameState) {
+    gameStates.push_back(gameState);
+}
+
+void GameStateManager::pop() {
+    gameStates.pop_back();
+}
+
+void GameStateManager::set(std::shared_ptr<GameState> gameState) {
+    gameStates.clear();
+    gameStates.push_back(gameState);
+}
--- Source/GameState/GameStateManager.h
+++ Source/GameState/GameStateManager.h
@@ -1,8 +1,16 @@
 #pragma once
 #include "../Core/Singleton.h"
+#include "GameState.h"
+#include <vector>
 
 class GameStateManager : public Singleton<GameStateManager> {
 
 public:
     GameStateManager();
+    void push(std::shared_ptr<GameState> gameState);
+    void pop();
+    void set(std::shared_ptr<GameState> gameState);
+
+private:
+    std::vector<std::shared_ptr<GameState>> gameStates;
 };

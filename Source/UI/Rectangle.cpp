6b86c80be6760ae037cec35c246eefb360155052
Vladimir Zarypov <krre31@gmail.com>
2016-07-28 06:09:58 +0300

    Add debug print to Rectangle

--- Source/UI/Rectangle.cpp
+++ Source/UI/Rectangle.cpp
@@ -10,13 +10,22 @@ Rectangle::Rectangle(int width, int height) :
 }
 
 void Rectangle::draw(float dt) {
-    glm::mat4 translationMatrix = glm::translate(glm::vec3(-100.5f, -50.0f, 0.0f));
-    glm::mat4 scaleMatrix = glm::scale(glm::mat4(1.0f), glm::vec3(500.0));
-    glm::quat rotation = toQuat(glm::rotate(glm::mat4(1.0f), 90.0f, glm::vec3(1.0, 0, 0)));
-    glm::mat4 rotationMatrix = glm::toMat4(rotation);
+//    glm::mat4 translationMatrix = glm::translate(glm::vec3(-100.5f, -50.0f, 0.0f));
+    glm::mat4 translationMatrix = glm::mat4(1.0f);
+//    glm::mat4 scaleMatrix = glm::scale(glm::mat4(1.0f), glm::vec3(500.0));
+    glm::mat4 scaleMatrix = glm::mat4(1.0f);
+    glm::mat4 rotationMatrix = glm::rotate(glm::mat4(1.0f), 90.0f, glm::vec3(1.0, 0.0, 0.0));
+//    print(glm::to_string(rotationMatrix));
+    print(glm::to_string(glm::rotate(glm::mat4(1.0f), 90.0f, glm::vec3(1.0, 0.0, 0.0))));
+//    glm::mat4 rotationMatrix = glm::mat4(1.0f);
     glm::mat4 model = rotationMatrix * translationMatrix * scaleMatrix;
-    glm::mat4 projection = glm::ortho(0.0f, static_cast<GLfloat>(App::getInstance()->getWidth()), 0.0f, static_cast<GLfloat>(App::getInstance()->getHeight()));
+//    glm::mat4 projection = glm::ortho(0.0f, static_cast<GLfloat>(App::getInstance()->getWidth()), 0.0f, static_cast<GLfloat>(App::getInstance()->getHeight()));
+    glm::mat4 projection = glm::mat4(1.0f);
     glm::mat4 mvp = projection * model;
+    print("triangle");
+    print(glm::to_string(mvp * glm::vec4(-1.0f, 0.0f, -1.0f, 1.0f)));
+    print(glm::to_string(mvp * glm::vec4(-1.0f, 0.0f, 1.0f, 1.0f)));
+    print(glm::to_string(mvp * glm::vec4(1.0f, 0.0f, -1.0f, 1.0f)));
     baseShaderGroup->use();
     glUniformMatrix4fv(glGetUniformLocation(baseShaderGroup->getProgram(), "mvp"), 1, GL_FALSE, glm::value_ptr(mvp));
     glUniform3f(glGetUniformLocation(baseShaderGroup->getProgram(), "color"), color.x, color.y, color.z);

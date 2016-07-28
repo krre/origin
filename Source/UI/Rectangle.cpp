03af8934800e365954bbcce1f582037a24b61585
Vladimir Zarypov <krre31@gmail.com>
2016-07-28 09:33:09 +0300

    Fix Rectangle size during drawing

--- Source/UI/Rectangle.cpp
+++ Source/UI/Rectangle.cpp
@@ -10,17 +10,12 @@ Rectangle::Rectangle(int width, int height) :
 }
 
 void Rectangle::draw(float dt) {
-    glm::mat4 translationMatrix = glm::translate(glm::vec3(0.0f, 0.0f, 0.0f));
-    glm::mat4 scaleMatrix = glm::scale(glm::mat4(1.0f), glm::vec3(1.0));
+    glm::mat4 translationMatrix = glm::translate(glm::vec3(width / 2.0f, 0.0f, height / 2.0f - App::getInstance()->getHeight()));
+    glm::mat4 scaleMatrix = glm::scale(glm::mat4(1.0f), glm::vec3(width, 1.0, height));
     glm::mat4 rotationMatrix = glm::rotate(glm::mat4(1.0f), glm::radians(90.0f), glm::vec3(1.0, 0.0, 0.0));
     glm::mat4 model = rotationMatrix * translationMatrix * scaleMatrix;
-//    glm::mat4 projection = glm::ortho(0.0f, static_cast<GLfloat>(App::getInstance()->getWidth()), 0.0f, static_cast<GLfloat>(App::getInstance()->getHeight()));
-    glm::mat4 projection = glm::mat4(1.0f);
+    glm::mat4 projection = glm::ortho(0.0f, static_cast<GLfloat>(App::getInstance()->getWidth()), 0.0f, static_cast<GLfloat>(App::getInstance()->getHeight()));
     glm::mat4 mvp = projection * model;
-    print("triangle");
-    print(glm::to_string(mvp * glm::vec4(-1.0f, 0.0f, -1.0f, 1.0f)));
-    print(glm::to_string(mvp * glm::vec4(-1.0f, 0.0f, 1.0f, 1.0f)));
-    print(glm::to_string(mvp * glm::vec4(1.0f, 0.0f, -1.0f, 1.0f)));
     baseShaderGroup->use();
     glUniformMatrix4fv(glGetUniformLocation(baseShaderGroup->getProgram(), "mvp"), 1, GL_FALSE, glm::value_ptr(mvp));
     glUniform3f(glGetUniformLocation(baseShaderGroup->getProgram(), "color"), color.x, color.y, color.z);

d4868b0f39155166d871bbb4ca6d644ecf2efb98
Vladimir Zarypov <krre31@gmail.com>
2016-08-01 06:04:32 +0300

    Add HORIZONTAL layout support

--- Source/UI/LinearLayout.cpp
+++ Source/UI/LinearLayout.cpp
@@ -11,6 +11,9 @@ void LinearLayout::updateContentPostion() {
         if (direction == VERICAL) {
             control->setPosition(glm::vec2(position.x, position.y + i * (control->getHeight() + spacing)));
             i++;
+        } else {
+            control->setPosition(glm::vec2(position.x + i * (control->getWidth() + spacing), position.y));
+            i++;
         }
     }
 }

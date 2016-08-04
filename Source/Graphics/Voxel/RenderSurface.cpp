fcf222119466544c8da7b6c67d2c95154a7223c2
Vladimir Zarypov <krre31@gmail.com>
2016-08-04 16:00:08 +0300

    Add Recangle member to RenderSurface class

--- Source/Graphics/Voxel/RenderSurface.cpp
+++ Source/Graphics/Voxel/RenderSurface.cpp
@@ -1,9 +1,9 @@
 #include "RenderSurface.h"
 
-RenderSurface::RenderSurface() {
-
+RenderSurface::RenderSurface() : rectangle(800, 480) {
+    rectangle.setColor(glm::vec3(0.9f, 0.7f, 0.5f));
 }
 
 void RenderSurface::draw(float dt) {
-
+    rectangle.draw(dt);
 }
--- Source/Graphics/Voxel/RenderSurface.h
+++ Source/Graphics/Voxel/RenderSurface.h
@@ -1,9 +1,13 @@
 #pragma once
 #include "../Drawable.h"
+#include "../UI/Rectangle.h"
 
 class RenderSurface : public Drawable {
 
 public:
     RenderSurface();
     void draw(float dt) override;
+
+private:
+    Rectangle rectangle;
 };

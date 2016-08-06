4490f1d1e290da06afb31562e50fab4bdea8d6cc
Vladimir Zarypov <krre31@gmail.com>
2016-08-06 15:30:19 +0300

    Init depth buffer in RenderSurface

--- Source/Graphics/Voxel/RenderSurface.cpp
+++ Source/Graphics/Voxel/RenderSurface.cpp
@@ -4,11 +4,13 @@
 RenderSurface::RenderSurface() : rectangle(800, 480) {
 //    rectangle.setColor(glm::vec3(0.9f, 0.7f, 0.5f));
     data = new uint32_t[rectangle.getWidth() * rectangle.getHeight()];
+    depth = new uint32_t[rectangle.getWidth() * rectangle.getHeight()];
     Event::getInstance()->windowResize.connect<RenderSurface, &RenderSurface::onWindowResize>(this);
 }
 
 RenderSurface::~RenderSurface() {
     delete data;
+    delete depth;
 }
 
 void RenderSurface::draw(float dt) {
@@ -18,5 +20,7 @@ void RenderSurface::draw(float dt) {
 void RenderSurface::onWindowResize(int width, int height) {
     rectangle.resize(width, height);
     delete data;
+    delete depth;
     data = new uint32_t[rectangle.getWidth() * rectangle.getHeight()];
+    depth = new uint32_t[rectangle.getWidth() * rectangle.getHeight()];
 }

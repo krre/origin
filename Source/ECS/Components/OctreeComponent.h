f648d2384904959bf8972b4389ecb0cc98602490
Vladimir Zarypov <krre31@gmail.com>
2016-08-09 08:31:28 +0300

    Place Octree's shared pointer to OctreeComponent

--- Source/ECS/Components/OctreeComponent.h
+++ Source/ECS/Components/OctreeComponent.h
@@ -1,9 +1,10 @@
 #pragma once
 #include "../Component.h"
-#include "../Graphics/Mesh.h"
+#include "../Graphics/Voxel/Octree.h"
 
 class OctreeComponent : public Component {
 
 public:
     OctreeComponent() { type = ComponentType::Octree; }
+    std::shared_ptr<Octree> octree;
 };

6897aecfb56cb3efe0364b91729774a737451086
Vladimir Zarypov <krre31@gmail.com>
2016-08-16 08:30:32 +0300

    Add aliases for fixed width integer types to Common.h

--- Source/Core/Common.h
+++ Source/Core/Common.h
@@ -1,4 +1,15 @@
 #pragma once
+#include <cstdint>
+
+using uint8 = std::uint8_t;
+using uint16 = std::uint16_t;
+using uint32 = std::uint32_t;
+using uint64 = std::uint64_t;
+
+using int8 = std::int8_t;
+using int16 = std::int16_t;
+using int32 = std::int32_t;
+using int64 = std::int64_t;
 
 #define print(x) std::cout << x << std::endl;
 #define error(x) std::cerr << x << std::endl;

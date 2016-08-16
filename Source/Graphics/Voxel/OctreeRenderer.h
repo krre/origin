54338a53245e7460f93ee3e0e78f2af42aeac07e
Vladimir Zarypov <krre31@gmail.com>
2016-08-16 19:31:29 +0300

    Add Ray struct to OctreeRenderer class

--- Source/Graphics/Voxel/OctreeRenderer.h
+++ Source/Graphics/Voxel/OctreeRenderer.h
@@ -1,11 +1,20 @@
 #pragma once
 #include "../../Core/Object.h"
+#include <glm/glm.hpp>
 
 class RenderSurface;
 
 class OctreeRenderer : public Object {
 
 public:
+    struct Ray {
+        glm::vec3 origin;
+        glm::vec3 direction;
+    };
+
     OctreeRenderer();
     void render(const RenderSurface* renderSurface);
+
+private:
+    Ray ray;
 };

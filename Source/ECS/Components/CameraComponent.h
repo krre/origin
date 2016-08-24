7db8ee5ad8a88074f5aac38c38742591be59cc04
Vladimir Zarypov <krre31@gmail.com>
2016-08-24 15:57:46 +0300

    Add up, view and right vectors to CameraComponent

--- Source/ECS/Components/CameraComponent.h
+++ Source/ECS/Components/CameraComponent.h
@@ -11,10 +11,14 @@ public:
         int width = App::getInstance()->getWidth();
         int height = App::getInstance()->getHeight();
         aspect = width * 1.0f / height;
+        right = glm::vec3(view.y, -view.x, 0.0);
     }
     float near = 0.01f;
     float far = 100.0f;
     float fov = glm::radians(50.0f);
     float aspect;
+    glm::vec3 up = glm::vec3(0.0, 1.0, 0.0);
+    glm::vec3 view = glm::vec3(0.0, 0.0, -1.0);
+    glm::vec3 right;
     glm::mat4 projection = glm::perspective(fov, aspect, near, far);
 };

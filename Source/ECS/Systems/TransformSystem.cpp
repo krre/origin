a8e0f326baa5298d3a3eda3e836228c40f07051b
Vladimir Zarypov <krre31@gmail.com>
2016-08-24 12:44:30 +0300

    Fix matrix multiplication order in TransformSystem::process() method

--- Source/ECS/Systems/TransformSystem.cpp
+++ Source/ECS/Systems/TransformSystem.cpp
@@ -13,7 +13,7 @@ void TransformSystem::process(float dt) {
             glm::mat4 translationMatrix = glm::translate(tc->position);
             glm::mat4 rotationMatrix = glm::toMat4(tc->rotation);
             glm::mat4 scaleMatrix = glm::scale(glm::mat4(1.0f), glm::vec3(tc->scale));
-            tc->localMatrix = rotationMatrix * translationMatrix * scaleMatrix;
+            tc->localMatrix = translationMatrix * rotationMatrix * scaleMatrix;
             tc->worldMatrix = tc->localMatrix;
             tc->dirty = false;
         }

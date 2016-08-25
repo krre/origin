b661607a47583d49e826783f1091e4dc3446cfc2
Vladimir Zarypov <krre31@gmail.com>
2016-08-25 11:31:24 +0300

    Ray casting works

--- Source/ECS/Components/CameraComponent.h
+++ Source/ECS/Components/CameraComponent.h
@@ -16,7 +16,7 @@ public:
     float far = 100.0f;
     float fov = glm::radians(50.0f);
     float aspect;
-    const float distance = 1.0f; // distance from camera to screen
+    const float distance = 0.5f; // distance from camera to screen
     glm::vec3 up = glm::vec3(0.0, 1.0, 0.0);
     glm::vec3 look = glm::vec3(0.0, 0.0, -1.0);
     glm::vec3 right = glm::vec3(1.0, 0.0, 0.0);
--- Source/Graphics/Voxel/OctreeRenderer.cpp
+++ Source/Graphics/Voxel/OctreeRenderer.cpp
@@ -38,32 +38,49 @@ void OctreeRenderer::render(const RenderSurface* renderSurface) {
     glm::vec3 skew;
     glm::vec4 perspective;
     glm::decompose(cameraTransform->worldMatrix, scale, rotation, translation, skew, perspective);
-//    print(glm::to_string(translation));
+
+    glm::vec3 up = cameraComp->up * rotation;
+    glm::vec3 look = cameraComp->look * rotation;
+    glm::vec3 right = cameraComp->right * rotation;
 
     Ray ray;
     ray.origin = translation;
+
+    glm::mat4 invMatrix = glm::translate(glm::mat4(1.0), -ray.origin) * glm::inverse(glm::mat4(cameraTransform->rotation));
+    invMatrix = glm::translate(invMatrix, ray.origin);
+    ray.origin = glm::vec3(invMatrix * glm::vec4(ray.origin.x, ray.origin.y, ray.origin.z, 1.0));
+
+    // Ray calculation base on Johns Hopkins presentation:
+    // http://www.cs.jhu.edu/~cohen/RendTech99/Lectures/Ray_Casting.bw.pdf
+    glm::vec3 h0 = look - up * glm::tan(cameraComp->fov); // min height vector
+    glm::vec3 h1 = look + up * glm::tan(cameraComp->fov); // max height vector
+    glm::vec3 stepH = (h1 - h0) / height;
+
+    glm::vec3 w0 = look - right * glm::tan(cameraComp->fov); // min width vector
+    glm::vec3 w1 = look + right * glm::tan(cameraComp->fov); // max width vector
+    glm::vec3 stepW = (w1 - w0) / width;
 
     AABB aabb;
     aabb.min = glm::vec3(transformComp->worldMatrix * glm::vec4(-1.0, -1.0, -1.0, 1.0));
     aabb.max = glm::vec3(transformComp->worldMatrix * glm::vec4( 1.0,  1.0,  1.0, 1.0));
 
+    glm::vec3 directionW;
+    glm::vec3 directionH = h0 - stepH / 2; // start height vector (from bottom to up)
+
     for (int y = 0; y < height; y++) {
-//        float yNorm = 1.0f - (2.0f * y) / height;
+        directionH = directionH + stepH;
+        directionW = w0 - stepW / 2; // start width vector (from left to right)
         for (int x = 0; x < width; x++) {
-//            float xNorm = (2.0f * x) / width - 1.0f;
-//            ray.origin.x = xNorm;
-//            ray.origin.y = yNorm;
+            directionW = directionW + stepW;
 
-//            glm::mat4 invMatrix = glm::translate(glm::mat4(1.0), -ray.origin) * invRotationMatrix;
-//            invMatrix = glm::translate(invMatrix, ray.origin);
+            ray.direction = glm::normalize(directionW + directionH);
+            ray.direction = glm::vec3(invMatrix * glm::vec4(ray.direction.x, ray.direction.y, ray.direction.z, 0.0));
 
-//            ray.origin = glm::vec3(invMatrix * glm::vec4(ray.origin.x, ray.origin.y, ray.origin.z, 1.0));
-
-//            if (rayAABBIntersect(&ray, &aabb)) {
-//                data[(height - y - 1) * width + x] = 0x00abffff;
-//            } else {
-//                data[(height - y - 1) * width + x] = 0xc4d3d3ffu; // background color;
-//            }
+            if (rayAABBIntersect(&ray, &aabb)) {
+                data[y * width + x] = 0x00abffff;
+            } else {
+                data[y * width + x] = 0xc4d3d3ffu; // background color;
+            }
         }
     }
 

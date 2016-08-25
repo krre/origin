31e3daee7a8a8826dc3199cdbcc042e76c3d5c1c
Vladimir Zarypov <krre31@gmail.com>
2016-08-25 08:11:12 +0300

    Rename CameraComponent::view property to look

--- Source/ECS/Components/CameraComponent.h
+++ Source/ECS/Components/CameraComponent.h
@@ -11,7 +11,6 @@ public:
         int width = App::getInstance()->getWidth();
         int height = App::getInstance()->getHeight();
         aspect = width * 1.0f / height;
-        right = glm::vec3(view.y, -view.x, 0.0);
     }
     float near = 0.01f;
     float far = 100.0f;
@@ -19,7 +18,7 @@ public:
     float aspect;
     const float distance = 1.0f; // distance from camera to screen
     glm::vec3 up = glm::vec3(0.0, 1.0, 0.0);
-    glm::vec3 view = glm::vec3(0.0, 0.0, -1.0);
-    glm::vec3 right;
+    glm::vec3 look = glm::vec3(0.0, 0.0, -1.0);
+    glm::vec3 right = glm::vec3(1.0, 0.0, 0.0);
     glm::mat4 projection = glm::perspective(fov, aspect, near, far);
 };

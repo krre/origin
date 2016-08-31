93e22459e83180901c748f7d3a23542eed1f397c
Vladimir Zarypov <krre31@gmail.com>
2016-08-31 18:39:15 +0300

    Remove OctreeRenderer class

--- Source/Graphics/Voxel/RenderSurface.h
+++ Source/Graphics/Voxel/RenderSurface.h
@@ -4,31 +4,25 @@
 #include "../Graphics/OpenGL/Texture.h"
 #include "../Graphics/OpenGL/VertexArray.h"
 #include "../Graphics/OpenGL/Buffer.h"
-#include "../Graphics/Voxel/OctreeRenderer.h"
-
-//class OctreeRenderer;
+#include <glm/glm.hpp>
 
 class RenderSurface : public Drawable {
 
 public:
+    struct Ray {
+        glm::vec3 origin;
+        glm::vec3 direction;
+    };
+
+    struct AABB {
+        glm::vec3 min;
+        glm::vec3 max;
+    };
+
     RenderSurface();
-    ~RenderSurface();
     void draw(float dt) override;
-    void clear();
-    int getWidth() const { return width; }
-    int getHeight() const { return height; }
-    uint32* getData() const { return data; }
 
 private:
-    void onWindowResize(int width, int height);
-
-    OctreeRenderer octreeRenderer;
-    uint32* data = nullptr;
-    uint32* depth = nullptr;
-    int width = 0;
-    int height = 0;
-
-    ShaderGroup* surfaceShaderGroup;
     ShaderGroup* voxelShaderGroup;
     Texture texture;
     VertexArray VAO;

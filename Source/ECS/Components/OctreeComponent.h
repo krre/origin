b6621cb79a1e07ce0281c893b315061570ea771e
Vladimir Zarypov <krre31@gmail.com>
2016-09-08 15:31:49 +0300

    Convert OctreeComponent::data type to std::vector

--- Source/ECS/Components/OctreeComponent.h
+++ Source/ECS/Components/OctreeComponent.h
@@ -1,10 +1,11 @@
 #pragma once
 #include "../Component.h"
 #include "../Graphics/Voxel/Octree.h"
+#include <vector>
 
 class OctreeComponent : public Component {
 
 public:
     OctreeComponent() { type = ComponentType::Octree; }
-    uint32* data;
+    std::vector<uint32> data;
 };

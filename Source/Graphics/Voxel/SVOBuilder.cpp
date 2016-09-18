7ca38645b6d31a0be6b49c6e56ab6b18318400fc
Vladimir Zarypov <krre31@gmail.com>
2016-09-18 09:12:44 +0300

    Set all bits of leaf mask for test octree

--- Source/Graphics/Voxel/SVOBuilder.cpp
+++ Source/Graphics/Voxel/SVOBuilder.cpp
@@ -6,6 +6,6 @@ SVOBuilder::SVOBuilder() {
 
 std::shared_ptr<std::vector<uint32_t> > SVOBuilder::buildTest() {
     auto data = std::make_shared<std::vector<uint32_t>>();
-    data->push_back(0x0000FEFE);
+    data->push_back(0x0000FFFF);
     return data;
 }

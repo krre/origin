440a62e935760e23e57ab4994e20e322f9e1d835
Vladimir Zarypov <krre31@gmail.com>
2016-09-21 08:05:08 +0300

    Add new child octree to test octree

--- Source/Graphics/Voxel/SVOBuilder.cpp
+++ Source/Graphics/Voxel/SVOBuilder.cpp
@@ -6,7 +6,8 @@ SVOBuilder::SVOBuilder() {
 
 std::shared_ptr<std::vector<uint32_t> > SVOBuilder::buildTest() {
     auto data = std::make_shared<std::vector<uint32_t>>();
-    data->push_back(0x00007F00);
 //    data->push_back(0x00000000);
+    data->push_back(0x00027F00); // 0000 0000 0000 0010 | 0111 1111 | 0100 0000
+    data->push_back(0x00006F00); // 0000 0000 0000 0000 | 0110 1111 | 0000 0000
     return data;
 }

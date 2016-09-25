1a9420bea34bb487e33ec8873690fb044faf13d1
Vladimir Zarypov <krre31@gmail.com>
2016-09-25 16:08:06 +0300

    OctreeFarm renders test octree

--- Tools/OctreeFarm/Camera.h
+++ Tools/OctreeFarm/Camera.h
@@ -10,7 +10,19 @@ public:
     glm::mat4 cameraToWorld() const { return m_cameraToWorld; }
     glm::mat4 worldToCamera() const { return m_worldToCamera; }
 
+    float fov() { return m_fov; }
+    glm::vec3 up() { return m_up; }
+    glm::vec3 look() { return m_look; }
+    glm::vec3 right() { return m_right; }
+
 private:
+    float m_fov = glm::radians(50.0f);
+    float aspect;
+    const float distance = 0.5f; // distance from camera to screen
+    const glm::vec3 m_up = glm::vec3(0.0, 1.0, 0.0);
+    const glm::vec3 m_look = glm::vec3(0.0, 0.0, -1.0);
+    const glm::vec3 m_right = glm::vec3(1.0, 0.0, 0.0);
+
     glm::mat4 m_cameraToWorld = glm::mat4(1.0);
     glm::mat4 m_worldToCamera = glm::mat4(1.0);
     glm::vec3 m_position;
--- Tools/OctreeFarm/GLWidget.cpp
+++ Tools/OctreeFarm/GLWidget.cpp
@@ -2,9 +2,9 @@
 #include <QtCore>
 #include <QApplication>
 #include <glm/glm.hpp>
+#include <glm/gtx/matrix_decompose.hpp>
 
-GLWidget::GLWidget(QWidget* parent) :
-    QOpenGLWidget(parent) {
+GLWidget::GLWidget(QWidget* parent) : QOpenGLWidget(parent) {
 }
 
 void GLWidget::initializeGL() {
@@ -43,7 +43,7 @@ void GLWidget::initializeGL() {
 
     glGenBuffers(1, &objectsTbo);
     glBindBuffer(GL_TEXTURE_BUFFER, objectsTbo);
-    glBufferData(GL_TEXTURE_BUFFER, 100, NULL, GL_STATIC_DRAW);
+    glBufferData(GL_TEXTURE_BUFFER, sizeof(glm::vec4) * 100, NULL, GL_STATIC_DRAW);
 
     glGenBuffers(1, &objectsTexture);
     glBindTexture(GL_TEXTURE_BUFFER, objectsTexture);
@@ -51,18 +51,79 @@ void GLWidget::initializeGL() {
 
     glGenBuffers(1, &octreesTbo);
     glBindBuffer(GL_TEXTURE_BUFFER, octreesTbo);
-    glBufferData(GL_TEXTURE_BUFFER, 100, NULL, GL_STATIC_DRAW);
+    glBufferData(GL_TEXTURE_BUFFER, sizeof(uint32_t) * 100, NULL, GL_STATIC_DRAW);
 
     glGenBuffers(1, &octreesTexture);
     glBindTexture(GL_TEXTURE_BUFFER, octreesTexture);
     glTexBuffer(GL_TEXTURE_BUFFER, GL_RGBA8UI, octreesTbo);
 
-    updateOctreeInGPU(0, octree.data(), octree.count());
+    updateOctreeInGPU(0, octree.data(), sizeof(uint32_t) * octree.count());
 }
 
 void GLWidget::paintGL() {
+
+    QVector<glm::vec4> object;
+    object.push_back(octree.octreeToWorld()[0]);
+    object.push_back(octree.octreeToWorld()[1]);
+    object.push_back(octree.octreeToWorld()[2]);
+    object.push_back(octree.octreeToWorld()[3]);
+
+    glm::mat4 cameraToOctree = octree.worldToOctree() * camera.cameraToWorld();
+
+    glm::vec3 scale;
+    glm::quat rotation;
+    glm::vec3 translation;
+    glm::vec3 skew;
+    glm::vec4 perspective;
+    glm::decompose(cameraToOctree, scale, rotation, translation, skew, perspective);
+
+    object.push_back(glm::vec4(translation.x, translation.y, translation.z, 1.0));
+
+    glm::vec3 up = camera.up() * rotation;
+    glm::vec3 look = camera.look() * rotation;
+    glm::vec3 right = camera.right() * rotation;
+
+    // Ray calculation is based on Johns Hopkins presentation:
+    // http://www.cs.jhu.edu/~cohen/RendTech99/Lectures/Ray_Casting.bw.pdf
+    glm::vec3 h0 = look - up * glm::tan(camera.fov()); // min height vector
+    glm::vec3 h1 = look + up * glm::tan(camera.fov()); // max height vector
+    glm::vec3 stepH = (h1 - h0) / height();
+    h0 += stepH / 2;
+
+    glm::vec3 w0 = look - right * glm::tan(camera.fov()) * width() / height(); // min width vector
+    glm::vec3 w1 = look + right * glm::tan(camera.fov()) * width() / height(); // max width vector
+    glm::vec3 stepW = (w1 - w0) / width();
+    w0 += stepW / 2;
+
+    glm::vec3 startCornerPos = w0 + h0;
+
+    object.push_back(glm::vec4(startCornerPos.x, startCornerPos.y, startCornerPos.z, 0.0));
+    object.push_back(glm::vec4(stepW.x, stepW.y, stepW.z, 0.0));
+    object.push_back(glm::vec4(stepH.x, stepH.y, stepH.z, 0.0));
+
+    glm::vec3 octreeColor = glm::vec3(0.0, 1.0, 0.0);
+    object.push_back(glm::vec4(octreeColor.x, octreeColor.y, octreeColor.z, 1.0));
+
+    glBindBuffer(GL_TEXTURE_BUFFER, objectsTbo);
+    glBufferSubData(GL_TEXTURE_BUFFER, 0, sizeof(glm::vec4) * object.size(), object.data());
+
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     program.bind();
+
+    QVector3D lightColor = QVector3D(1.0, 1.0, 1.0);
+    QVector3D lightPos = QVector3D(1.5, 2.5, 1.0);
+
+    program.setUniformValue("lightColor", lightColor);
+    program.setUniformValue("lightPos", lightPos);
+    program.setUniformValue("ambientStrength", 0.1f);
+    program.setUniformValue("objectCount", 1);
+
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_BUFFER, objectsTexture);
+
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_BUFFER, octreesTexture);
+
     vao.bind();
     glDrawArrays(GL_TRIANGLES, 0, 6);
 }

80f2de33d79e02896e543f89a3060e3c4b2c6cd5
Vladimir Zarypov <krre31@gmail.com>
2016-09-25 14:41:32 +0300

    OctreeFarm: Add transform matrices to Camera class

--- Tools/OctreeFarm/Camera.cpp
+++ Tools/OctreeFarm/Camera.cpp
@@ -1,5 +1,6 @@
 #include "Camera.h"
 
 Camera::Camera(QObject* parent) : QObject(parent) {
-
+    m_cameraToWorld = glm::translate(m_cameraToWorld, glm::vec3(0.0, 0.0, 5.0));
+    m_worldToCamera = glm::inverse(m_cameraToWorld);
 }
--- Tools/OctreeFarm/Camera.h
+++ Tools/OctreeFarm/Camera.h
@@ -1,9 +1,21 @@
 #pragma once
 #include <QObject>
+#include <glm/ext.hpp>
 
 class Camera : public QObject {
     Q_OBJECT
 
 public:
     explicit Camera(QObject* parent = 0);
+    glm::mat4 cameraToWorld() const { return m_cameraToWorld; }
+    glm::mat4 worldToCamera() const { return m_worldToCamera; }
+
+private:
+    glm::mat4 m_cameraToWorld = glm::mat4(1.0);
+    glm::mat4 m_worldToCamera = glm::mat4(1.0);
+    glm::vec3 m_position;
+    glm::quat m_rotation;
+    float m_scale = 1.0;
+    float m_yaw = 0;
+    float m_pitch = 0;
 };

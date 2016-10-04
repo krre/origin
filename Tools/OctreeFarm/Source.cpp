003b1b77b94b38d661adb91235f19f62db1406a5
Vladimir Zarypov <krre31@gmail.com>
2016-10-04 16:28:26 +0300

    OctreeFarm: Source::binary() supports split octrees

--- Tools/OctreeFarm/Source.cpp
+++ Tools/OctreeFarm/Source.cpp
@@ -27,42 +27,72 @@ QString Source::serialize() {
 QSharedPointer<QVector<uint32_t>> Source::binary() {
     QSharedPointer<QVector<uint32_t>> data;
     data.reset(new QVector<uint32_t>());
-    data->append(0); // Header
+
+    // Append header
+    data->append(0);
+
+    QVector<json::object_t*> octreesFirst;
+    QVector<json::object_t*> octreesSecond;
 
     json::object_t* parent = root.get_ptr<json::object_t*>();
-    uint32_t offset = 0;
+    octreesFirst.append(parent);
+
     QVector<uint32_t> colorDescriptors;
     QVector<uint32_t> colors;
+    uint32_t offset = 0;
+
+    std::cout << root.dump(4) << std::endl;
 
     // Append node descriptors
     while (true) {
 
         uint32_t nodeDescriptor = 0;
         uint32_t colorDescriptor = 0;
+        octreesSecond.clear();
+        json::object_t* octree = octreesFirst.takeFirst();
 
-        for (auto& node: (*parent)) {
-            nodeDescriptor |= (1 << (8 + std::stoi(node.first)));
+        for (auto& node: (*octree)) {
+            nodeDescriptor |= (1 << (8 + std::stoi(node.first))); // Valid nodes
 
             json::iterator iter = node.second.find("children");
             if (iter != node.second.end()) {
-                nodeDescriptor |= (1 << std::stoi(node.first));
+                nodeDescriptor |= (1 << std::stoi(node.first)); // Non-leaf nodes
+                octreesSecond.append(iter.value().get_ptr<json::object_t*>());
             }
 
             iter = node.second.find("color");
             if (iter != node.second.end()) {
-                colorDescriptor |= (1 << std::stoi(node.first));
-                std::string nameColor = (*parent)[node.first]["color"];
+                colorDescriptor |= (1 << std::stoi(node.first)); // Valid colors
+                std::string nameColor = (*octree)[node.first]["color"];
                 QColor color(nameColor.c_str());
                 colors.append(color.rgba());
             }
         }
 
+
+        int childNum = Utils::bitCount8(nodeDescriptor);
+        if (childNum) {
+            if (data->count() == 1) { // First descriptor in vector
+                nodeDescriptor |= (1 << 17); // Set offset to 1
+            } else {
+                uint32_t lastDescriptor = data->at(offset);
+                nodeDescriptor |= ((lastDescriptor >> 17) + Utils::bitCount8(lastDescriptor));
+            }
+        }
+
         data->append(nodeDescriptor);
+
         colorDescriptors.append(colorDescriptor);
         offset++;
-        parents[offset] = parent;
+        parents[offset] = octree;
 
-        break;
+        if (octreesFirst.isEmpty()) {
+            if (octreesSecond.isEmpty()) {
+                break;
+            } else {
+                octreesFirst = octreesSecond;
+            }
+        }
     }
 
     (*data)[0] = offset + 1; // Address to block info
@@ -74,15 +104,20 @@ QSharedPointer<QVector<uint32_t>> Source::binary() {
     offset = colorDescriptors.count();
     for (int i = 0 ; i < colorDescriptors.count(); i++) {
         uint32_t colorDescriptor = colorDescriptors[i];
-        colorDescriptor |= (offset << 8);
+        int numColors = Utils::bitCount8(colorDescriptor);
+        if (numColors) {
+            colorDescriptor |= (offset << 8);
+            offset += numColors;
+        }
         data->append(colorDescriptor);
-        offset += Utils::bitCount8(colorDescriptor);
     }
 
     // Append colors
     for (int i = 0; i < colors.count(); i++) {
         data->append(colors[i]);
     }
+
+    qDebug() << *data;
 
     return data;
 }

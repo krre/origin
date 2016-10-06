c8d8fc4cd0a50752c787bb771807628a6fdf1462
Vladimir Zarypov <krre31@gmail.com>
2016-10-06 13:53:02 +0300

    Fix comment from test octree

--- Source/Graphics/Voxel/SVOBuilder.cpp
+++ Source/Graphics/Voxel/SVOBuilder.cpp
@@ -10,7 +10,7 @@ std::shared_ptr<std::vector<uint32_t> > SVOBuilder::buildTest() {
     data->push_back(0x00000003); // => Block info
     // Nodes
     data->push_back(0x00027F08); // 0000 0000 0000 0010 | 0111 1111 | 0000 1000
-    data->push_back(0x0000BF00); // 0000 0000 0000 0000 | 1101 1111 | 0000 0000
+    data->push_back(0x0000BF00); // 0000 0000 0000 0000 | 1011 1111 | 0000 0000
     // Block info
     data->push_back(0x00000000);
     // Attach data

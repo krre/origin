cbc34048f592a4e437288a2701e4d6410af84e4e
Vladimir Zarypov <krre31@gmail.com>
2016-10-07 08:14:29 +0300

    OctreeFarm: Update data for test octree

--- Tools/OctreeFarm/Octree.cpp
+++ Tools/OctreeFarm/Octree.cpp
@@ -38,28 +38,31 @@ void Octree::createTest() {
     storage.clear();
     // Test data
     // Header
-    storage.append(0x00000003); // => Block info
+    data->push_back(0x00000004); // => Block info
     // Nodes
-    storage.append(0x00027F08); // 0000 0000 0000 0010 | 0111 1111 | 0000 1000
-    storage.append(0x0000BF00); // 0000 0000 0000 0000 | 1101 1111 | 0000 0000
+    data->push_back(0x00027F08); // 0000 0000 0000 0010 | 0111 1111 | 0000 1000
+    data->push_back(0x0002BF80); // 0000 0000 0000 0010 | 1011 1111 | 1000 0000
+    data->push_back(0x00008F00); // 0000 0000 0000 0000 | 1000 1111 | 0000 0000
     // Block info
-    storage.append(0x00000000);
+    data->push_back(0x00000000);
     // Attach data
-    storage.append(0x0000027F); // 0000 0000 0000 0000 0000 0002 | 0111 1111
-    storage.append(0x00000999); // 0000 0000 0000 0000 0000 1001 | 1001 1001
+    data->push_back(0x0000037F); // 0000 0000 0000 0000 0000 0003 | 0111 1111
+    data->push_back(0x00000A99); // 0000 0000 0000 0000 0000 1010 | 1001 1001
+    data->push_back(0x00000000); // 0000 0000 0000 0000 0000 0000 | 0000 0000
     // Colors
-    storage.append(0x044A00FF);
-    storage.append(0xA30000FF);
-    storage.append(0x13DAFFFF);
-    storage.append(0x7E4681FF);
-    storage.append(0xFF0011FF);
-    storage.append(0x903472FF);
-    storage.append(0xAD7D4DFF);
+    data->push_back(0x044A00FF);
+    data->push_back(0xA30000FF);
+    data->push_back(0x13DAFFFF);
+    data->push_back(0x7E4681FF);
+    data->push_back(0xFF0011FF);
+    data->push_back(0x903472FF);
+    data->push_back(0xAD7D4DFF);
 
-    storage.append(0x5A1681FF);
-    storage.append(0x6B38ACFF);
-    storage.append(0x5DD772FF);
-    storage.append(0xC5651FFF);
+    data->push_back(0x5A1681FF);
+    data->push_back(0x6B38ACFF);
+    data->push_back(0x5DD772FF);
+    data->push_back(0xC5651FFF);
+    return data;
     */
 }
 

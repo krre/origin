ace8bce230f846cf7c3545a2ca5bd95e21f2979c
Vladimir Zarypov <krre31@gmail.com>
2016-10-08 09:41:06 +0300

    OctreeFarm: Fix code format

--- Tools/OctreeFarm/main.cpp
+++ Tools/OctreeFarm/main.cpp
@@ -1,7 +1,7 @@
 #include "MainWindow.h"
 #include <QApplication>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
     app.setApplicationName("OctreeFarm");
     app.setApplicationVersion("0.1.0");

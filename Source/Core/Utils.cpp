91ab7f9ecf0debdf8b3eb9c80bb81ab1523fda7b
Vladimir Zarypov <krre31@gmail.com>
2016-10-11 10:51:16 +0300

    Add Utils::bitCount8() method

--- Source/Core/Utils.cpp
+++ Source/Core/Utils.cpp
@@ -31,3 +31,15 @@ std::string Utils::readTextFile(const std::string& filePath) {
         return "";
     }
 }
+
+int Utils::bitCount8(int value) {
+    int count = 0;
+    for (int i = 0; i < 8; i++) {
+        if ((value & 1) == 1) {
+            count++;
+        }
+        value >>= 1;
+    }
+
+    return count;
+}
--- Source/Core/Utils.h
+++ Source/Core/Utils.h
@@ -11,4 +11,5 @@ public:
     static std::string getPathSeparator();
     static uint32_t rgbaToUint32(const glm::vec4& color);
     static std::string readTextFile(const std::string& filePath);
+    static int bitCount8(int value);
 };

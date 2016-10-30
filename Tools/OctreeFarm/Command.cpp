657433b14d79796c258090fbe91a5ea52fd10e55
Vladimir Zarypov <krre31@gmail.com>
2016-10-30 07:17:10 +0200

    OctreeFarm: Add AddCommand class for Undo system

--- Tools/OctreeFarm/Command.cpp
+++ Tools/OctreeFarm/Command.cpp
@@ -3,3 +3,7 @@
 DeleteCommand::DeleteCommand(QUndoCommand* parent) : QUndoCommand(parent) {
 
 }
+
+AddCommand::AddCommand(QUndoCommand* parent) : QUndoCommand(parent) {
+
+}
--- Tools/OctreeFarm/Command.h
+++ Tools/OctreeFarm/Command.h
@@ -10,3 +10,13 @@ signals:
 
 public slots:
 };
+
+class AddCommand : public QUndoCommand {
+
+public:
+    explicit AddCommand(QUndoCommand* parent = 0);
+
+signals:
+
+public slots:
+};

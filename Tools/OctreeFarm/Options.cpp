586cb2d079a8976fe49365d16f675746c391a23d
Vladimir Zarypov <krre31@gmail.com>
2016-11-10 08:51:01 +0200

    OctreeFarm: Add OK|Cancel buttons to Options dialog

--- Tools/OctreeFarm/Options.cpp
+++ Tools/OctreeFarm/Options.cpp
@@ -1,7 +1,20 @@
 #include "Options.h"
-#include <QtCore>
+#include <QtWidgets>
 
 Options::Options(QWidget* parent) : QDialog(parent) {
     setWindowTitle(tr("Options"));
     setMinimumSize(400, 300);
+
+    buttonBox = new QDialogButtonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel);
+    connect(buttonBox, &QDialogButtonBox::accepted, this, &QDialog::accept);
+    connect(buttonBox, &QDialogButtonBox::rejected, this, &QDialog::reject);
+    connect(this, &Options::accepted, this, &Options::onAccepted);
+
+    QVBoxLayout* mainLayout = new QVBoxLayout;
+    mainLayout->addWidget(buttonBox);
+    setLayout(mainLayout);
+}
+
+void Options::onAccepted() {
+    qDebug() << "accepted";
 }
--- Tools/OctreeFarm/Options.h
+++ Tools/OctreeFarm/Options.h
@@ -1,8 +1,15 @@
 #pragma once
 #include <QDialog>
+#include <QDialogButtonBox>
 
 class Options : public QDialog {
 
 public:
     Options(QWidget* parent = 0);
+
+private slots:
+    void onAccepted();
+
+private:
+    QDialogButtonBox* buttonBox;
 };

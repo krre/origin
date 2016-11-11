4281d9de17f85e8ccf897777d18fc78904f5db90
Vladimir Zarypov <krre31@gmail.com>
2016-11-11 11:37:43 +0200

    Add ProceduralObject::data member

--- Source/Universe/ProceduralObject.h
+++ Source/Universe/ProceduralObject.h
@@ -1,11 +1,14 @@
 #pragma once
 #include "../Core/Object.h"
+#include <vector>
 
 class ProceduralObject : public Object {
 
 public:
     ProceduralObject();
+    virtual void make() = 0;
 
 private:
     uint64_t seed;
+    std::shared_ptr<std::vector<uint32_t>> data;
 };

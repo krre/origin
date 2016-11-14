4b0ebc2976be2526e7d9b297a1650c05101b79c8
Vladimir Zarypov <krre31@gmail.com>
2016-11-14 14:07:09 +0200

    Find primary and secondary GPU in Vulkan::PhysicalDevice class

--- Source/Graphics/Vulkan/PhysicalDevice.cpp
+++ Source/Graphics/Vulkan/PhysicalDevice.cpp
@@ -6,4 +6,29 @@ PhysicalDevice::PhysicalDevice(const Instance* instance) : instance(instance) {
     vkEnumeratePhysicalDevices(instance->getHandle(), &count, nullptr);
     devices.resize(count);
     vkEnumeratePhysicalDevices(instance->getHandle(), &count, devices.data());
+
+    for (auto device: devices) {
+        VkPhysicalDeviceProperties deviceProperties;
+        vkGetPhysicalDeviceProperties(device, &deviceProperties);
+        devicesByType[deviceProperties.deviceType] = device;
+    }
+}
+
+VkPhysicalDevice PhysicalDevice::getPrimary() {
+    auto it = devicesByType.find(VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU);
+    if (it != devicesByType.end()) {
+        return it->second;
+    } else {
+        return devices[0]; // Any first
+    }
+}
+
+VkPhysicalDevice PhysicalDevice::getSecondary() {
+    for (auto it: devicesByType) {
+        if (it.first != VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU) {
+            return it.second; // Any not discrete
+        }
+    }
+
+    return 0;
 }
--- Source/Graphics/Vulkan/PhysicalDevice.h
+++ Source/Graphics/Vulkan/PhysicalDevice.h
@@ -3,6 +3,7 @@
 #include "Instance.h"
 #include <vulkan/vulkan.h>
 #include <vector>
+#include <map>
 
 namespace Vulkan {
 
@@ -11,11 +12,14 @@ class PhysicalDevice : public VulkanObject {
 public:
     PhysicalDevice(const Instance* instance);
     uint32_t getCount() const { return count; }
+    VkPhysicalDevice getPrimary();
+    VkPhysicalDevice getSecondary();
 
 private:
     const Instance* instance;
     uint32_t count = 0;
     std::vector<VkPhysicalDevice> devices;
+    std::map<VkPhysicalDeviceType, VkPhysicalDevice> devicesByType;
 };
 
 } // Vulkan

0e6d344607227c64c3011d56c649d912d71033e7
Vladimir Zarypov <krre31@gmail.com>
2016-11-15 09:48:41 +0200

    Add creating Vulkan logical device

--- Source/Core/App.cpp
+++ Source/Core/App.cpp
@@ -38,13 +38,20 @@ void App::setBackend(App::GraphicsBackend backend) {
 
 void App::init() {
 
+    // Vulkan instance
     vulkanInstance.reset(new Vulkan::Instance);
     if (!vulkanInstance->isValid()) {
         error(vulkanInstance->getError())
         return;
     }
 
+    // Vulkan device
     vulkanPhysicalDevices.reset(new Vulkan::PhysicalDevices(vulkanInstance.get()));
+    vulkanDevice.reset(new Vulkan::Device(vulkanPhysicalDevices.get()));
+    if (!vulkanDevice->isValid()) {
+        error(vulkanDevice->getError());
+        return;
+    }
 
     if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
         error("SDL could not initialize! SDL_Error: " << SDL_GetError());
--- Source/Graphics/Vulkan/Device.cpp
+++ Source/Graphics/Vulkan/Device.cpp
@@ -4,7 +4,59 @@
 using namespace Vulkan;
 
 Device::Device(const PhysicalDevices* physicalDevices) : physicalDevices(physicalDevices) {
+    float queuePriority = 1.0f;
+    VkDeviceQueueCreateInfo queueCreateInfo;
+    queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+    queueCreateInfo.queueCount = 1;
+    queueCreateInfo.pQueuePriorities = &queuePriority;
 
+    uint32_t familyCount;
+    vkGetPhysicalDeviceQueueFamilyProperties(physicalDevices->getPrimary(), &familyCount, nullptr);
+    std::vector<VkQueueFamilyProperties> familyProperties(familyCount);
+    vkGetPhysicalDeviceQueueFamilyProperties(physicalDevices->getPrimary(), &familyCount, familyProperties.data());
+
+    bool found = false;
+    for (int i = 0; i < familyCount; i++) {
+        if (familyProperties[i].queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+            queueCreateInfo.queueFamilyIndex = i;
+            found = true;
+            break;
+        }
+    }
+
+    VkDeviceCreateInfo createInfo;
+    createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+    createInfo.queueCreateInfoCount = 1;
+    createInfo.pQueueCreateInfos = &queueCreateInfo;
+    createInfo.enabledLayerCount = 0;
+    createInfo.enabledExtensionCount = 0;
+
+    VkResult result = vkCreateDevice(physicalDevices->getPrimary(), &createInfo, nullptr, &handle);
+
+    if (result != VK_SUCCESS) {
+        switch (result) {
+        case VK_ERROR_OUT_OF_HOST_MEMORY:
+            error = "Out of host memory";
+            break;
+        case VK_ERROR_OUT_OF_DEVICE_MEMORY:
+            error = "Out of device memory";
+            break;
+        case VK_ERROR_INITIALIZATION_FAILED:
+            error = "Initialization failed";
+            break;
+        case VK_ERROR_EXTENSION_NOT_PRESENT:
+            error = "Extension not present";
+            break;
+        case VK_ERROR_FEATURE_NOT_PRESENT:
+            error = "Feature not present";
+            break;
+        case VK_ERROR_TOO_MANY_OBJECTS:
+            error = "Too many objects";
+            break;
+        case VK_ERROR_DEVICE_LOST:
+            error = "Device lost";
+        }
+    }
 }
 
 Device::~Device() {

d197debaca9470af9085ac5e804ffae8a4cf3851
Vladimir Zarypov <krre31@gmail.com>
2016-11-16 14:16:08 +0200

    Add descriptions of error codes to VulkanManager class

--- Source/Graphics/Vulkan/VulkanManager.cpp
+++ Source/Graphics/Vulkan/VulkanManager.cpp
@@ -2,8 +2,26 @@
 
 using namespace Vulkan;
 
-VulkanManager::VulkanManager() {
+std::map<VkResult, std::string> VulkanManager::resultDescription;
 
+VulkanManager::VulkanManager() {
+    resultDescription[VK_SUCCESS] = "Success";
+    resultDescription[VK_NOT_READY] = "Not ready";
+    resultDescription[VK_TIMEOUT] = "Timeout";
+    resultDescription[VK_EVENT_SET] = "Event set";
+    resultDescription[VK_EVENT_RESET] = "Event reset";
+    resultDescription[VK_INCOMPLETE] = "Incoplete";
+    resultDescription[VK_ERROR_OUT_OF_HOST_MEMORY] = "Out of host memory";
+    resultDescription[VK_ERROR_OUT_OF_DEVICE_MEMORY] = "Out of device memory";
+    resultDescription[VK_ERROR_INITIALIZATION_FAILED] = "Initialization failed";
+    resultDescription[VK_ERROR_DEVICE_LOST] = "Device lost";
+    resultDescription[VK_ERROR_MEMORY_MAP_FAILED] = "Memory map failed";
+    resultDescription[VK_ERROR_LAYER_NOT_PRESENT] = "Layer not present";
+    resultDescription[VK_ERROR_EXTENSION_NOT_PRESENT] = "Extension not present";
+    resultDescription[VK_ERROR_FEATURE_NOT_PRESENT] = "Feature not present";
+    resultDescription[VK_ERROR_INCOMPATIBLE_DRIVER] = "Incompatible driver";
+    resultDescription[VK_ERROR_TOO_MANY_OBJECTS] = "Too many objects";
+    resultDescription[VK_ERROR_FORMAT_NOT_SUPPORTED] = "Format not supported";
 }
 
 bool VulkanManager::init() {
--- Source/Graphics/Vulkan/VulkanManager.h
+++ Source/Graphics/Vulkan/VulkanManager.h
@@ -3,7 +3,9 @@
 #include "Instance.h"
 #include "PhysicalDevices.h"
 #include "Device.h"
+#include <vulkan/vulkan.h>
 #include <string>
+#include <map>
 
 const std::string initError = "Vulkan initialization error\n\n";
 
@@ -15,8 +17,10 @@ public:
     std::string getError() const { return error; }
     Vulkan::Instance* getInstance() const { return instance.get(); }
     Vulkan::Device* getDevice() const { return device.get(); }
+    static std::string getResultDescription(VkResult result) { resultDescription.at(result); }
 
 private:
+    static std::map<VkResult, std::string> resultDescription;
     std::string error = "Undefined error";
     std::unique_ptr<Vulkan::Instance> instance;
     std::unique_ptr<Vulkan::PhysicalDevices> physicalDevices;

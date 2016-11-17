152051a7c09b7a15b72bcd01d8530c9628ebf67e
Vladimir Zarypov <krre31@gmail.com>
2016-11-17 15:56:53 +0200

    Add TODO to Queue class

--- Source/Graphics/Vulkan/Queue.cpp
+++ Source/Graphics/Vulkan/Queue.cpp
@@ -3,5 +3,6 @@
 using namespace Vulkan;
 
 Queue::Queue(const Device* device) : device(device) {
+    // TODO: Set family index and queue index
     vkGetDeviceQueue(device->getHandle(), 0, 0, &handle);
 }

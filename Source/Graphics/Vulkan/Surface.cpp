9d71ad31acdc45473105fab445e63707ff3d7cce
Vladimir Zarypov <krre31@gmail.com>
2016-11-17 12:45:38 +0200

    Separate platform dependend code

--- Source/Graphics/Vulkan/Surface.cpp
+++ Source/Graphics/Vulkan/Surface.cpp
@@ -1,7 +1,12 @@
 #include "Surface.h"
 #include "../../Core/App.h"
 #include <SDL_syswm.h>
-#include <X11/Xlib-xcb.h>
+#ifdef __linux__
+    #include <X11/Xlib-xcb.h>
+#elif _WIN32
+
+#endif
+
 
 using namespace Vulkan;
 
@@ -11,20 +16,25 @@ Surface::Surface(const Instance* instance) : instance(instance) {
     SDL_GetWindowWMInfo(App::get()->getWindow(), &wminfo);
 
     switch (wminfo.subsystem) {
-        case SDL_SYSWM_X11: {
-            VkXcbSurfaceCreateInfoKHR createInfo;
-            createInfo.sType = VK_STRUCTURE_TYPE_XCB_SURFACE_CREATE_INFO_KHR;
-            createInfo.flags = 0;
-            createInfo.connection = XGetXCBConnection(wminfo.info.x11.display);
-            createInfo.window = wminfo.info.x11.window;
+#ifdef __linux__
+    case SDL_SYSWM_X11: {
+        VkXcbSurfaceCreateInfoKHR createInfo;
+        createInfo.sType = VK_STRUCTURE_TYPE_XCB_SURFACE_CREATE_INFO_KHR;
+        createInfo.flags = 0;
+        createInfo.connection = XGetXCBConnection(wminfo.info.x11.display);
+        createInfo.window = wminfo.info.x11.window;
 
-            result = vkCreateXcbSurfaceKHR(instance->getHandle(), &createInfo, nullptr, &handle);
-            print("X11: " << result)
-            break;
-        }
-        case SDL_SYSWM_WINDOWS: {
-            print("Windows")
-            break;
-        }
+        result = vkCreateXcbSurfaceKHR(instance->getHandle(), &createInfo, nullptr, &handle);
+        print("X11: " << result)
+        break;
+    }
+
+#elif _WIN32
+    case SDL_SYSWM_WINDOWS: {
+        print("Windows")
+        break;
+    }
+#endif
+
     }
 }
--- Source/Graphics/Vulkan/Surface.h
+++ Source/Graphics/Vulkan/Surface.h
@@ -1,5 +1,10 @@
 #pragma once
-#define VK_USE_PLATFORM_XCB_KHR
+#ifdef __linux__
+    #define VK_USE_PLATFORM_XCB_KHR
+#elif _WIN32
+    #define VK_USE_PLATFORM_WIN32_KHR
+#endif
+
 #include "VulkanObject.h"
 #include "Instance.h"
 

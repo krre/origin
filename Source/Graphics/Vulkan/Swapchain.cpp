61ce0a2c04e43d24cf447b01466bdf41e2187ff5
Vladimir Zarypov <krre31@gmail.com>
2016-11-18 14:25:11 +0200

    Minimal working swapchain settings

--- Source/Graphics/Vulkan/Swapchain.cpp
+++ Source/Graphics/Vulkan/Swapchain.cpp
@@ -3,15 +3,31 @@
 using namespace Vulkan;
 
 Swapchain::Swapchain(const Device* device, const Surface* surface) : device(device) {
-    VkSwapchainCreateInfoKHR createInfo;
+    VkSwapchainCreateInfoKHR createInfo = {};
     createInfo.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
+    createInfo.flags = 0;
     createInfo.surface = surface->getHandle();
+//    createInfo.minImageCount =
+//    createInfo.imageFormat =
+//    createInfo.imageColorSpace =
+//    createInfo.imageExtent =
+    createInfo.imageArrayLayers = 1;
+    createInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+//    createInfo.imageSharingMode =
+//    createInfo.queueFamilyIndexCount =
+//    createInfo.pQueueFamilyIndices =
+//    createInfo.preTransform =
+    createInfo.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
+//    createInfo.presentMode =
+    createInfo.clipped = VK_TRUE;
+    createInfo.oldSwapchain = VK_NULL_HANDLE;
+
     result = vkCreateSwapchainKHR(device->getHandle(), &createInfo, nullptr, &handle);
     print(handle);
 }
 
 Swapchain::~Swapchain() {
-    if (handle) {
+    if (handle != VK_NULL_HANDLE) {
         vkDestroySwapchainKHR(device->getHandle(), handle, nullptr);
     }
 }

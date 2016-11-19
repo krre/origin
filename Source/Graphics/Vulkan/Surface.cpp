17c238294b5533a660cf3bf055ac09cafbf7cadb
Vladimir Zarypov <krre31@gmail.com>
2016-11-19 05:13:03 +0200

    Cleanup code

--- Source/Graphics/Vulkan/Surface.cpp
+++ Source/Graphics/Vulkan/Surface.cpp
@@ -23,29 +23,18 @@ Surface::Surface(const Instance* instance) : instance(instance) {
         createInfo.flags = 0;
         createInfo.connection = XGetXCBConnection(wminfo.info.x11.display);
         createInfo.window = wminfo.info.x11.window;
-
         result = vkCreateXcbSurfaceKHR(instance->getHandle(), &createInfo, nullptr, &handle);
-        print("X11: " << handle)
         break;
     }
 
 #elif _WIN32
     case SDL_SYSWM_WINDOWS: {
-//        TCHAR* className;
-//        GetClassName(wminfo.info.win.window, className, 256);
-//        WNDCLASS wce;
-//        GetClassInfo(GetModuleHandle(NULL), className, &wce);
-
         VkWin32SurfaceCreateInfoKHR createInfo = {};
         createInfo.sType = VK_STRUCTURE_TYPE_WIN32_SURFACE_CREATE_INFO_KHR;
         createInfo.flags = 0;
         createInfo.hwnd = wminfo.info.win.window;
-//        createInfo.hinstance = wce.hInstance;
         createInfo.hinstance = GetModuleHandle(nullptr);
-
         result = vkCreateWin32SurfaceKHR(instance->getHandle(), &createInfo, nullptr, &handle);
-
-        print("Windows: " << handle)
         break;
     }
 #endif

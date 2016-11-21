11d8a0b08fab00ba8eb5181665ba89234160abaf
Vladimir Zarypov <krre31@gmail.com>
2016-11-21 13:48:21 +0200

    Add CompileToSPIR-V.bat

---
+++ Data/Shader/CompileToSPIR-V.bat
@@ -0,0 +1,10 @@
+@ECHO OFF
+set validator=%VULKAN_SDK%/bin/glslangValidator -V
+%validator% Base.vert
+%validator% Base.frag
+%validator% Font.vert
+%validator% Font.frag
+%validator% Surface.frag
+%validator% Surface.vert
+%validator% Voxel.vert
+%validator% Voxel-Vk.frag

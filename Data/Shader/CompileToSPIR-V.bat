cd63dbc3c494295eac6344466f1357b93d315441
Vladimir Zarypov <krre31@gmail.com>
2016-11-21 15:44:44 +0200

    Add output filename for glslangValidator command

--- Data/Shader/CompileToSPIR-V.bat
+++ Data/Shader/CompileToSPIR-V.bat
@@ -1,10 +1,10 @@
 @ECHO OFF
 set validator=%VULKAN_SDK%/bin/glslangValidator -V
-%validator% Base-Vk.vert
-%validator% Base-Vk.frag
-%validator% Font-Vk.vert
-%validator% Font-Vk.frag
-%validator% Surface.frag
-%validator% Surface.vert
-%validator% Voxel.vert
-%validator% Voxel-Vk.frag
+%validator% Base-Vk.vert -o Base.vert.spv
+%validator% Base-Vk.frag -o Base.frag.spv
+%validator% Font-Vk.vert -o Font.vert.spv
+%validator% Font-Vk.frag -o Font.frag.spv
+%validator% Surface.vert -o Surface.vert.spv
+%validator% Surface.frag -o Surface.frag.spv
+%validator% Voxel.vert -o Voxel.vert.spv
+%validator% Voxel-Vk.frag -o Voxel.frag.spv

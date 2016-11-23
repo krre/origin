7b32804cc8b05d8a8d29409ebe35c55b1a5edd38
Vladimir Zarypov <krre31@gmail.com>
2016-11-23 13:47:33 +0200

    Fill ShaderResource::load() method

--- Source/Resource/ShaderResource.cpp
+++ Source/Resource/ShaderResource.cpp
@@ -1,9 +1,22 @@
 #include "ShaderResource.h"
+#include <fstream>
 
 ShaderResource::ShaderResource() {
 
 }
 
 void ShaderResource::load(const std::string& path) {
+    std::ifstream file(path, std::ios::ate | std::ios::binary);
 
+    if (!file.is_open()) {
+        error("Failed to open file: " << path);
+    }
+
+    size_t fileSize = (size_t) file.tellg();
+    buffer.resize(fileSize);
+
+    file.seekg(0);
+    file.read(buffer.data(), fileSize);
+
+    file.close();
 }
--- Source/Resource/ShaderResource.h
+++ Source/Resource/ShaderResource.h
@@ -1,9 +1,13 @@
 #pragma once
 #include "Resource.h"
+#include <vector>
 
 class ShaderResource : public Resource {
 
 public:
     ShaderResource();
     void load(const std::string& path) override;
+
+private:
+    std::vector<char> buffer;
 };

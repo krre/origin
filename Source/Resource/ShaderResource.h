812a20c759cd0acce12b98fe569f4433e0f1023e
Vladimir Zarypov <krre31@gmail.com>
2016-11-24 12:08:46 +0200

    Add ShaderResource::getBuffer() method

--- Source/Resource/ShaderResource.h
+++ Source/Resource/ShaderResource.h
@@ -7,6 +7,7 @@ class ShaderResource : public Resource {
 public:
     ShaderResource();
     void load(const std::string& path) override;
+    std::vector<char> getBuffer() const { return buffer; }
 
 private:
     std::vector<char> buffer;

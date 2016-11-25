0ffc7a2b5a6d01b90d0620522f9d0eb8a7d43b3b
Vladimir Zarypov <krre31@gmail.com>
2016-11-25 15:22:08 +0200

    Enable LunarG and Google validation layers

--- Source/Graphics/Vulkan/Instance.cpp
+++ Source/Graphics/Vulkan/Instance.cpp
@@ -16,6 +16,16 @@ Instance::Instance() {
     print("")
 
     const std::vector<const char*> enabledLayers = {
+//        "VK_LAYER_LUNARG_api_dump",
+        "VK_LAYER_LUNARG_image",
+        "VK_LAYER_LUNARG_parameter_validation",
+//        "VK_LAYER_LUNARG_vktrace",
+        "VK_LAYER_LUNARG_core_validation",
+        "VK_LAYER_LUNARG_screenshot",
+        "VK_LAYER_LUNARG_swapchain",
+        "VK_LAYER_LUNARG_object_tracker",
+        "VK_LAYER_GOOGLE_unique_objects",
+        "VK_LAYER_GOOGLE_threading",
         "VK_LAYER_LUNARG_standard_validation"
     };
 
@@ -38,6 +48,7 @@ Instance::Instance() {
     appInfo.pEngineName = "Gagarin Engine";
     appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);;
     appInfo.apiVersion = VK_API_VERSION_1_0;
+    appInfo.pNext = nullptr;
 
     VkInstanceCreateInfo createInfo = {};
     createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;

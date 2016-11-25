03dd9f6713c3faa8bff0e6ea683093aaa24d9fa8
Vladimir Zarypov <krre31@gmail.com>
2016-11-25 10:18:13 +0200

    Setup creating Vulkan pipeline layout

--- Source/Graphics/Vulkan/PipelineLayout.cpp
+++ Source/Graphics/Vulkan/PipelineLayout.cpp
@@ -3,7 +3,12 @@
 using namespace Vulkan;
 
 PipelineLayout::PipelineLayout(const Device* device) : device(device) {
+    VkPipelineLayoutCreateInfo pipelineLayoutInfo = {};
+    pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+    pipelineLayoutInfo.setLayoutCount = 0;
+    pipelineLayoutInfo.pushConstantRangeCount = 0;
 
+    result = vkCreatePipelineLayout(device->getHandle(), &pipelineLayoutInfo, nullptr, &handle);
 }
 
 PipelineLayout::~PipelineLayout() {

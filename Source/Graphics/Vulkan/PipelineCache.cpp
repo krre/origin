263b5ef79f20a389bceccc6cda356fec8d02f188
Vladimir Zarypov <krre31@gmail.com>
2016-11-25 13:53:44 +0200

    Setup creating Vulkan::PipelineCache instance

--- Source/Graphics/Vulkan/PipelineCache.cpp
+++ Source/Graphics/Vulkan/PipelineCache.cpp
@@ -3,7 +3,9 @@
 using namespace Vulkan;
 
 PipelineCache::PipelineCache(const Device* device) : device(device) {
-
+    VkPipelineCacheCreateInfo createInfo = {};
+    createInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_CACHE_CREATE_INFO;
+    result = vkCreatePipelineCache(device->getHandle(), &createInfo, nullptr, &handle);
 }
 
 PipelineCache::~PipelineCache() {

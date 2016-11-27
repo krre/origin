f7f91a8b8257dcd1ec7ba6c05c36568705192b48
Vladimir Zarypov <krre31@gmail.com>
2016-11-27 16:50:45 +0200

    Add Vulkan::Fence class

---
+++ Source/Graphics/Vulkan/Fence.cpp
@@ -0,0 +1,18 @@
+#include "Fence.h"
+
+using namespace Vulkan;
+
+Fence::Fence(const Device* device) : device(device) {
+    VkFenceCreateInfo createInfo = {};
+    createInfo.sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO;
+    createInfo.pNext = nullptr;
+    createInfo.flags = 0;
+
+    result = vkCreateFence(device->getHandle(), &createInfo, nullptr, &handle);
+}
+
+Fence::~Fence() {
+    if (handle != VK_NULL_HANDLE) {
+        vkDestroyFence(device->getHandle(), handle, nullptr);
+    }
+}
---
+++ Source/Graphics/Vulkan/Fence.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "VkObject.h"
+#include "Device.h"
+
+namespace Vulkan {
+
+class Fence : public VkObject {
+
+public:
+    Fence(const Device* device);
+    ~Fence();
+    VkFence getHandle() const { return handle; }
+    bool isValid() const { return handle != VK_NULL_HANDLE; }
+
+private:
+    VkFence handle = VK_NULL_HANDLE;
+    const Device* device;
+};
+
+} // Vulkan

cd61d678ae7c691390cd6d7597727a2479fe977d
Vladimir Zarypov <krre31@gmail.com>
2016-11-29 08:24:06 +0200

    Rename pApplicationName and pEngineName in VkApplicationInfo struct

--- Source/Graphics/Vulkan/Instance.cpp
+++ Source/Graphics/Vulkan/Instance.cpp
@@ -24,9 +24,9 @@ Instance::Instance() {
     }
 
     appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-    appInfo.pApplicationName = "Application name";
+    appInfo.pApplicationName = "Application";
     appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);;
-    appInfo.pEngineName = "Engine name";
+    appInfo.pEngineName = "Engine";
     appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);;
     appInfo.apiVersion = VK_API_VERSION_1_0;
     appInfo.pNext = nullptr;

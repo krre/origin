50ee9e247658ab86929e378ba691a5a1978556ce
Vladimir Zarypov <krre31@gmail.com>
2016-11-30 16:26:51 +0200

    Refactor Common.h

--- Source/Core/Common.h
+++ Source/Core/Common.h
@@ -4,13 +4,7 @@
 #include <memory>
 #ifndef QT_VERSION
     #include <nano-signal-slot/nano_signal_slot.hpp>
-#endif
-
-#ifndef QT_VERSION
     #define print(x) std::cout << x << std::endl;
-#endif
-
-#ifndef QT_VERSION
     #define error(x) std::cerr << x << std::endl;
 #endif
 

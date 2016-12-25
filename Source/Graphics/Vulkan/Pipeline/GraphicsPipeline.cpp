c78bb629d34449e969ff63497babedbd530caafb
Vladimir Zarypov <krre31@gmail.com>
2016-12-25 17:30:56 +0200

    Fix Vulkan pipeline rasterizer

--- Source/Graphics/Vulkan/Pipeline/GraphicsPipeline.cpp
+++ Source/Graphics/Vulkan/Pipeline/GraphicsPipeline.cpp
@@ -62,8 +62,8 @@ VkResult GraphicsPipeline::create() {
     rasterizer.rasterizerDiscardEnable = VK_FALSE;
     rasterizer.polygonMode = VK_POLYGON_MODE_FILL;
     rasterizer.lineWidth = 1.0f;
-    rasterizer.cullMode = VK_CULL_MODE_FRONT_AND_BACK;
-    rasterizer.frontFace = VK_FRONT_FACE_COUNTER_CLOCKWISE;
+    rasterizer.cullMode = VK_CULL_MODE_BACK_BIT;
+    rasterizer.frontFace = VK_FRONT_FACE_CLOCKWISE;
     rasterizer.depthBiasEnable = VK_FALSE;
 
     VkPipelineMultisampleStateCreateInfo multisampling = {};

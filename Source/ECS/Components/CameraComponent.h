a42f13a9ad55e835d343577d83e6cdb5d7f37863
Vladimir Zarypov <krre31@gmail.com>
2016-12-28 09:53:53 +0200

    Flip up vector in CameraComponent

--- Source/ECS/Components/CameraComponent.h
+++ Source/ECS/Components/CameraComponent.h
@@ -17,7 +17,7 @@ public:
     float fov = glm::radians(50.0f);
     float aspect;
     const float distance = 0.5f; // distance from camera to screen
-    const glm::vec4 up = glm::vec4(0.0, 1.0, 0.0, 0.0);
+    const glm::vec4 up = glm::vec4(0.0, -1.0, 0.0, 0.0); // Flip Vulkan coordinate system by setting Y to -1
     const glm::vec4 look = glm::vec4(0.0, 0.0, -1.0, 0.0);
     const glm::vec4 right = glm::vec4(1.0, 0.0, 0.0, 0.0);
     glm::mat4 projection = glm::perspective(fov, aspect, near, far);

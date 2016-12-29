d24a2966f6edad8f957f3abaa27707acb51f9e31
Vladimir Zarypov <krre31@gmail.com>
2016-12-29 15:24:04 +0200

    Add values map in Debug class

--- Source/Debug/Debug.cpp
+++ Source/Debug/Debug.cpp
@@ -3,3 +3,11 @@
 Debug::Debug() {
 
 }
+
+void Debug::setValue(const std::string& key, const std::string& value) {
+    values[key] = value;
+}
+
+std::string Debug::getValue(const std::string& key) const {
+    return values.at(key);
+}
--- Source/Debug/Debug.h
+++ Source/Debug/Debug.h
@@ -1,8 +1,14 @@
 #pragma once
 #include "../Core/Singleton.h"
+#include <map>
 
 class Debug : public Singleton<Debug> {
 
 public:
     Debug();
+    void setValue(const std::string& key, const std::string& value);
+    std::string getValue(const std::string& key) const;
+
+private:
+    std::map<std::string, std::string> values;
 };

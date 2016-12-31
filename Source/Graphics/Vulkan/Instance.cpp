af87c8435c89fc956cb9377fcca46866729887ab
Vladimir Zarypov <krre31@gmail.com>
2016-12-31 07:24:00 +0200

    Remove pEngineName and engineVersion assigns from Vulkan::Instance::appInfo

--- Source/Graphics/Vulkan/Instance.cpp
+++ Source/Graphics/Vulkan/Instance.cpp
@@ -26,8 +26,6 @@ Instance::Instance() {
     appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
     appInfo.pApplicationName = "Application";
     appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
-    appInfo.pEngineName = "Engine";
-    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
     appInfo.apiVersion = VK_API_VERSION_1_0;
 
     createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;

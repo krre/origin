3602b0a2f641a06541219009e3ed0f4ee99efe04
Vladimir Zarypov <krre31@gmail.com>
2017-01-05 12:59:38 +0200

    Fix setup of Vulkan::Buffer

--- Source/Graphics/Vulkan/Buffer.cpp
+++ Source/Graphics/Vulkan/Buffer.cpp
@@ -43,6 +43,10 @@ Buffer::Buffer(const Device* device, VkDeviceSize size, Buffer::Type type, Buffe
     default:
         break;
     }
+
+    if (destination == Destination::DEVICE) {
+        createInfo.usage |= VK_BUFFER_USAGE_TRANSFER_DST_BIT;
+    }
 }
 
 Buffer::~Buffer() {
@@ -59,7 +63,10 @@ VkResult Buffer::create() {
 
     VkMemoryRequirements memRequirements;
     vkGetBufferMemoryRequirements(device->getHandle(), handle, &memRequirements);
-    memory.setMemoryTypeIndex(Vulkan::Manager::get()->getPhysicalDevices()->findMemoryType(device->getPhysicalDevice(), memRequirements.memoryTypeBits, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT));
+    VkMemoryPropertyFlags properties = destination == Destination::HOST ?
+                (VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT) :
+                VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT;
+    memory.setMemoryTypeIndex(Vulkan::Manager::get()->getPhysicalDevices()->findMemoryType(device->getPhysicalDevice(), memRequirements.memoryTypeBits, properties));
     memory.allocate(memRequirements.size);
 
     vkBindBufferMemory(device->getHandle(), handle, memory.getHandle(), 0);

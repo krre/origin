d2c81837082ecf9246e1bf62756d4048ea349842
Vladimir Zarypov <krre31@gmail.com>
2017-01-10 11:22:31 +0200

    Add memory, buffer memory and image memory barrier vectors to Vulkan::PipelineBarrier class

--- Source/Graphics/Vulkan/Pipeline/PipelineBarrier.h
+++ Source/Graphics/Vulkan/Pipeline/PipelineBarrier.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Pipeline.h"
+#include <vector>
 
 namespace Vulkan {
 
@@ -7,9 +8,19 @@ class PipelineBarrier {
 
 public:
     PipelineBarrier();
+    uint32_t getMemoryBarrierCount() const { return memoryBarries.size(); }
+    VkMemoryBarrier* getMemoryBarrierData() { return memoryBarries.data(); }
+
+    uint32_t getBufferMemoryBarrierCount() const { return bufferMemoryBarries.size(); }
+    VkBufferMemoryBarrier* getBufferMemoryBarrierData() { return bufferMemoryBarries.data(); }
+
+    uint32_t getImageMemoryBarrierCount() const { return imageMemoryBarries.size(); }
+    VkImageMemoryBarrier* getImageMemoryBarrierData() { return imageMemoryBarries.data(); }
 
 private:
-
+    std::vector<VkMemoryBarrier> memoryBarries;
+    std::vector<VkBufferMemoryBarrier> bufferMemoryBarries;
+    std::vector<VkImageMemoryBarrier> imageMemoryBarries;
 };
 
 } // Vulkan

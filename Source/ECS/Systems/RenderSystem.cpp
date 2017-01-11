c090a19d03953f2ff84605ba201c10f48ee217b7
Vladimir Zarypov <krre31@gmail.com>
2017-01-11 11:40:32 +0200

    Cleanup includes in RenderSystem.cpp

--- Source/ECS/Systems/RenderSystem.cpp
+++ Source/ECS/Systems/RenderSystem.cpp
@@ -1,10 +1,4 @@
 #include "RenderSystem.h"
-#include "../../Debug/Console.h"
-#include "../../Debug/DebugHUD.h"
-#include "../../Core/App.h"
-#include "../../UI/Toast.h"
-#include "../../ECS/EntityManager.h"
-#include "../../ECS/Components/Components.h"
 
 RenderSystem::RenderSystem() {
     type = SystemType::Render;

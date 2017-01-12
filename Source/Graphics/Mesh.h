8e2e59f08f82c1ede5f42caf91ba038a33fb0098
Vladimir Zarypov <krre31@gmail.com>
2017-01-12 14:42:11 +0200

    Add Mesh::getVerticesSize() method

--- Source/Graphics/Mesh.h
+++ Source/Graphics/Mesh.h
@@ -8,6 +8,7 @@ public:
     Mesh();
     void setVertices(const std::vector<float> vertices);
     const std::vector<float>& getVertices() const { return vertices; }
+    uint32_t getVerticesSize() const { return sizeof(vertices[0]) * vertices.size(); }
 
     void setIndices(const std::vector<uint32_t> indices);
     const std::vector<uint32_t>& getIndices() const { return indices; }

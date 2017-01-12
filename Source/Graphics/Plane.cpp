a76db6d241167a44b86eac5e04ded7706ba9fe0c
Vladimir Zarypov <krre31@gmail.com>
2017-01-12 14:47:34 +0200

    Setup vertices and indices in Plane

--- Source/Graphics/Plane.cpp
+++ Source/Graphics/Plane.cpp
@@ -1,15 +1,12 @@
 #include "Plane.h"
 
 Plane::Plane(float width, float height) : width(width), height(height) {
-//    GLfloat vertexData[] = {
-//        -1.0f * width, 0.0f, -1.0f * height, 0.0f, 1.0f, 0.0f,
-//         1.0f * width, 0.0f,  1.0f * height, 0.0f, 1.0f, 0.0f,
-//         1.0f * width, 0.0f, -1.0f * height, 0.0f, 1.0f, 0.0f,
+    setVertices({
+        -1.0f, -1.0f,
+         1.0f, -1.0f,
+         1.0f,  1.0f,
+        -1.0f,  1.0f,
+    });
 
-//        -1.0f * width, 0.0f, -1.0f * height, 0.0f, 1.0f, 0.0f,
-//        -1.0f * width, 0.0f,  1.0f * height, 0.0f, 1.0f, 0.0f,
-//         1.0f * width, 0.0f,  1.0f * height, 0.0f, 1.0f, 0.0f,
-//    };
-
-//    verticles = std::vector<GLfloat>(vertexData, vertexData + sizeof vertexData / sizeof vertexData[0]);
+    setIndices({ 0, 1, 2, 2, 3, 0 });
 }

5219b87ee839eea19433017bfa720a2f5121819a
Vladimir Zarypov <krre31@gmail.com>
2017-01-28 12:51:56 +0200

    Setup Vulkan::Sample::createInfo by default values

--- Source/Graphics/Vulkan/Sampler.cpp
+++ Source/Graphics/Vulkan/Sampler.cpp
@@ -4,6 +4,17 @@ using namespace Vulkan;
 
 Sampler::Sampler(const Device* device) : device(device) {
     createInfo.sType = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO;
+    createInfo.magFilter = VK_FILTER_LINEAR;
+    createInfo.minFilter = VK_FILTER_LINEAR;
+    createInfo.mipmapMode = VK_SAMPLER_MIPMAP_MODE_LINEAR;
+    createInfo.addressModeU = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+    createInfo.addressModeV = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+    createInfo.addressModeW = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+    createInfo.mipLodBias = 0.0f;
+    createInfo.compareOp = VK_COMPARE_OP_NEVER;
+    createInfo.minLod = 0.0f;
+    createInfo.maxLod = 1.0f;
+    createInfo.borderColor = VK_BORDER_COLOR_FLOAT_OPAQUE_WHITE;
 }
 
 Sampler::~Sampler() {

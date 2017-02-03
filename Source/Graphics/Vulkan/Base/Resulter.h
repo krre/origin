33258e598ec976a2149655bbdfbae1f019af5886
Vladimir Zarypov <krre31@gmail.com>
2017-02-03 12:36:07 +0200

    Remove empty line in CHECK_RESULT macros

--- Source/Graphics/Vulkan/Base/Resulter.h
+++ Source/Graphics/Vulkan/Base/Resulter.h
@@ -6,7 +6,6 @@
 #include <assert.h>
 
 #define CHECK_RESULT(result, message) { \
-    \
     if ((result) != VK_SUCCESS) { \
         error(std::string((message)) + ": \"" + resultToString((result)) << "\" in " << __FILE__ << " at line " << __LINE__); \
         assert((result) == VK_SUCCESS); \

b1271dbda85759a51f03de1be376bcaf68eb9169
Vladimir Zarypov <krre31@gmail.com>
2017-02-05 12:36:03 +0200

    Enable glyph loading to Font

--- Source/UI/Font.cpp
+++ Source/UI/Font.cpp
@@ -24,18 +24,13 @@ void Font::load(const std::string& path) {
 
     FT_Set_Pixel_Sizes(face, 0, size);
 
-    //    fontShaderGroup = ResourceManager::get()->getResource<ShaderGroup>("FontShaderGroup");
-    //    fontShaderGroup->bind();
-        // Disable byte-alignment restriction
-    //    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-
         // Load first 128 characters of ASCII set
         for (uint8_t i = 0; i < 128; i++) {
             // Load character glyph
-    //        if (FT_Load_Char(face, i, FT_LOAD_RENDER)) {
-    //            ERROR("Failed to load Glyph");
-    //            continue;
-    //        }
+            if (FT_Load_Char(face, i, FT_LOAD_RENDER)) {
+                ERROR("Failed to load Glyph");
+                continue;
+            }
 
             // Generate texture
     //        GLuint texture;
@@ -60,19 +55,15 @@ void Font::load(const std::string& path) {
     //        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
             // Now store character for later use
-    //        Character character = {
-    ////            texture,
-    //            glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
-    //            glm::ivec2(face->glyph->bitmap_left, face->glyph->bitmap_top),
-    //            (uint32_t)face->glyph->advance.x
-    //        };
-    //        characters.insert(std::pair<char, Character>(i, character));
+            Character character = {
+    //            texture,
+                glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
+                glm::ivec2(face->glyph->bitmap_left, face->glyph->bitmap_top),
+                (uint32_t)face->glyph->advance.x
+            };
+            characters.insert(std::pair<char, Character>(i, character));
     //        glBindTexture(GL_TEXTURE_2D, 0);
         }
-
-        // Destroy FreeType once we're finished
-
-
 
         // Configure VAO/VBO for texture quads
     //    glGenBuffers(1, &vbo);

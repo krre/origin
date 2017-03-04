3f9a43d178068f89da2febdf2137fd4712a5ee35
Vladimir Zarypov <krre31@gmail.com>
2017-03-04 08:35:33 +0200

    Move allocateInfo.commandPool assigning to constructor of CommandBuffers class

--- Source/Graphics/Vulkan/Command/CommandBuffers.cpp
+++ Source/Graphics/Vulkan/Command/CommandBuffers.cpp
@@ -7,6 +7,7 @@ CommandBuffers::CommandBuffers(const Device* device, const CommandPool* commandP
     Devicer(device), commandPool(commandPool) {
     allocateInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
     allocateInfo.level = level;
+    allocateInfo.commandPool = commandPool->getHandle();
 }
 
 CommandBuffers::~CommandBuffers() {
@@ -16,7 +17,6 @@ CommandBuffers::~CommandBuffers() {
 bool CommandBuffers::allocate(uint32_t count) {
     assert(collection.size() == 0 && count > 0);
     collection.resize(count);
-    allocateInfo.commandPool = commandPool->getHandle();
     allocateInfo.commandBufferCount = count;
 
     return checkError(vkAllocateCommandBuffers(device->getHandle(), &allocateInfo, collection.data()), "Failed to allocate command buffers");

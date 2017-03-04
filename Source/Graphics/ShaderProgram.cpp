00eec838baaa4a8d5d9172c45fe79aa93c90ab77
Vladimir Zarypov <krre31@gmail.com>
2017-03-04 12:38:44 +0200

    Add ShaderProgram class

---
+++ Source/Graphics/ShaderProgram.cpp
@@ -0,0 +1,5 @@
+#include "ShaderProgram.h"
+
+ShaderProgram::ShaderProgram() {
+
+}
---
+++ Source/Graphics/ShaderProgram.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "../Core/Object.h"
+
+class ShaderProgram : public Object {
+
+public:
+    ShaderProgram();
+};

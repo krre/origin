86e65cb84b7dbe596a9fb01e6ac6bf1812a2b489
Vladimir Zarypov <krre31@gmail.com>
2017-03-06 14:44:49 +0200

    Parse shader binary code to vector of lines

--- Source/Graphics/SpirvParser.cpp
+++ Source/Graphics/SpirvParser.cpp
@@ -1,5 +1,6 @@
 #include "SpirvParser.h"
 #include <assert.h>
+#include <vector>
 
 SpirvParser::SpirvParser() {
     context = spvContextCreate(SPV_ENV_UNIVERSAL_1_0);
@@ -13,12 +14,35 @@ void SpirvParser::parse(const uint32_t* code, size_t count) {
     spv_diagnostic diagnostic = nullptr;
     spv_text resultText = nullptr;
     spvBinaryToText(context, code, count, 0, &resultText, &diagnostic);
-    PRINT("================")
-    PRINT(resultText->str)
     if (diagnostic) {
       spvDiagnosticPrint(diagnostic);
       spvDiagnosticDestroy(diagnostic);
       assert(false);
     }
+
+//    PRINT("================")
+//    PRINT(resultText->str)
+
+    std::vector<std::vector<std::string>> document;
+    std::vector<std::string> line;
+    std::string word;
+
+    int i = 0;
+    while (i < resultText->length) {
+        char c = resultText->str[i++];
+        if (c == ' ') {
+            line.push_back(word);
+            word.clear();
+            continue;
+        }
+
+        if (c == '\n') {
+            document.push_back(line);
+            line.clear();
+            continue;
+        }
+        word += c;
+    }
+
     spvTextDestroy(resultText);
 }

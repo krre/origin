7561725e18e01e37d3e8b19be91deb9784c7c817
Vladimir Zarypov <krre31@gmail.com>
2017-03-06 14:11:37 +0200

    Add SpirvParser::parse() method

--- Source/Graphics/SpirvParser.cpp
+++ Source/Graphics/SpirvParser.cpp
@@ -1,5 +1,24 @@
 #include "SpirvParser.h"
+#include <assert.h>
 
 SpirvParser::SpirvParser() {
+    context = spvContextCreate(SPV_ENV_UNIVERSAL_1_0);
+}
 
+SpirvParser::~SpirvParser() {
+    spvContextDestroy(context);
+}
+
+void SpirvParser::parse(const uint32_t* code, size_t count) {
+    spv_diagnostic diagnostic = nullptr;
+    spv_text resultText = nullptr;
+    spvBinaryToText(context, code, count, 0, &resultText, &diagnostic);
+    PRINT("================")
+    PRINT(resultText->str)
+    if (diagnostic) {
+      spvDiagnosticPrint(diagnostic);
+      spvDiagnosticDestroy(diagnostic);
+      assert(false);
+    }
+    spvTextDestroy(resultText);
 }
--- Source/Graphics/SpirvParser.h
+++ Source/Graphics/SpirvParser.h
@@ -1,11 +1,14 @@
 #pragma once
 #include "../Core/Object.h"
+#include <vulkan/libspirv.h>
 
 class SpirvParser : public Object {
 
 public:
     SpirvParser();
+    ~SpirvParser();
+    void parse(const uint32_t* code, size_t count);
 
 private:
-
+    spv_context context;
 };

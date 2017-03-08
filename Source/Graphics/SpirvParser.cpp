57a0c85fae9166aaaaad6a3a191f2a68204244c9
Vladimir Zarypov <krre31@gmail.com>
2017-03-08 19:26:54 +0200

    Finish SPIR-V parser

--- Source/Graphics/SpirvParser.cpp
+++ Source/Graphics/SpirvParser.cpp
@@ -26,8 +26,9 @@ void SpirvParser::parse(const uint32_t* code, size_t count) {
 //    PRINT("================")
 
     std::map<std::string, std::string> names;
-    std::map<std::string, VkDescriptorType> types;
+    std::map<std::string, VkDescriptorType> descriptorTypes;
     std::map<std::string, Attributes> attributes;
+    std::map<std::string, std::string> typePointers;
     std::vector<std::string> line;
     std::string word;
 
@@ -51,29 +52,46 @@ void SpirvParser::parse(const uint32_t* code, size_t count) {
                 std::string& decorateName = line.at(2);
 
                 if (decorateName == "Block") {
-                    types[id] = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
+                    descriptorTypes[id] = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
                 } else if (decorateName == "BufferBlock") {
-                    types[id] = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER;
+                    descriptorTypes[id] = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER;
                 } else if (decorateName == "Binding") {
                     if (attributes.find(id) == attributes.end()) {
                         attributes[id] = {};
                     }
                     attributes.at(id).binding = std::stoi(line.at(3));
+                    attributes.at(id).name = names.at(id);
                 } else if (decorateName == "Location") {
                     if (attributes.find(id) == attributes.end()) {
                         attributes[id] = {};
                     }
                     attributes.at(id).location = std::stoi(line.at(3));
+                    attributes.at(id).name = names.at(id);
                 } else if (decorateName == "DescriptorSet") {
                     if (attributes.find(id) == attributes.end()) {
                         attributes[id] = {};
                     }
                     attributes.at(id).descriptorSet = std::stoi(line.at(3));
+                    attributes.at(id).name = names.at(id);
                 }
 
 
             } else if (firstWord.at(0) == '%') {
-
+                std::string& id = firstWord;
+                if (line.at(2) == "OpTypePointer") {
+                    typePointers[id] = line.at(4);
+                } else if (line.at(2) == "OpVariable") {
+                    std::string& typePointerId = line.at(3);
+                    if (typePointers.find(typePointerId) != typePointers.end()) {
+                        std::string& descriptorTypeId = typePointers.at(typePointerId);
+                        if (descriptorTypes.find(descriptorTypeId) != descriptorTypes.end()) {
+                            VkDescriptorType descriptorType = descriptorTypes.at(descriptorTypeId);
+                            attributes.at(id).type = descriptorType;
+                        }
+                    }
+                }
+            } else if (line.size() == 3 && line.at(2) == "OpFunction") {
+                break;
             }
             line.clear();
             word.clear();
@@ -83,11 +101,15 @@ void SpirvParser::parse(const uint32_t* code, size_t count) {
         word += c;
     }
 
+    for (auto& it : attributes) {
+        descriptors[it.second.name] = it.second;
+    }
+
     spvTextDestroy(resultText);
 }
 
 void SpirvParser::dumpDescriptors() {
-    for (auto& descriptor : descriptors) {
-//        PRINT("name: " << descriptor.name << ", location: " << descriptor.location  << ", descriptorSet: " << descriptor.descriptorSet << ", binding: " << descriptor.binding)
+    for (auto& it : descriptors) {
+        PRINT("name: " << it.second.name << ", location: " << it.second.location  << ", descriptorSet: " << it.second.descriptorSet << ", binding: " << it.second.binding << ", type: " << it.second.type)
     }
 }

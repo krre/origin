f16bd681617fa443c89f8b40709b3d14c5bc7ec8
Vladimir Zarypov <krre31@gmail.com>
2017-03-09 16:10:30 +0200

    Swap createDescriptors() call in WorldShaderProgram constructor

--- Source/Scene/World/WorldShaderProgram.cpp
+++ Source/Scene/World/WorldShaderProgram.cpp
@@ -3,8 +3,10 @@
 WorldShaderProgram::WorldShaderProgram(const Vulkan::Device* device) : ShaderProgram(device) {
     addShader("Shader/Voxel.vert.spv");
     addShader("Shader/Voxel.frag.spv");
-    createDescriptors();
+
     link("ubo", &ubo, sizeof(ubo));
     link("pickResult", &pickResult, sizeof(pickResult));
     link("debugOut", &debugOut, sizeof(debugOut));
+
+    createDescriptors();
 }

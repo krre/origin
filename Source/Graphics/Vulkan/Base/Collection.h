3a630cc86cfa605a3153c6704d73365687fc4dcc
Vladimir Zarypov <krre31@gmail.com>
2017-03-09 12:30:53 +0200

    Add VULKAN_DESTROY_COLLECTION macro to Collection.h

--- Source/Graphics/Vulkan/Base/Collection.h
+++ Source/Graphics/Vulkan/Base/Collection.h
@@ -2,6 +2,13 @@
 #include "Resulter.h"
 #include <vector>
 
+#define VULKAN_DESTROY_COLLECTION(f) { \
+    if (collection.size()) { \
+        (f); \
+        collection.clear(); \
+    } \
+} \
+
 namespace Vulkan {
 
 template<typename T> class Collection : public Resulter {
@@ -11,6 +18,7 @@ public:
     int getCount() const { return collection.size(); }
     T at(size_t i) const { return collection.at(i); }
     const T* getData() const { return collection.data(); }
+    virtual void destroy() = 0;
 
 protected:
     std::vector<T> collection;

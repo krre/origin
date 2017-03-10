b78c3b8b87dc7b85a49adcb79916422855e351ef
Vladimir Zarypov <krre31@gmail.com>
2017-03-10 12:16:34 +0200

    Add Octree and RenderList struct to WorldShaderProgram class

--- Source/Scene/World/WorldShaderProgram.cpp
+++ Source/Scene/World/WorldShaderProgram.cpp
@@ -1,10 +1,13 @@
 #include "WorldShaderProgram.h"
+#include "../../Graphics/Voxel/GPUMemoryManager.h"
 
 WorldShaderProgram::WorldShaderProgram(const Vulkan::Device* device) : ShaderProgram(device) {
     addShader("Shader/Voxel.vert.spv");
     addShader("Shader/Voxel.frag.spv");
 
     linkBuffer("ubo", &ubo, sizeof(ubo));
+    linkBuffer("octree", &octree, MEMORY_SIZE);
+    linkBuffer("renderList", &renderList, MAX_OCTREE_COUNT * sizeof(uint32_t));
     linkBuffer("pickResult", &pickResult, sizeof(pickResult));
     linkBuffer("debugOut", &debugOut, sizeof(debugOut));
 

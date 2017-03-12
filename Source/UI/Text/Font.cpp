e9fafdf37f92d296499bf048cd167a09f1423390
Vladimir Zarypov <krre31@gmail.com>
2017-03-12 11:52:22 +0200

    Fill Font::characters vector by parsed from SDF font data

--- Source/UI/Text/Font.cpp
+++ Source/UI/Text/Font.cpp
@@ -48,7 +48,32 @@ void Font::load(const std::string& path) {
             std::vector<std::string> pair = Utils::split(words.at(1), '=');
             characters.resize(std::stoi(pair.at(1)));
         } else if (head == "char") {
+            Character character = {};
+            int id;
+            for (int i = 1; i < words.size(); i++) {
+                std::vector<std::string> pair = Utils::split(words.at(i), '=');
+                std::string& name = pair.at(0);
+                int value = std::stoi(pair.at(1));
+                if (name == "id") {
+                    id = value;
+                } else if (name == "x") {
+                    character.x = value;
+                } else if (name == "y") {
+                    character.y = value;
+                } else if (name == "width") {
+                    character.width = value;
+                } else if (name == "height") {
+                    character.heigth = value;
+                } else if (name == "xoffset") {
+                    character.xoffset = value;
+                } else if (name == "yoffset") {
+                    character.yoffset = value;
+                } else if (name == "xadvance") {
+                    character.xadvance = value;
+                }
+            }
 
+            characters[id] = character;
         }
     }
 }
--- Source/UI/Text/Font.h
+++ Source/UI/Text/Font.h
@@ -9,16 +9,13 @@ class Font : public Resource {
 public:
 
     struct Character {
-      float ax; // advance.x
-      float ay; // advance.y
-
-      float bw; // bitmap.width;
-      float bh; // bitmap.rows;
-
-      float bl; // bitmap_left;
-      float bt; // bitmap_top;
-
-      float tx; // x offset of glyph in texture coordinates
+        int x;
+        int y;
+        int width;
+        int heigth;
+        int xoffset;
+        int yoffset;
+        int xadvance;
     } character;
 
     Font();

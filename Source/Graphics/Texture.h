f465111ec70926aeb16021aa59ee7be294a13497
Vladimir Zarypov <krre31@gmail.com>
2017-03-12 14:03:23 +0200

    Add getters for width and height Texture::members

--- Source/Graphics/Texture.h
+++ Source/Graphics/Texture.h
@@ -6,9 +6,11 @@ class Texture : public Object {
 
 public:
     Texture(const std::string& path);
+    uint32_t getWidth() const { return width; }
+    uint32_t getHeight() const { return height; }
 
 private:
     std::vector<unsigned char> data;
-    uint32_t width;
-    uint32_t height;
+    uint32_t width = 0;
+    uint32_t height = 0;
 };

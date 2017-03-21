93c98716edd4e7611aabbab2877e510ac9085e0c
Vladimir Zarypov <krre31@gmail.com>
2017-03-21 06:29:36 +0200

    Move DebugReportCallback::createInfo member to private section

--- Source/Graphics/Vulkan/DebugReportCallback.h
+++ Source/Graphics/Vulkan/DebugReportCallback.h
@@ -18,10 +18,9 @@ public:
     VkResult create() override;
     void destroy() override;
 
-    VkDebugReportCallbackCreateInfoEXT createInfo = {};
-
 private:
     const Instance* instance;
+    VkDebugReportCallbackCreateInfoEXT createInfo = {};
     PFN_vkCreateDebugReportCallbackEXT pfnCreateDebugReportCallback;
     PFN_vkDestroyDebugReportCallbackEXT pfnDestroyDebugReportCallback;
 };

e2adef56156dfbde521ca583bda717a6789c2883
Vladimir Zarypov <krre31@gmail.com>
2017-03-24 11:37:55 +0200

    Convert Surface::getFormat() return value to reference

--- Source/Graphics/Vulkan/Surface.h
+++ Source/Graphics/Vulkan/Surface.h
@@ -22,7 +22,7 @@ public:
     uint32_t getHeight() const { return capabilities.currentExtent.height; }
 
     int getFormatCount() const { return formats.size(); }
-    VkSurfaceFormatKHR getFormat(size_t i) const { return formats.at(i); }
+    const VkSurfaceFormatKHR& getFormat(size_t i) const { return formats.at(i); }
 
     int getPresentModeCount() const { return presentModes.size(); }
     VkPresentModeKHR getPresentMode(size_t i) const { return presentModes.at(i); }

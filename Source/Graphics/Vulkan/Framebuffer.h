4566c312cb2f48671717353cfe6c6caf97760459
Vladimir Zarypov <krre31@gmail.com>
2017-03-25 13:03:52 +0200

    Add getters for width and height values to Vulkan::Framebuffer class

--- Source/Graphics/Vulkan/Framebuffer.h
+++ Source/Graphics/Vulkan/Framebuffer.h
@@ -14,8 +14,13 @@ public:
     void create() override;
     void destroy() override;
     void addAttachment(VkImageView attachment);
+
     void setWidth(uint32_t width);
+    uint32_t getWidth() const { return createInfo.width; }
+
     void setHeight(uint32_t height);
+    uint32_t getHeight() const { return createInfo.height; }
+
     void setRenderPass(VkRenderPass renderPass);
 
 private:

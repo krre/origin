b92e33ea33ff07f64c2dcb2f634098a4e030dbb0
Vladimir Zarypov <krre31@gmail.com>
2017-03-27 09:46:16 +0300

    Change texture image layout

--- Source/Graphics/Texture.cpp
+++ Source/Graphics/Texture.cpp
@@ -1,9 +1,13 @@
 #include "Texture.h"
 #include <lodepng/lodepng.h>
 #include "Vulkan/Manager.h"
+#include "Vulkan/Command/CommandBuffer.h"
+#include "Vulkan/Fence.h"
+#include "Vulkan/Queue/SubmitQueue.h"
 
 Texture::Texture(const std::string& path, VkFormat format) :
         image(Vulkan::Manager::get()->getDevice()) {
+    Vulkan::Device* device = Vulkan::Manager::get()->getDevice();
     uint32_t width;
     uint32_t height;
     unsigned result = lodepng::decode(data, width, height, path);
@@ -20,7 +24,35 @@ Texture::Texture(const std::string& path, VkFormat format) :
 
     image.write(data.data(), data.size());
 
-    imageView = std::make_shared<Vulkan::ImageView>(Vulkan::Manager::get()->getDevice(), image.getHandle());
+    imageView = std::make_shared<Vulkan::ImageView>(device, image.getHandle());
     imageView->createInfo.format = image.getFormat();
     imageView->create();
+
+    VkFormatProperties formatProps;
+    vkGetPhysicalDeviceFormatProperties(device->getPhysicalDevice()->getHandle(), VK_FORMAT_R8G8B8A8_UNORM, &formatProps);
+
+    /* See if we can use a linear tiled image for a texture, if not, we will
+     * need a staging image for the texture data */
+    bool needStaging = (!(formatProps.linearTilingFeatures & VK_FORMAT_FEATURE_SAMPLED_IMAGE_BIT)) ? true : false;
+    PRINT(needStaging)
+
+    Vulkan::CommandBuffers commandBuffers(image.getDevice(), Vulkan::Manager::get()->getCommandPool());
+    commandBuffers.allocate(1);
+
+    Vulkan::CommandBuffer commandBuffer(commandBuffers.at(0));
+    commandBuffer.begin();
+
+    commandBuffer.setImageLayout(image.getHandle(), VK_IMAGE_ASPECT_COLOR_BIT, VK_IMAGE_LAYOUT_PREINITIALIZED, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL, VK_PIPELINE_STAGE_HOST_BIT, VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT);
+
+    commandBuffer.end();
+
+    Vulkan::Fence fence(device);
+    fence.create();
+
+    Vulkan::SubmitQueue queue(device, Vulkan::Manager::get()->getGraphicsFamily());
+    queue.addCommandBuffer(commandBuffer.getHandle());
+    queue.submit(fence.getHandle());
+
+    device->waitForFences({ fence.getHandle() });
+
 }

4d78402dd58550e6090d43260d2c13221ca310b5
Vladimir Zarypov <krre31@gmail.com>
2017-03-29 15:45:25 +0300

    Move SPIR-V parsing to ShaderResource class

--- Source/Resource/ShaderResource.cpp
+++ Source/Resource/ShaderResource.cpp
@@ -1,8 +1,14 @@
 #include "ShaderResource.h"
 #include <fstream>
+#include "../Core/Utils.h"
+#include <assert.h>
 
 ShaderResource::ShaderResource() {
+    context = spvContextCreate(SPV_ENV_UNIVERSAL_1_0);
+}
 
+ShaderResource::~ShaderResource() {
+    spvContextDestroy(context);
 }
 
 void ShaderResource::load(const std::string& path) {
@@ -19,4 +25,156 @@ void ShaderResource::load(const std::string& path) {
     file.read(reinterpret_cast<char*>(buffer.data()), fileSize);
 
     file.close();
+
+    if (!buffer.empty()) {
+        parse(buffer.data(), buffer.size());
+    }
+}
+
+void ShaderResource::parse(const uint32_t* code, size_t count) {
+    descriptors.clear();
+    spv_diagnostic diagnostic = nullptr;
+    spv_text resultText = nullptr;
+    spvBinaryToText(context, code, count, 0, &resultText, &diagnostic);
+    if (diagnostic) {
+      spvDiagnosticPrint(diagnostic);
+      spvDiagnosticDestroy(diagnostic);
+      assert(false);
+    }
+
+//    PRINT(resultText->str)
+//    PRINT("================")
+
+    std::map<std::string, std::string> names;
+    std::map<std::string, std::vector<std::string>> instructions;
+    std::vector<std::string> line;
+    std::string word;
+    VkDescriptorType blockType;
+
+    // Parse SPIR-V text code to vector of lines
+    int i = 0;
+    const char quote = '\"';
+    while (i < resultText->length) {
+        char c = resultText->str[i++];
+        if (c == ' ') {
+            line.push_back(word);
+            word.clear();
+            continue;
+        }
+
+        if (c == '\n') {
+            line.push_back(word);
+            std::string& firstWord = line.at(0);
+            if (firstWord == "OpName") {
+                // Example:
+                // OpName %44 "ubo"
+                Utils::removeChar(line.at(2), quote);
+                names[line.at(1)] = line.at(2);
+            } else if (firstWord == "OpDecorate") {
+                // Set descriptor for uniform buffers and storage buffers
+                // Example:
+                // OpDecorate %83 Block|BufferBlock
+                // OpDecorate %85 DescriptorSet 0
+                // OpDecorate %85 Binding 1
+                std::string& id = line.at(1);
+                std::string& decorateName = line.at(2);
+
+                if (decorateName == "Block") {
+                    blockType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
+                } else if (decorateName == "BufferBlock") {
+                    blockType = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER;
+                } else if (decorateName == "DescriptorSet") {
+                    std::string& name = names.at(id);
+                    descriptors[name] = {};
+                    descriptors.at(name).set = std::stoi(line.at(3));
+                    descriptors.at(name).descriptorType = blockType; // from previous line
+                } else if (decorateName == "Binding") {
+                    std::string& name = names.at(id);
+                    descriptors.at(name).binding = std::stoi(line.at(3));
+                } else if (decorateName == "Location") {
+                    std::string& name = names.at(id);
+                    inputs[name] = {};
+                    inputs.at(name).location = std::stoi(line.at(3));
+                }
+            } else if (firstWord.at(0) == '%') {
+                std::string& id = firstWord;
+                // Set descriptor for samplers and texel buffers
+                // Example:
+                // %9 = OpTypeImage %6 2D 0 0 0 1 Unknown
+                // %10 = OpTypeSampledImage %9
+                // %11 = OpTypePointer UniformConstant %10
+                // %12 = OpVariable %11 UniformConstant
+                if (line.size() == 5 && line.at(2) == "OpVariable") {
+                    std::string& name = names.at(id);
+                    std::string& pointerId = line.at(3);
+                    std::string& typeId = instructions.at(pointerId).at(4);
+                    std::string& type = instructions.at(typeId).at(2);
+
+                    if (line.at(4) == "UniformConstant") {
+                        if (type == "OpTypeImage") {
+                            if (instructions.at(typeId).at(4) == "Buffer") {
+                                descriptors.at(name).descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_TEXEL_BUFFER;
+                            } else {
+                                if (instructions.at(typeId).at(9) == "Unknown") {
+                                    descriptors.at(name).descriptorType = VK_DESCRIPTOR_TYPE_SAMPLED_IMAGE;
+                                } else {
+                                    descriptors.at(name).descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_IMAGE;
+                                }
+                            }
+                        } else if (type == "OpTypeSampler") {
+                            descriptors.at(name).descriptorType = VK_DESCRIPTOR_TYPE_SAMPLER;
+                        } else if (type == "OpTypeSampledImage") {
+                            std::string& typeId2 = instructions.at(typeId).at(3);
+                            std::string& type2 = instructions.at(typeId2).at(4);
+                            if (type2 == "Buffer") {
+                                descriptors.at(name).descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_TEXEL_BUFFER;
+                            } else {
+                                descriptors.at(name).descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
+                            }
+                        }
+                    } else if (line.at(4) == "Input") {
+                        if (inputs.find(name) != inputs.end()) {
+                            inputs.at(name).variableType = type;
+                            if (type == "OpTypeVector") {
+                                inputs.at(name).vectorCount = std::stoi(instructions.at(typeId).at(4));
+                                std::string& vectorTypeId = instructions.at(typeId).at(3);
+                                inputs.at(name).valueType = instructions.at(vectorTypeId).at(2);
+                            }
+                        }
+                    }
+                }
+                instructions[id] = line;
+            } else if (line.size() >= 3 && line.at(2) == "OpFunction") {
+                break;
+            }
+            line.clear();
+            word.clear();
+            continue;
+        }
+
+        word += c;
+    }
+
+    spvTextDestroy(resultText);
+}
+
+void ShaderResource::dumpDescriptors() {
+    PRINT("Dump SPIR-V descriptors:")
+    for (auto& descriptor : descriptors) {
+        PRINT("name: " << descriptor.first
+              << ", set: " << descriptor.second.set
+              << ", binding: " << descriptor.second.binding
+              << ", descriptorType: " << descriptor.second.descriptorType)
+    }
+}
+
+void ShaderResource::dumpInputs() {
+    PRINT("Dump SPIR-V inputs:")
+    for (auto& input : inputs) {
+        PRINT("name: " << input.first
+              << ", location: " << input.second.location
+              << ", variableType: " << input.second.variableType
+              << ", valueType: " << input.second.valueType
+              << ", vectorCount: " << input.second.vectorCount)
+    }
 }
--- Source/Resource/ShaderResource.h
+++ Source/Resource/ShaderResource.h
@@ -1,15 +1,47 @@
 #pragma once
 #include "Resource.h"
 #include <vector>
+#include <map>
+#include <vulkan/vulkan.h>
+
+#ifdef __linux__
+#include <vulkan/libspirv.h>
+#elif _WIN32
+#include <spirv-tools/libspirv.h>
+#endif
 
 class ShaderResource : public Resource {
 
 public:
+
+    struct Descriptor {
+        int set;
+        int location;
+        int binding;
+        VkDescriptorType descriptorType;
+    };
+
+    struct Input {
+        int location;
+        std::string variableType;
+        std::string valueType;
+        int vectorCount;
+    };
+
     ShaderResource();
+    ~ShaderResource();
     void load(const std::string& path) override;
     const uint32_t* getData() const { return buffer.data(); }
     size_t getSize() const { return buffer.size(); }
 
+    void dumpDescriptors();
+    void dumpInputs();
+
 private:
+    spv_context context;
     std::vector<uint32_t> buffer;
+    std::map<std::string, Input> inputs;
+    std::map<std::string, Descriptor> descriptors;
+
+    void parse(const uint32_t* code, size_t count);
 };

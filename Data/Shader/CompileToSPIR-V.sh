8a2b753455ae0d2812bb63c4e149dcb7ee82329c
Vladimir Zarypov <krre31@gmail.com>
2017-04-02 13:38:58 +0300

    Remove Font shader

--- Data/Shader/CompileToSPIR-V.sh
+++ Data/Shader/CompileToSPIR-V.sh
@@ -1,8 +1,6 @@
 validator="$VULKAN_SDK/bin/glslangValidator -V -t"
 $validator Base.vert -o Base.vert.spv
 $validator Base.frag -o Base.frag.spv
-$validator Font.vert -o Font.vert.spv
-$validator Font.frag -o Font.frag.spv
 $validator Surface.vert -o Surface.vert.spv
 $validator Surface.frag -o Surface.frag.spv
 $validator Voxel.vert -o Voxel.vert.spv

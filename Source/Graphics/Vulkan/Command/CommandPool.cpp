09e793eb2369f757b2d2e1c87e36a76abc45b435
Vladimir Zarypov <krre31@gmail.com>
2017-04-09 15:34:10 +0300

    Set VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT flag in CommandPool::createInfo struct

--- Source/Graphics/Vulkan/Command/CommandPool.cpp
+++ Source/Graphics/Vulkan/Command/CommandPool.cpp
@@ -4,6 +4,7 @@ using namespace Vulkan;
 
 CommandPool::CommandPool(const Device* device, uint32_t familyIndex) : Devicer(device) {
     createInfo.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
+    createInfo.flags = VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT;
     createInfo.queueFamilyIndex = familyIndex;
 }
 

273f4e87307fee1bd29dc5b27ffe9da0d4f9daed
Vladimir Zarypov <krre31@gmail.com>
2017-04-09 17:26:35 +0300

    Add PauseDialog class

---
+++ Source/UI/Dialog/PauseDialog.cpp
@@ -0,0 +1,5 @@
+#include "PauseDialog.h"
+
+PauseDialog::PauseDialog() {
+
+}
---
+++ Source/UI/Dialog/PauseDialog.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "Dialog.h"
+
+class PauseDialog : public Dialog {
+
+public:
+    PauseDialog();
+
+private:
+
+};

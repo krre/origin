52888fb4179180b2df27b4d7b1108fae661f3a49
Vladimir Zarypov <krre31@gmail.com>
2017-04-09 18:10:31 +0300

    Move cursor to center of window on Dialog showing

--- Source/UI/Dialog/Dialog.cpp
+++ Source/UI/Dialog/Dialog.cpp
@@ -1,9 +1,11 @@
 #include "Dialog.h"
 #include "../../Scene/SceneManager.h"
 #include "../../Event/Input.h"
+#include "../../Core/App.h"
 
 Dialog::Dialog() {
-
+    SDL_SetRelativeMouseMode(SDL_FALSE);
+    SDL_WarpMouseInWindow(App::get()->getWindow(), App::get()->getWidth() / 2, App::get()->getHeight() / 2);
 }
 
 void Dialog::onKeyPressed(const SDL_KeyboardEvent& event) {

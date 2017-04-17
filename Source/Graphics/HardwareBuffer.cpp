66478b6371dd20ad8978d054788ce8c34375526a
Vladimir Zarypov <krre31@gmail.com>
2017-04-17 09:52:06 +0300

    Add HardwareBuffer class

---
+++ Source/Graphics/HardwareBuffer.cpp
@@ -0,0 +1,5 @@
+#include "HardwareBuffer.h"
+
+HardwareBuffer::HardwareBuffer() {
+
+}
---
+++ Source/Graphics/HardwareBuffer.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "../Core/Object.h"
+
+class HardwareBuffer : public Object {
+
+public:
+    HardwareBuffer();
+};

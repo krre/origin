dd244f0c9ce14daec49d850785dbd5aa9a8fb310
Vladimir Zarypov <krre31@gmail.com>
2017-04-17 13:10:39 +0300

    Add forward declaration for Device class in ShaderProgram.h

--- Source/Graphics/ShaderProgram.h
+++ Source/Graphics/ShaderProgram.h
@@ -1,7 +1,6 @@
 #pragma once
 #include "../Core/Object.h"
 #include "../Resource/ShaderResource.h"
-#include "Vulkan/Device/Device.h"
 #include "Vulkan/Descriptor/DescriptorPool.h"
 #include "Vulkan/Descriptor/DescriptorSetLayout.h"
 #include "Vulkan/Descriptor/DescriptorSets.h"
@@ -9,6 +8,10 @@
 #include "Vulkan/Buffer.h"
 #include "Vulkan/Image/Image.h"
 #include <map>
+
+namespace Vulkan {
+    class Device;
+}
 
 class ShaderProgram : public Object {
 

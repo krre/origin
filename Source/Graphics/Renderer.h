75085a8584807312960eebb14fe369679076f821
Vladimir Zarypov <krre31@gmail.com>
2017-04-25 09:18:17 +0300

    Add Renderer::pipelines map

--- Source/Graphics/Renderer.h
+++ Source/Graphics/Renderer.h
@@ -1,9 +1,12 @@
 #pragma once
 #include "Core/Singleton.h"
 #include "Vulkan/Instance.h"
+#include "GraphicsTypes.h"
+#include <map>
 
 namespace Vulkan {
     class Instance;
+    class GraphicsPipeline;
 }
 
 class Renderer : public Singleton<Renderer> {
@@ -11,7 +14,9 @@ class Renderer : public Singleton<Renderer> {
 public:
     Renderer();
     void render();
+    Vulkan::GraphicsPipeline* getPipeline(PipelinePreset preset) const { return pipelines.at(preset).get(); }
 
 private:
     std::unique_ptr<Vulkan::Instance> vulkan;
+    std::map<PipelinePreset, std::shared_ptr<Vulkan::GraphicsPipeline>> pipelines;
 };

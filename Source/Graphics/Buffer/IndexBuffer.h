6e7b89cf36b0d63d76a7a0ddbf6f3320307e63b1
Vladimir Zarypov <krre31@gmail.com>
2017-04-28 14:06:50 +0300

    Add IndexBuffer::getIndexType() method

--- Source/Graphics/Buffer/IndexBuffer.h
+++ Source/Graphics/Buffer/IndexBuffer.h
@@ -5,6 +5,7 @@ class IndexBuffer : public HardwareBuffer {
 
 public:
     IndexBuffer(uint64_t count, VkIndexType indexType = VK_INDEX_TYPE_UINT32);
+    VkIndexType getIndexType() const { return indexType; }
 
 private:
     VkIndexType indexType;

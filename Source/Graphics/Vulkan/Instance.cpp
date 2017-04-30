c69c4828fd6b45a87f10aeec743aaa896cf35920
Vladimir Zarypov <krre31@gmail.com>
2017-04-30 08:06:49 +0300

    Disable all default Vulkan layers

--- Source/Graphics/Vulkan/Instance.cpp
+++ Source/Graphics/Vulkan/Instance.cpp
@@ -29,15 +29,15 @@ Instance::Instance() {
 
     enabledLayers = {
 //        "VK_LAYER_LUNARG_api_dump",
-        "VK_LAYER_LUNARG_parameter_validation",
+//        "VK_LAYER_LUNARG_parameter_validation",
 //        "VK_LAYER_LUNARG_vktrace",
-        "VK_LAYER_LUNARG_core_validation",
-        "VK_LAYER_LUNARG_screenshot",
-        "VK_LAYER_LUNARG_swapchain",
-        "VK_LAYER_LUNARG_object_tracker",
-        "VK_LAYER_GOOGLE_unique_objects",
-        "VK_LAYER_GOOGLE_threading",
-        "VK_LAYER_LUNARG_standard_validation"
+//        "VK_LAYER_LUNARG_core_validation",
+//        "VK_LAYER_LUNARG_screenshot",
+//        "VK_LAYER_LUNARG_swapchain",
+//        "VK_LAYER_LUNARG_object_tracker",
+//        "VK_LAYER_GOOGLE_unique_objects",
+//        "VK_LAYER_GOOGLE_threading",
+//        "VK_LAYER_LUNARG_standard_validation"
     };
 
 #ifdef __linux__

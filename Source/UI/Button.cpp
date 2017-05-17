da72a818282a8afa01cbc9bba89fab65192dd1e9
Vladimir Zarypov <krre31@gmail.com>
2017-05-17 14:39:35 +0300

    dd forward declaration for Label class to Button.h

--- Source/UI/Button.cpp
+++ Source/UI/Button.cpp
@@ -1,5 +1,6 @@
 #include "Button.h"
 #include "Event/Event.h"
+#include "Text/Label.h"
 
 Button::Button() : Rectangle({ 100, 20 }) {
     setColor({ 0.6, 0.6, 0.6 });
@@ -20,8 +21,16 @@ void Button::setText(const std::string &text) {
     label->setZ(1.0f);
 }
 
+std::string Button::getText() const {
+    return label->getText();
+}
+
 void Button::setLabelColor(const Color& labelColor) {
     label->setColor(labelColor);
+}
+
+const Color& Button::getLabelColor() const {
+    return label->getColor();
 }
 
 void Button::prepareBatch(Batch2D* batch, VertexBuffer* vertexBuffer, IndexBuffer* indexBuffer) {
--- Source/UI/Button.h
+++ Source/UI/Button.h
@@ -1,7 +1,8 @@
 #pragma once
 #include "Rectangle.h"
-#include "Text/Label.h"
 #include <SDL.h>
+
+class Label;
 
 class Button : public Rectangle {
 
@@ -9,10 +10,10 @@ public:
     Button();
     ~Button();
     void setText(const std::string& text);
-    std::string getText() const { return label->getText(); }
+    std::string getText() const;
 
     void setLabelColor(const Color& labelColor);
-    const Color& getLabelColor() const { return label->getColor(); }
+    const Color& getLabelColor() const;
 
     void prepareBatch(Batch2D* batch, VertexBuffer* vertexBuffer, IndexBuffer* indexBuffer) override;
 

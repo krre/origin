8abcd2f1e7bebf046ad775dccb03adc6f3a989fb
Vladimir Zarypov <krre31@gmail.com>
2017-07-17 20:28:20 +0300

    Add Node::addChild() method

--- Source/Core/Node.cpp
+++ Source/Core/Node.cpp
@@ -3,3 +3,7 @@
 Node::Node() {
 
 }
+
+void Node::addChild(const std::shared_ptr<Node>& child) {
+    children.push_back(child);
+}
--- Source/Core/Node.h
+++ Source/Core/Node.h
@@ -1,11 +1,13 @@
 #pragma once
 #include "Object.h"
+#include <vector>
 
 class Node : public Object {
 
 public:
     Node();
+    void addChild(const std::shared_ptr<Node>& child);
 
 private:
-
+    std::vector<std::shared_ptr<Node>> children;
 };

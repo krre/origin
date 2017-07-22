042751192aea894e0856623c6266dc744752c581
Vladimir Zarypov <krre31@gmail.com>
2017-07-22 10:53:54 +0300

    Add throw exception to Window::create() method

--- Source/Core/Window.cpp
+++ Source/Core/Window.cpp
@@ -1,5 +1,6 @@
 #include "Window.h"
 #include <SDL_video.h>
+#include <stdexcept>
 
 Window::Window() {
 
@@ -8,6 +9,6 @@ Window::Window() {
 void Window::create() {
     handle = SDL_CreateWindow(title.c_str(), x, y, width, height, SDL_WINDOW_HIDDEN | SDL_WINDOW_RESIZABLE);
     if (handle == nullptr) {
-        // TODO: Throw exception
+        throw std::runtime_error(std::string("Window could not be created\n") + SDL_GetError());
     }
 }

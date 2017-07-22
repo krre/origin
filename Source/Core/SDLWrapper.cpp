56f4bf28f1bba37f2930cdfaa5c228a95e832525
Vladimir Zarypov <krre31@gmail.com>
2017-07-22 13:48:04 +0300

    Add SDLWrapper singleton

---
+++ Source/Core/SDLWrapper.cpp
@@ -0,0 +1,5 @@
+#include "SDLWrapper.h"
+
+SDLWrapper::SDLWrapper() {
+
+}
---
+++ Source/Core/SDLWrapper.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "Singleton.h"
+
+class SDLWrapper : public Singleton<SDLWrapper> {
+
+public:
+    SDLWrapper();
+};

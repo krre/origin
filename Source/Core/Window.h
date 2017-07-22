b8f2152054456a56b41b567f87155947114202b4
Vladimir Zarypov <krre31@gmail.com>
2017-07-22 10:17:10 +0300

    Add getters and setters for x and y property in Window class

--- Source/Core/Window.h
+++ Source/Core/Window.h
@@ -6,12 +6,19 @@ class Window {
 public:
     Window();
 
+    void setX(int x) { this->x = x; }
+    void setY(int y) { this->y = y; }
+    int getX() const { return x; }
+    int getY() const { return y; }
+
     void setWidth(int width);
     void setHeight(int height);
     int getWidth() const;
     int getHeight() const;
 
 private:
+    int x = 0;
+    int y = 0;
     int width = 100;
     int height = 100;
 };

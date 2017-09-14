20404d8a61abb43ad46054a76049ce21fdc2285d
Vladimir Zarypov <krre31@gmail.com>
2017-09-14 16:26:40 +0300

    Add backends storage to RenderManager

--- Source/Graphics/Render/RenderManager.cpp
+++ Source/Graphics/Render/RenderManager.cpp
@@ -1,5 +1,21 @@
 #include "RenderManager.h"
+#include "Origin.h"
 
 RenderManager::RenderManager() {
+    backends.push_back(GraphicsBackend::OPENGL);
+#ifdef VULKAN_BACKEND
+    backends.push_back(GraphicsBackend::VULKAN);
+#endif
+}
 
+GraphicsBackend RenderManager::getCurrentBackend() const {
+    return currentBackend;
+}
+
+void RenderManager::setCurrentBackend(GraphicsBackend backend) {
+    currentBackend = backend;
+}
+
+const std::vector<GraphicsBackend>&RenderManager::getBackends() const {
+    return backends;
 }

8daf0138ff870d766f148db17d7357fa124003c0
Vladimir Zarypov <krre31@gmail.com>
2017-09-14 16:43:54 +0300

    Set current graphics backend from settings

--- Source/Graphics/Render/RenderManager.cpp
+++ Source/Graphics/Render/RenderManager.cpp
@@ -1,11 +1,25 @@
 #include "RenderManager.h"
 #include "Origin.h"
+#include "Core/Settings.h"
+#include <algorithm>
 
 RenderManager::RenderManager() {
     backends.push_back(GraphicsBackend::OPENGL);
 #ifdef VULKAN_BACKEND
     backends.push_back(GraphicsBackend::VULKAN);
 #endif
+
+    std::string backend = Settings::get()->getValue("gfx_backend");
+    if (backend == "opengl") {
+        currentBackend = GraphicsBackend::OPENGL;
+    } else if (backend == "vulkan") {
+        currentBackend = GraphicsBackend::VULKAN;
+    }
+
+    if (std::find(backends.begin(), backends.end(), currentBackend) == backends.end()) {
+        PRINT(std::string("Graphics backend ") + backend + " is not supported. Set to OpenGL");
+        currentBackend = GraphicsBackend::OPENGL;
+    }
 }
 
 GraphicsBackend RenderManager::getCurrentBackend() const {

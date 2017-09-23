fc474bc01504e8a26f3eb679e968faa82212e76a
Vladimir Zarypov <krre31@gmail.com>
2017-09-23 07:37:48 +0300

    Set current graphics backend from command line arguments

--- Source/Core/Context.cpp
+++ Source/Core/Context.cpp
@@ -1,6 +1,8 @@
 #include "Context.h"
 #include "Origin.h"
 #include "Core/Settings.h"
+#include "Core/App.h"
+#include "Core/Utils.h"
 #include <algorithm>
 
 Context::Context() {
@@ -12,15 +14,29 @@ Context::Context() {
     backends.push_back(GraphicsBackend::VULKAN);
 #endif
 
-    std::string backend = Settings::get()->getValue("gfx_backend");
-    if (backend == "opengl") {
-        currentBackend = GraphicsBackend::OPENGL;
-    } else if (backend == "vulkan") {
-        currentBackend = GraphicsBackend::VULKAN;
+    for (const auto& arg : App::get()->getArgv()) {
+        if (arg == "-v") {
+            currentBackend = GraphicsBackend::VULKAN;
+            break;
+        }
+
+        if (arg == "-g") {
+            currentBackend = GraphicsBackend::OPENGL;
+            break;
+        }
+    }
+
+    if (currentBackend == GraphicsBackend::NONE) {
+        std::string backend = Settings::get()->getValue("gfx_backend");
+        if (backend == "opengl") {
+            currentBackend = GraphicsBackend::OPENGL;
+        } else if (backend == "vulkan") {
+            currentBackend = GraphicsBackend::VULKAN;
+        }
     }
 
     if (std::find(backends.begin(), backends.end(), currentBackend) == backends.end()) {
-        throw std::runtime_error(std::string("Graphics backend ") + backend + " is not supported");
+        throw std::runtime_error(std::string("Graphics backend ") + Utils::graphicsBackendToString(currentBackend) + " is not supported");
     }
 }
 

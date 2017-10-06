39847d80848c3000aa5a01bea352a9444729ddc4
Vladimir Zarypov <krre31@gmail.com>
2017-10-06 20:20:33 +0300

    Add OpenGLRenderManager class

---
+++ Source/Graphics/OpenGL/OpenGLRenderManager.cpp
@@ -0,0 +1,5 @@
+#include "OpenGLRenderManager.h"
+
+OpenGLRenderManager::OpenGLRenderManager() {
+
+}
---
+++ Source/Graphics/OpenGL/OpenGLRenderManager.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "Graphics/render/RenderManager.h"
+
+class OpenGLRenderManager : public RenderManager {
+
+public:
+    OpenGLRenderManager();
+};

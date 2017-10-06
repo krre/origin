cfb0b104fdb3656554e8b5d663efe065e0f61e30
Vladimir Zarypov <krre31@gmail.com>
2017-10-06 20:17:29 +0300

    Add VulkanRenderManager class

---
+++ Source/Graphics/Vulkan/VulkanRenderManager.cpp
@@ -0,0 +1,5 @@
+#include "VulkanRenderManager.h"
+
+VulkanRenderManager::VulkanRenderManager() {
+
+}
---
+++ Source/Graphics/Vulkan/VulkanRenderManager.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "Graphics/render/RenderManager.h"
+
+class VulkanRenderManager : public RenderManager {
+
+public:
+    VulkanRenderManager();
+};

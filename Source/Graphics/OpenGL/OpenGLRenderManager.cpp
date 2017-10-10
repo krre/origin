bd9e2b52a4fc047b26522d08ec3dc71b8adaa4e4
Vladimir Zarypov <krre31@gmail.com>
2017-10-10 15:23:52 +0300

    Move GLEW initing to OpenGLRenderManager class

--- Source/Graphics/OpenGL/OpenGLRenderManager.cpp
+++ Source/Graphics/OpenGL/OpenGLRenderManager.cpp
@@ -1,8 +1,16 @@
 #include "OpenGLRenderManager.h"
 #include "OpenGLRenderWindow.h"
+#include <GL/glew.h>
 
 OpenGLRenderManager::OpenGLRenderManager() {
     setName("OpenGLRenderManager");
+    glewExperimental = GL_TRUE;
+    glewInit();
+
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 }
 
 std::shared_ptr<RenderWindow> OpenGLRenderManager::createRenderWindow() {

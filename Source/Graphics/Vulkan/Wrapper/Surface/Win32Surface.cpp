d10230dd3acbaa6b10da38295ef03a3f8f69301e
Vladimir Zarypov <krre31@gmail.com>
2017-10-13 05:54:18 +0300

    Fix creating Vulkan surface on Windows OS

--- Source/Graphics/Vulkan/Wrapper/Surface/Win32Surface.cpp
+++ Source/Graphics/Vulkan/Wrapper/Surface/Win32Surface.cpp
@@ -1,6 +1,5 @@
 #define VK_USE_PLATFORM_WIN32_KHR
 #include "Win32Surface.h"
-#include "../Instance.h"
 
 using namespace Vulkan;
 
@@ -15,5 +14,5 @@ void Vulkan::Win32Surface::platformCreateHandle() {
     createInfo.sType = VK_STRUCTURE_TYPE_WIN32_SURFACE_CREATE_INFO_KHR;
     createInfo.hinstance = hinstance;
     createInfo.hwnd = hwnd;
-    VULKAN_CHECK_RESULT(vkCreateWin32SurfaceKHR(Instance::get()->getHandle(), &createInfo, nullptr, &handle), "Failed to create win32 surface");
+    VULKAN_CHECK_RESULT(vkCreateWin32SurfaceKHR(instance, &createInfo, nullptr, &handle), "Failed to create win32 surface");
 }

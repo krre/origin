7a6bb6857339b81147da486f724f2985e5df78f1
Vladimir Zarypov <krre31@gmail.com>
2017-10-14 06:19:05 +0300

    Rename Handle::isValid() method to getIsValid()

--- Source/Graphics/Vulkan/Wrapper/Base/Handle.h
+++ Source/Graphics/Vulkan/Wrapper/Base/Handle.h
@@ -66,7 +66,7 @@ public:
         assert(handle != VK_NULL_HANDLE);
         isOwner = false;
     }
-    bool isValid() const { return handle != VK_NULL_HANDLE; }
+    bool getIsValid() const { return handle != VK_NULL_HANDLE; }
     bool getIsOwner() const { return isOwner; }
     T getHandle() const { return handle; }
     virtual void create() = 0;

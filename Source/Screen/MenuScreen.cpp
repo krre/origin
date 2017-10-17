61b7ab58b3d848e80f2ab394c87467ab7716815a
Vladimir Zarypov <krre31@gmail.com>
2017-10-17 14:43:53 +0300

    Replace test Label in menu scene on Rectangle

--- Source/Screen/MenuScreen.cpp
+++ Source/Screen/MenuScreen.cpp
@@ -1,12 +1,12 @@
 #include "MenuScreen.h"
 #include "Scene/Scene.h"
 #include "UI/Text/Label.h"
+#include "UI/Rectangle.h"
 
 MenuScreen::MenuScreen() {
     menuScene = std::make_shared<Scene>();
     pushScene(menuScene);
 
-    auto label = std::make_shared<Label>();
-    label->setText("Label");
-    menuScene->setRootControl(label);
+    auto rectangle = std::make_shared<Rectangle>(Size(100, 100));
+    menuScene->setRootControl(rectangle);
 }

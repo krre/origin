b4241e9af05c50989f5125a8d7ae19c313d25899
Vladimir Zarypov <krre31@gmail.com>
2017-12-04 17:03:07 +0200

    DSE: Save and load layers and extensions lists

--- Tools/DebugSettingsEditor/VulkanTab.cpp
+++ Tools/DebugSettingsEditor/VulkanTab.cpp
@@ -3,6 +3,8 @@
 #include "Graphics/Vulkan/Instance.h"
 #include <QJsonObject>
 #include <QJsonValue>
+#include <QJsonArray>
+#include <QDebug>
 
 VulkanTab::VulkanTab() :
         ui(new Ui::VulkanTab) {
@@ -13,6 +15,9 @@ VulkanTab::VulkanTab() :
 
     connect(ui->checkBoxExtensionsUse, &QCheckBox::toggled, this, &VulkanTab::flush);
     connect(ui->checkBoxExtensionsDump, &QCheckBox::toggled, this, &VulkanTab::flush);
+
+    connect(ui->listWidgetLayers, &QListWidget::itemSelectionChanged, this, &VulkanTab::flush);
+    connect(ui->listWidgetExtensions, &QListWidget::itemSelectionChanged, this, &VulkanTab::flush);
 
     Vulkan::Instance instance;
     instance.create();
@@ -33,22 +38,53 @@ VulkanTab::~VulkanTab() {
 void VulkanTab::setDebugSettings(const QJsonObject& settings) {
     ui->checkBoxLayersUse->setChecked(settings["layers"]["use"].toBool());
     ui->checkBoxLayersDump->setChecked(settings["layers"]["dump"].toBool());
+    for (const auto& layer : settings["layers"]["list"].toArray()) {
+        QList<QListWidgetItem*> items = ui->listWidgetLayers->findItems(layer.toString(), Qt::MatchFixedString);
+        if (items.length()) {
+            ui->listWidgetLayers->setCurrentItem(items.at(0), QItemSelectionModel::Select);
+        }
+    }
 
     ui->checkBoxExtensionsUse->setChecked(settings["extensions"]["use"].toBool());
     ui->checkBoxExtensionsDump->setChecked(settings["extensions"]["dump"].toBool());
+    for (const auto& extension : settings["extensions"]["list"].toArray()) {
+        QList<QListWidgetItem*> items = ui->listWidgetExtensions->findItems(extension.toString(), Qt::MatchFixedString);
+        if (items.length()) {
+            ui->listWidgetExtensions->setCurrentItem(items.at(0), QItemSelectionModel::Select);
+        }
+    }
 }
 
 QJsonObject VulkanTab::debugSettings() const {
+    QJsonObject obj;
+
+    // Layers
     QJsonObject layersObj;
     layersObj["use"] = QJsonValue(ui->checkBoxLayersUse->isChecked());
     layersObj["dump"] = QJsonValue(ui->checkBoxLayersDump->isChecked());
+    QJsonArray layersList;
 
+    for (const auto& item : ui->listWidgetLayers->selectedItems()) {
+        layersList.append(item->text());
+    }
+
+    layersObj["list"] = layersList;
+
+    obj["layers"] = layersObj;
+
+    // Extensions
     QJsonObject extensionsObj;
     extensionsObj["use"] = QJsonValue(ui->checkBoxExtensionsUse->isChecked());
     extensionsObj["dump"] = QJsonValue(ui->checkBoxExtensionsDump->isChecked());
 
-    QJsonObject obj;
-    obj["layers"] = layersObj;
+    QJsonArray extensionsList;
+
+    for (const auto& item : ui->listWidgetExtensions->selectedItems()) {
+        extensionsList.append(item->text());
+    }
+
+    extensionsObj["list"] = extensionsList;
+
     obj["extensions"] = extensionsObj;
 
     return obj;

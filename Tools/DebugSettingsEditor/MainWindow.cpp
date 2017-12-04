415e7d5147a48c1252b3aee44bd99f91989c0ae8
Vladimir Zarypov <krre31@gmail.com>
2017-12-04 10:52:03 +0200

    DSE: Read and write settings to file

--- Tools/DebugSettingsEditor/MainWindow.cpp
+++ Tools/DebugSettingsEditor/MainWindow.cpp
@@ -1,14 +1,38 @@
 #include "MainWindow.h"
 #include "ui_MainWindow.h"
+#include "Defines.h"
+#include <QtWidgets>
 
 MainWindow::MainWindow(QWidget *parent) :
         QMainWindow(parent),
     ui(new Ui::MainWindow) {
     ui->setupUi(this);
 
-    resize(800, 600);
+    settingsPath = QApplication::applicationDirPath() + "/" + APP_SETTINGS_NAME;
+    readSettings();
 }
 
 MainWindow::~MainWindow() {
     delete ui;
+}
+
+void MainWindow::closeEvent(QCloseEvent* event) {
+    writeSettings();
+    event->accept();
+}
+
+void MainWindow::readSettings() {
+    QSettings settings(settingsPath, QSettings::IniFormat);
+    settings.beginGroup("MainWindow");
+    resize(settings.value("size", QSize(800, 600)).toSize());
+    move(settings.value("pos", QPoint(200, 200)).toPoint());
+    settings.endGroup();
+}
+
+void MainWindow::writeSettings() {
+    QSettings settings(settingsPath, QSettings::IniFormat);
+    settings.beginGroup("MainWindow");
+    settings.setValue("size", size());
+    settings.setValue("pos", pos());
+    settings.endGroup();
 }
--- Tools/DebugSettingsEditor/MainWindow.h
+++ Tools/DebugSettingsEditor/MainWindow.h
@@ -12,6 +12,13 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+protected:
+    void closeEvent(QCloseEvent* event);
+
 private:
+    void readSettings();
+    void writeSettings();
+
     Ui::MainWindow* ui;
+    QString settingsPath;
 };

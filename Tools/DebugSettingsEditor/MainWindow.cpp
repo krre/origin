07766c0e31be909e9c2f61c0bcf96b9e9a80d999
Vladimir Zarypov <krre31@gmail.com>
2017-12-04 09:08:38 +0200

    DSE: Increase main window's size

--- Tools/DebugSettingsEditor/MainWindow.cpp
+++ Tools/DebugSettingsEditor/MainWindow.cpp
@@ -5,6 +5,8 @@ MainWindow::MainWindow(QWidget *parent) :
         QMainWindow(parent),
     ui(new Ui::MainWindow) {
     ui->setupUi(this);
+
+    resize(800, 600);
 }
 
 MainWindow::~MainWindow() {

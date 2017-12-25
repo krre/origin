67e4f0fefff6dfd1c9829c85c448acba175bc630
Vladimir Zarypov <krre31@gmail.com>
2017-12-25 16:36:54 +0200

    Add data structures to Batch2D

--- Source/UI/Batch2D.h
+++ Source/UI/Batch2D.h
@@ -1,10 +1,23 @@
 #pragma once
-#include "Core/Object.h"
 #include "UITypes.h"
+#include <vector>
+#include <glm/glm.hpp>
 
-class Batch2D : public Object {
+class Batch2D {
 
 public:
+
+    struct Vertex {
+        glm::vec2 pos;
+        glm::vec2 uv;
+        uint16_t color;
+    };
+
     Batch2D();
+    std::vector<Vertex> vertices;
+    std::vector<uint32_t> indices;
     ControlType controlType = ControlType::USUAL;
+
+private:
+
 };

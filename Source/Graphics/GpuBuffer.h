86c068702fd2479289df4cccd35c20978569f4cd
Vladimir Zarypov <krre31@gmail.com>
2018-01-16 13:09:53 +0200

    Add GpuBuffer::getSize() method

--- Source/Graphics/GpuBuffer.h
+++ Source/Graphics/GpuBuffer.h
@@ -17,6 +17,7 @@ public:
     GpuBuffer(Usage usage, uint32_t size);
     ~GpuBuffer();
     Usage getUsage() const { return usage; }
+    uint32_t getSize() const { return size; }
 
     virtual void write(const void* data, uint32_t size, uint32_t offset = 0) = 0;
     virtual void read(void* data, uint32_t size, uint32_t offset = 0) = 0;

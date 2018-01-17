ead213db6e4db61204de627e47c08c7f3328c05d
Vladimir Zarypov <krre31@gmail.com>
2018-01-17 10:46:46 +0200

    Get file pathes for shader name

--- Source/Graphics/ShaderProgram.cpp
+++ Source/Graphics/ShaderProgram.cpp
@@ -1,9 +1,23 @@
 #include "ShaderProgram.h"
+#include "Resource/ResourceManager.h"
+#include "Core/Utils.h"
 
 namespace Origin {
 
 ShaderProgram::ShaderProgram(const std::string& name) {
+    std::string shaderDirPath = ResourceManager::getDataPath() + "/Shader";
 
+    for (auto& filePath : fs::directory_iterator(shaderDirPath)) {
+        std::string fileName = filePath.path().filename().string();
+        std::vector<std::string> words = Utils::split(fileName, '.');
+        if (words.at(0) == name) { // base name
+            if (words.at(1) == "vert") { // first extension
+                files[ShaderType::Vertex].push_back(filePath);
+            } else if (words.at(1) == "frag") {
+                files[ShaderType::Fragment].push_back(filePath);
+            }
+        }
+    }
 }
 
 ShaderProgram::~ShaderProgram() {
--- Source/Graphics/ShaderProgram.h
+++ Source/Graphics/ShaderProgram.h
@@ -1,16 +1,25 @@
 #pragma once
 #include "Core/Common.h"
+#include <experimental/filesystem>
 
 namespace Origin {
+
+namespace fs = std::experimental::filesystem;
 
 class ShaderProgram {
 
 public:
+
+    enum class ShaderType {
+        Vertex,
+        Fragment
+    };
+
     ShaderProgram(const std::string& name);
     ~ShaderProgram();
 
-private:
-
+protected:
+    std::map<ShaderType, std::vector<fs::path>> files;
 };
 
 } // Origin

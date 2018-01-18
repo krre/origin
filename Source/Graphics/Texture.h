381927ee6dfb6552408eb2fb8e4f1f0901e0dd24
Vladimir Zarypov <krre31@gmail.com>
2018-01-18 18:45:19 +0200

    Remove VulkanTexture class

--- Source/Graphics/Texture.h
+++ Source/Graphics/Texture.h
@@ -1,18 +1,31 @@
 #pragma once
 #include "Core/Object.h"
+#include <vector>
+#include <vulkan/vulkan.h>
 
 namespace Origin {
+
+namespace Vulkan {
+    class ImageView;
+    class Image;
+}
 
 class Texture : public Object {
 
 public:
     Texture(const std::string& path);
+    ~Texture();
     uint32_t getWidth() const { return width; }
     uint32_t getHeight() const { return height; }
+    const Vulkan::Image* getImage() const { return image.get(); }
+    const Vulkan::ImageView* getImageView() const { return imageView.get(); }
 
 private:
     uint32_t width = 0;
     uint32_t height = 0;
+    std::vector<unsigned char> data;
+    std::unique_ptr<Vulkan::Image> image;
+    std::unique_ptr<Vulkan::ImageView> imageView;
 };
 
 } // Origin

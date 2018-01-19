ad3866c23ecef2413dc0c68f3fb83d9d7e77adb4
Vladimir Zarypov <krre31@gmail.com>
2018-01-19 17:01:29 +0200

    DSE: Add Debug report groupbox on Vulkan tab

--- Tools/DebugSettingsEditor/VulkanTab.cpp
+++ Tools/DebugSettingsEditor/VulkanTab.cpp
@@ -16,6 +16,13 @@ VulkanTab::VulkanTab() :
         ui(new Ui::VulkanTab) {
     ui->setupUi(this);
 
+    connect(ui->groupBoxDebugReport, &QGroupBox::toggled, this, &VulkanTab::flush);
+    connect(ui->checkBoxInformation, &QCheckBox::toggled, this, &VulkanTab::flush);
+    connect(ui->checkBoxWarning, &QCheckBox::toggled, this, &VulkanTab::flush);
+    connect(ui->checkBoxPerformance, &QCheckBox::toggled, this, &VulkanTab::flush);
+    connect(ui->checkBoxError, &QCheckBox::toggled, this, &VulkanTab::flush);
+    connect(ui->checkBoxDebug, &QCheckBox::toggled, this, &VulkanTab::flush);
+
     connect(ui->groupBoxLayers, &QGroupBox::toggled, this, &VulkanTab::flush);
     connect(ui->groupBoxExtensions, &QGroupBox::toggled, this, &VulkanTab::flush);
 
@@ -46,8 +53,18 @@ VulkanTab::~VulkanTab() {
 }
 
 void VulkanTab::setDebugSettings(const QJsonObject& settings) {
+    // Device
     ui->comboBoxDevice->setCurrentIndex(settings["device"].toInt());
 
+    // Debug report
+    ui->groupBoxDebugReport->setChecked(settings["debugReport"].toObject()["use"].toBool());
+    ui->checkBoxInformation->setChecked(settings["debugReport"].toObject()["information"].toBool());
+    ui->checkBoxWarning->setChecked(settings["debugReport"].toObject()["warning"].toBool());
+    ui->checkBoxPerformance->setChecked(settings["debugReport"].toObject()["performance"].toBool());
+    ui->checkBoxError->setChecked(settings["debugReport"].toObject()["error"].toBool());
+    ui->checkBoxDebug->setChecked(settings["debugReport"].toObject()["debug"].toBool());
+
+    // Layers
     ui->groupBoxLayers->setChecked(settings["layers"].toObject()["use"].toBool());
     for (const auto& layer : settings["layers"].toObject()["list"].toArray()) {
         QList<QListWidgetItem*> items = ui->listWidgetLayers->findItems(layer.toString(), Qt::MatchFixedString);
@@ -56,6 +73,7 @@ void VulkanTab::setDebugSettings(const QJsonObject& settings) {
         }
     }
 
+    // Extensions
     ui->groupBoxExtensions->setChecked(settings["extensions"].toObject()["use"].toBool());
     for (const auto& extension : settings["extensions"].toObject()["list"].toArray()) {
         QList<QListWidgetItem*> items = ui->listWidgetExtensions->findItems(extension.toString(), Qt::MatchFixedString);
@@ -70,6 +88,16 @@ QJsonObject VulkanTab::debugSettings() const {
 
     // Device
     obj["device"] = QJsonValue(ui->comboBoxDevice->currentIndex());
+
+    // Debug report
+    QJsonObject debugReportObj;
+    debugReportObj["use"] = QJsonValue(ui->groupBoxDebugReport->isChecked());
+    debugReportObj["information"] = QJsonValue(ui->checkBoxInformation->isChecked());
+    debugReportObj["warning"] = QJsonValue(ui->checkBoxWarning->isChecked());
+    debugReportObj["performance"] = QJsonValue(ui->checkBoxPerformance->isChecked());
+    debugReportObj["error"] = QJsonValue(ui->checkBoxError->isChecked());
+    debugReportObj["debug"] = QJsonValue(ui->checkBoxDebug->isChecked());
+    obj["debugReport"] = debugReportObj;
 
     // Layers
     QJsonObject layersObj;

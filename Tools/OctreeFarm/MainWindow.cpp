b302ab1e828c4ef4f3030f2413461226fba27301
Vladimir Zarypov <krre31@gmail.com>
2018-01-24 11:00:26 +0200

    OctreeFarm: Add menu actions

--- Tools/OctreeFarm/MainWindow.cpp
+++ Tools/OctreeFarm/MainWindow.cpp
@@ -2,6 +2,7 @@
 #include "ui_MainWindow.h"
 #include "Defines.h"
 #include "Viewport.h"
+#include "Command.h"
 #include <QtWidgets>
 
 namespace OctreeFarm {
@@ -11,7 +12,9 @@ MainWindow::MainWindow(QWidget* parent) :
     ui(new Ui::MainWindow) {
     ui->setupUi(this);
 
-    Viewport* viewport = new Viewport;
+    undoStack = new QUndoStack(this);
+
+    viewport = new Viewport;
     QWidget* container = QWidget::createWindowContainer(viewport);
     QBoxLayout* layout = new QBoxLayout(QBoxLayout::TopToBottom, ui->frameViewport);
     layout->setMargin(0);
@@ -21,6 +24,8 @@ MainWindow::MainWindow(QWidget* parent) :
 
     settings = new QSettings(QCoreApplication::applicationDirPath() + "/" + APP_SETTINGS_NAME, QSettings::IniFormat, this);
     readSettings();
+
+    connect(&octree, &Octree::isModifiedChanged, this, &MainWindow::setWindowModified);
 }
 
 MainWindow::~MainWindow() {
@@ -30,6 +35,97 @@ MainWindow::~MainWindow() {
 void MainWindow::closeEvent(QCloseEvent* event) {
     writeSettings();
     event->accept();
+}
+
+void MainWindow::on_actionNew_triggered() {
+    if (maybeSave()) {
+        octree.createNew();
+        viewport->reset();
+        viewport->updateOctreeInGPU(0, octree.data(), sizeof(uint32_t) * octree.count());
+//        viewport->update();
+        setCurrentFile(QString());
+    }
+}
+
+void MainWindow::on_actionOpen_triggered() {
+    if (maybeSave()) {
+        QString fileName = openFileDialog(QFileDialog::AcceptOpen);
+        if (!fileName.isEmpty()) {
+            loadFile(fileName);
+            viewport->reset();
+            viewport->updateOctreeInGPU(0, octree.data(), sizeof(uint32_t) * octree.count());
+//            viewport->update();
+        }
+    }
+}
+
+void MainWindow::on_actionSave_triggered() {
+    save();
+}
+
+void MainWindow::on_actionSaveAs_triggered() {
+    saveAs();
+}
+
+void MainWindow::on_actionRevert_triggered() {
+    if (!currentFile.isEmpty()) {
+        loadFile(currentFile);
+        viewport->updateOctreeInGPU(0, octree.data(), sizeof(uint32_t) * octree.count());
+//        viewport->update();
+    }
+}
+
+void MainWindow::on_actionExit_triggered() {
+    close();
+}
+
+void MainWindow::on_actionUndo_triggered() {
+    undoStack->undo();
+}
+
+void MainWindow::on_actionRedo_triggered() {
+    undoStack->redo();
+}
+
+void MainWindow::on_actionCopy_triggered() {
+    octree.copy();
+}
+
+void MainWindow::on_actionPaste_triggered() {
+    octree.paste();
+}
+
+void MainWindow::on_actionDeselect_triggered() {
+    octree.deselect();
+}
+
+void MainWindow::on_actionSplit_triggered() {
+    QUndoCommand* splitCommand = new SplitCommand(&octree);
+    undoStack->push(splitCommand);
+}
+
+void MainWindow::on_actionMerge_triggered() {
+    QUndoCommand* addCommand = new AddCommand(&octree, QGuiApplication::keyboardModifiers() == Qt::ShiftModifier);
+    undoStack->push(addCommand);
+}
+
+void MainWindow::on_actionAddForward_triggered() {
+    QUndoCommand* mergeCommand = new MergeCommand(&octree);
+    undoStack->push(mergeCommand);
+}
+
+void MainWindow::on_actionAddBack_triggered() {
+    QUndoCommand* mergeCommand = new MergeCommand(&octree);
+    undoStack->push(mergeCommand);
+}
+
+void MainWindow::on_actionDelete_triggered() {
+    QUndoCommand* deleteCommand = new DeleteCommand(&octree);
+    undoStack->push(deleteCommand);
+}
+
+void MainWindow::on_actionOptions_triggered() {
+
 }
 
 void MainWindow::on_actionAbout_triggered() {
@@ -64,6 +160,121 @@ void MainWindow::writeSettings() {
     settings->setValue("pos", pos());
     settings->setValue("splitter", ui->splitter->saveState());
     settings->endGroup();
+}
+
+bool MainWindow::maybeSave() {
+    if (!octree.getIsModified()) {
+        return true;
+    }
+
+    const QMessageBox::StandardButton ret
+        = QMessageBox::warning(this, QCoreApplication::applicationName(),
+                               tr("The octree has been modified.\n"
+                                  "Do you want to save your changes?"),
+                               QMessageBox::Save | QMessageBox::Discard | QMessageBox::Cancel);
+    switch (ret) {
+    case QMessageBox::Save:
+        return save();
+    case QMessageBox::Cancel:
+        return false;
+    default:
+        break;
+    }
+    return true;
+}
+
+bool MainWindow::save() {
+    if (currentFile.isEmpty()) {
+        return saveAs();
+    } else {
+        return saveFile(currentFile);
+    }
+}
+
+bool MainWindow::saveAs() {
+    QString fileName = openFileDialog(QFileDialog::AcceptSave);
+    if (!fileName.isEmpty()) {
+        return saveFile(fileName);
+    }
+
+    return false;
+}
+
+bool MainWindow::saveFile(const QString& fileName) {
+    QFile file(fileName);
+    if (!file.open(QFile::WriteOnly | QFile::Text)) {
+        QMessageBox::warning(this, QCoreApplication::applicationName(),
+                             tr("Cannot write file %1:\n%2.")
+                             .arg(QDir::toNativeSeparators(fileName),
+                                  file.errorString()));
+        return false;
+    }
+
+
+#ifndef QT_NO_CURSOR
+    QApplication::setOverrideCursor(Qt::WaitCursor);
+#endif
+
+    octree.save(fileName);
+
+#ifndef QT_NO_CURSOR
+    QApplication::restoreOverrideCursor();
+#endif
+
+    setCurrentFile(fileName);
+    return true;
+}
+
+void MainWindow::loadFile(const QString& fileName) {
+    QFile file(fileName);
+    if (!file.open(QFile::ReadOnly | QFile::Text)) {
+        QMessageBox::warning(this, QCoreApplication::applicationName(),
+                             tr("Cannot read file %1:\n%2.")
+                             .arg(QDir::toNativeSeparators(fileName), file.errorString()));
+        return;
+    }
+
+#ifndef QT_NO_CURSOR
+    QApplication::setOverrideCursor(Qt::WaitCursor);
+#endif
+
+    octree.load(fileName);
+
+#ifndef QT_NO_CURSOR
+    QApplication::restoreOverrideCursor();
+#endif
+
+    setCurrentFile(fileName);
+}
+
+QString MainWindow::openFileDialog(QFileDialog::AcceptMode mode) {
+    QFileDialog dialog(this);
+    dialog.setWindowModality(Qt::WindowModal);
+    dialog.setAcceptMode(mode);
+    dialog.setNameFilter(tr("JSON Octrees (*.json)"));
+    if (dialog.exec() != QDialog::Accepted) {
+        return QString();
+    }
+    QString fileName = dialog.selectedFiles().first();
+    QStringList list = fileName.split(".");
+    if (list.length() > 0 && list.at(list.length() - 1) != "json") {
+        fileName += ".json";
+    }
+
+    return fileName;
+}
+
+void MainWindow::setCurrentFile(const QString& fileName) {
+    currentFile = fileName;
+    octree.setIsModified(false);
+    setWindowModified(false);
+
+    QString shownName = QFileInfo(currentFile).fileName();
+    if (currentFile.isEmpty()) {
+        shownName = "untitled.json";
+    }
+    setWindowFilePath(shownName);
+    setWindowTitle(shownName + "[*] - " + QCoreApplication::applicationName());
 }
 
 } // OctreeFarm
--- Tools/OctreeFarm/MainWindow.h
+++ Tools/OctreeFarm/MainWindow.h
@@ -1,12 +1,18 @@
 #pragma once
+#include "Octree.h"
 #include <QMainWindow>
 #include <QSettings>
+#include <QFileDialog>
 
 namespace Ui {
     class MainWindow;
 }
 
+class QUndoStack;
+
 namespace OctreeFarm {
+
+class Viewport;
 
 class MainWindow : public QMainWindow {
     Q_OBJECT
@@ -19,14 +25,54 @@ protected:
     void closeEvent(QCloseEvent* event) override;
 
 private slots:
+    // File
+    void on_actionNew_triggered();
+    void on_actionOpen_triggered();
+    void on_actionSave_triggered();
+    void on_actionSaveAs_triggered();
+    void on_actionRevert_triggered();
+    void on_actionExit_triggered();
+
+    // Edit
+    void on_actionUndo_triggered();
+    void on_actionRedo_triggered();
+    void on_actionCopy_triggered();
+    void on_actionPaste_triggered();
+
+    // Node
+    void on_actionDeselect_triggered();
+    void on_actionSplit_triggered();
+    void on_actionMerge_triggered();
+    void on_actionAddForward_triggered();
+    void on_actionAddBack_triggered();
+    void on_actionDelete_triggered();
+
+    // Tools
+    void on_actionOptions_triggered();
+
+    // Help
     void on_actionAbout_triggered();
 
 private:
     void readSettings();
     void writeSettings();
 
+    bool save();
+    bool saveAs();
+
+    bool maybeSave();
+    bool saveFile(const QString& fileName);
+    void loadFile(const QString& fileName);
+    QString openFileDialog(QFileDialog::QFileDialog::AcceptMode mode);
+    void setCurrentFile(const QString& fileName);
+
     Ui::MainWindow* ui;
     QSettings* settings;
+    Viewport* viewport;
+    Octree octree;
+    QString currentFile;
+    QUndoStack* undoStack;
+
 };
 
 } // OctreeFarm

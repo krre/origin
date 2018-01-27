74c4077ec7b06763b1a466f21d9b83353bdc7801
Vladimir Zarypov <krre31@gmail.com>
2018-01-27 15:42:55 +0200

    Add vector of Batch2D values to UIRenderer

--- Source/UI/UIRenderer.cpp
+++ Source/UI/UIRenderer.cpp
@@ -1,13 +1,19 @@
 #include "UIRenderer.h"
+#include "VulkanRenderer/GpuBuffer.h"
 
 namespace Origin {
 
 UIRenderer::UIRenderer() {
-
+    uint32_t startSize = 10000; // TODO: Set optimal value or take from constant
+    vertexBuffer = std::make_unique<Vulkan::GpuBuffer>(VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, startSize);
 }
 
 UIRenderer::~UIRenderer() {
 
+}
+
+void UIRenderer::addBatch(Batch2D batch) {
+    batches.push_back(batch);
 }
 
 } // Origin
--- Source/UI/UIRenderer.h
+++ Source/UI/UIRenderer.h
@@ -1,5 +1,10 @@
 #pragma once
 #include "Core/Common.h"
+#include "Batch2D.h"
+
+namespace Vulkan {
+    class GpuBuffer;
+}
 
 namespace Origin {
 
@@ -8,9 +13,12 @@ class UIRenderer {
 public:
     UIRenderer();
     ~UIRenderer();
+    void addBatch(Batch2D batch);
 
 private:
-
+    std::vector<Batch2D> batches;
+    std::vector<Batch2D::Vertex> vertices;
+    std::unique_ptr<Vulkan::GpuBuffer> vertexBuffer;
 };
 
 } // Origin

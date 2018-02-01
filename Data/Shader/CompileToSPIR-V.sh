35e485d6d701695dbfb505c84459761f1a03d7b0
Vladimir Zarypov <krre31@gmail.com>
2018-02-01 13:23:04 +0200

    Rename Base shader files to BaseShape

--- Data/Shader/CompileToSPIR-V.sh
+++ Data/Shader/CompileToSPIR-V.sh
@@ -1,6 +1,6 @@
 validator="$VULKAN_SDK/bin/glslangValidator -V -t"
-$validator Base.vert -o Base.vert.spv
-$validator Base.frag -o Base.frag.spv
+$validator BaseShape.vert -o BaseShape.vert.spv
+$validator BaseShape.frag -o BaseShape.frag.spv
 $validator Voxel.vert -o Voxel.vert.spv
 $validator Voxel.frag -o Voxel.frag.spv
 $validator SDF.vert -o SDF.vert.spv

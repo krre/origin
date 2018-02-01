5c86eee1d7dfa14b0455372edfc91712cf1c4fff
Vladimir Zarypov <krre31@gmail.com>
2018-02-01 16:03:01 +0200

    VulkanRenderer: Use new Shader class for parse spv file

--- Libs/VulkanRenderer/Shader.cpp
+++ Libs/VulkanRenderer/Shader.cpp
@@ -1,6 +1,6 @@
 #include "Shader.h"
 #include <fstream>
-#include <spirv_cross/spirv_cross.hpp>
+#include <iostream>
 
 namespace Vulkan {
 
@@ -27,18 +27,40 @@ void Shader::load(const std::string& filePath) {
     file.close();
 
     if (!code.empty()) {
-        analyze();
+        parse();
     }
 }
 
-void Shader::analyze() {
+void Shader::parse() {
     spirv_cross::Compiler compiler(code);
-    spirv_cross::ShaderResources resources = compiler.get_shader_resources();
-    for (auto &u : resources.uniform_buffers) {
-        uint32_t set = compiler.get_decoration(u.id, spv::DecorationDescriptorSet);
-        uint32_t binding = compiler.get_decoration(u.id, spv::DecorationBinding);
-        printf("Found UBO %s at set = %u, binding = %u!\n", u.name.c_str(), set, binding);
-    }
+
+    // Stage flag
+    spirv_cross::SPIREntryPoint entryPoint = compiler.get_entry_point("main");
+    stage = getStage(entryPoint.model);
+//    std::cout << stage << std::endl;
+
+//    spirv_cross::ShaderResources resources = compiler.get_shader_resources();
+//    for (auto &u : resources.uniform_buffers) {
+//        uint32_t set = compiler.get_decoration(u.id, spv::DecorationDescriptorSet);
+//        uint32_t binding = compiler.get_decoration(u.id, spv::DecorationBinding);
+//        printf("Found UBO %s at set = %u, binding = %u!\n", u.name.c_str(), set, binding);
+//    }
+
+
 }
 
-} // Origin
+VkShaderStageFlagBits Shader::getStage(spv::ExecutionModel model) {
+    switch (model) {
+        case spv::ExecutionModel::ExecutionModelVertex: return VK_SHADER_STAGE_VERTEX_BIT;
+        case spv::ExecutionModel::ExecutionModelTessellationControl: return VK_SHADER_STAGE_TESSELLATION_CONTROL_BIT;
+        case spv::ExecutionModel::ExecutionModelTessellationEvaluation: return VK_SHADER_STAGE_TESSELLATION_EVALUATION_BIT;
+        case spv::ExecutionModel::ExecutionModelGeometry: return VK_SHADER_STAGE_GEOMETRY_BIT;
+        case spv::ExecutionModel::ExecutionModelFragment: return VK_SHADER_STAGE_FRAGMENT_BIT;
+        case spv::ExecutionModel::ExecutionModelGLCompute: return VK_SHADER_STAGE_COMPUTE_BIT;
+        default: break;
+    }
+
+    return VK_SHADER_STAGE_FLAG_BITS_MAX_ENUM;
+}
+
+} // Vulkan
--- Libs/VulkanRenderer/Shader.h
+++ Libs/VulkanRenderer/Shader.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <vector>
 #include <string>
+#include <vulkan/vulkan.h>
+#include <spirv_cross/spirv_cross.hpp>
 
 namespace Vulkan {
 
@@ -10,11 +12,14 @@ public:
     Shader();
     ~Shader();
     void load(const std::string& filePath);
+    const std::vector<uint32_t>& getCode() const { return code; }
 
 private:
-    void analyze();
+    void parse();
+    VkShaderStageFlagBits getStage(spv::ExecutionModel model);
 
     std::vector<uint32_t> code;
+    VkShaderStageFlagBits stage;
 };
 
 } // Vulkan

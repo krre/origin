edaef563b1b02d7e54190d57d88dd39ffb1ff305
Vladimir Zarypov <krre31@gmail.com>
2018-02-04 14:41:47 +0200

    Add second test rectangle into menu screen

--- Source/Screen/MenuScreen.cpp
+++ Source/Screen/MenuScreen.cpp
@@ -7,7 +7,11 @@ namespace Origin {
 MenuScreen::MenuScreen() {
     Rectangle* rectangle = new Rectangle(Size(500, 500), this);
     rectangle->setPosition(Pos2(50, 50));
-    rectangle->setColor(Color(0,0, 1.0, 0.0));
+    rectangle->setColor(Color(0.0, 1.0, 0.0));
+
+    Rectangle* rectangle2 = new Rectangle(Size(200, 200), this);
+    rectangle2->setPosition(Pos2(100, 100));
+    rectangle2->setColor(Color(1.0, 0.0, 0.0));
 }
 
 } // Origin

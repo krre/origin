c5884e8902db46af9c791fed943df2d42a05e60b
Vladimir Zarypov <krre31@gmail.com>
2018-02-09 16:06:16 +0200

    Add new buttons into PauseDialog

--- Source/UI/Dialog/PauseDialog.cpp
+++ Source/UI/Dialog/PauseDialog.cpp
@@ -11,23 +11,41 @@ const int BUTTON_WINDTH = 200;
 const int BUTTON_HEIGHT = 40;
 
 PauseDialog::PauseDialog() {
-
+    LinearLayout* layout = new LinearLayout(LinearLayout::Direction::Vertical, this);
 
     Button* buttonContinue = new Button("Continue");
     buttonContinue->resize(BUTTON_WINDTH, BUTTON_HEIGHT);
     buttonContinue->clicked.connect([&]() {
         this->close();
     });
+    layout->addControl(buttonContinue);
+
+    Button* buttonSave = new Button("Save game");
+    buttonSave->resize(BUTTON_WINDTH, BUTTON_HEIGHT);
+    buttonSave->clicked.connect([&]() {
+        PRINT("Save game")
+    });
+    layout->addControl(buttonSave);
+
+    Button* buttonLoad = new Button("Load game");
+    buttonLoad->resize(BUTTON_WINDTH, BUTTON_HEIGHT);
+    buttonLoad->clicked.connect([&]() {
+        PRINT("Load game")
+    });
+    layout->addControl(buttonLoad);
+
+    Button* buttonSettings = new Button("Settings");
+    buttonSettings->resize(BUTTON_WINDTH, BUTTON_HEIGHT);
+    buttonSettings->clicked.connect([&]() {
+        PRINT("Settings")
+    });
+    layout->addControl(buttonSettings);
 
     Button* buttonExit = new Button("Exit");
     buttonExit->resize(BUTTON_WINDTH, BUTTON_HEIGHT);
     buttonExit->clicked.connect([&]() {
         Game::quit();
     });
-
-
-    LinearLayout* layout = new LinearLayout(LinearLayout::Direction::Vertical, this);
-    layout->addControl(buttonContinue);
     layout->addControl(buttonExit);
 
     resizeToContent();

75728df4462ab823456d741339f1b7283ae07ade
Vladimir Zarypov <krre31@gmail.com>
2018-02-11 12:02:35 +0200

    Add ListBox class

---
+++ Source/UI/ListBox.cpp
@@ -0,0 +1,13 @@
+#include "ListBox.h"
+
+namespace Origin {
+
+ListBox::ListBox(Control* parent) : Control(parent) {
+
+}
+
+ListBox::~ListBox() {
+
+}
+
+} // Origin
---
+++ Source/UI/ListBox.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "UI/Control.h"
+
+namespace Origin {
+
+class ListBox : public Control {
+
+public:
+    ListBox(Control* parent = nullptr);
+    ~ListBox();
+
+private:
+
+};
+
+} // Origin

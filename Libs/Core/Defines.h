9fa823d3ccf430aafe45c229a8249f04f394ea61
Vladimir Zarypov <krre31@gmail.com>
2018-02-24 19:20:15 +0200

    Core: Add DEFINE_ENUM_CLASS_BITWISE_OPERATOR(EnumClass) macros

--- Libs/Core/Defines.h
+++ Libs/Core/Defines.h
@@ -10,3 +10,14 @@
 #endif
 
 #define UNUSED(x) (void)x;
+
+#define DEFINE_ENUM_CLASS_BITWISE_OPERATOR(EnumClass) \
+using T = std::underlying_type_t <EnumClass>; \
+inline EnumClass operator | (EnumClass lhs, EnumClass rhs) { \
+    return (EnumClass)(static_cast<T>(lhs) | static_cast<T>(rhs)); \
+} \
+\
+inline EnumClass& operator |= (EnumClass& lhs, EnumClass rhs) { \
+    lhs = (EnumClass)(static_cast<T>(lhs) | static_cast<T>(rhs)); \
+    return lhs; \
+}

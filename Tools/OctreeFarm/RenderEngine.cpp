18e2696f2f821f0f5edebddfb22b57633299d103
Vladimir Zarypov <krre31@gmail.com>
2018-02-24 08:31:19 +0200

    OctreeFarm: Render engine ready to render mesh

--- Tools/OctreeFarm/RenderEngine.cpp
+++ Tools/OctreeFarm/RenderEngine.cpp
@@ -3,6 +3,12 @@
 #include "Vulkan/API/Framebuffer.h"
 #include "Vulkan/API/RenderPass.h"
 #include "Vulkan/API/Surface/Surface.h"
+#include "Vulkan/ShaderProgram.h"
+#include "Vulkan/GpuBuffer.h"
+#include "Vulkan/API/Pipeline/GraphicsPipeline.h"
+#include "Vulkan/API/Pipeline/PipelineLayout.h"
+#include "Vulkan/API/Descriptor/DescriptorSets.h"
+#include <QApplication>
 
 namespace OctreeFarm {
 
@@ -17,7 +23,60 @@ RenderEngine::~RenderEngine() {
 }
 
 void RenderEngine::init() {
+    uint32_t startSize = 1000000; // TODO: Set optimal value or take from constant
+    vertexBuffer.reset(new Vulkan::GpuBuffer(getGraphicsDevice(), VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, startSize));
 
+    uboBuffer.reset(new Vulkan::GpuBuffer(getGraphicsDevice(), VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT, sizeof(glm::mat4)));
+
+    shaderProgram.reset(new Vulkan::ShaderProgram(getGraphicsDevice()));
+    std::string shaderDataPath = QApplication::applicationDirPath().toStdString() + "/Data/Shader";
+    shaderProgram->loadShader(shaderDataPath + "/Voxel.vert.spv");
+    shaderProgram->loadShader(shaderDataPath + "/Voxel.frag.spv");
+
+    VkDescriptorBufferInfo bufferInfo = {};
+    bufferInfo.buffer = uboBuffer->getHandle();
+    bufferInfo.range = VK_WHOLE_SIZE;
+    shaderProgram->bindBuffer("ubo", bufferInfo);
+
+    shaderProgram->create();
+
+    graphicsPipeline.reset(new Vulkan::GraphicsPipeline(getGraphicsDevice()));
+    graphicsPipeline->setRenderPass(getRenderPass()->getHandle());
+    graphicsPipeline->setPipelineLayout(shaderProgram->getPipelineLayout()->getHandle());
+
+    graphicsPipeline->addDynamicState(VK_DYNAMIC_STATE_VIEWPORT);
+    graphicsPipeline->addDynamicState(VK_DYNAMIC_STATE_SCISSOR);
+
+    for (auto& shader : shaderProgram->getShaders()) {
+        graphicsPipeline->addShaderCode(shader->getStage(), shader->getCode().size() * sizeof(uint32_t), shader->getCode().data(), "main");
+    }
+
+    VkVertexInputBindingDescription bindingDescription;
+    bindingDescription.binding = 0;
+    bindingDescription.stride = sizeof(Vertex);
+    bindingDescription.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
+    graphicsPipeline->addVertexBindingDescription(bindingDescription);
+
+    {
+        const Vulkan::Shader::LocationInfo locationInfo = shaderProgram->getLocationInfo("position");
+        VkVertexInputAttributeDescription attributeDescription = {};
+        attributeDescription.binding = bindingDescription.binding;
+        attributeDescription.location = locationInfo.location;
+        attributeDescription.format = locationInfo.format;
+        graphicsPipeline->addVertexAttributeDescription(attributeDescription);
+    }
+
+    {
+        const Vulkan::Shader::LocationInfo locationInfo = shaderProgram->getLocationInfo("color");
+        VkVertexInputAttributeDescription attributeDescription = {};
+        attributeDescription.binding = bindingDescription.binding;
+        attributeDescription.location = locationInfo.location;
+        attributeDescription.format = locationInfo.format;
+        attributeDescription.offset = sizeof(Vertex::pos);
+        graphicsPipeline->addVertexAttributeDescription(attributeDescription);
+    }
+
+    graphicsPipeline->create();
 }
 
 void RenderEngine::writeCommandBuffers(Vulkan::CommandBuffer* commandBuffer, Vulkan::Framebuffer* framebuffer) {
@@ -39,23 +98,25 @@ void RenderEngine::writeCommandBuffers(Vulkan::CommandBuffer* commandBuffer, Vul
 
     commandBuffer->beginRenderPass(renderPassBegin.getInfo());
 
-    //    commandBuffer->bindPipeline(shaderProgram.getGraphicsPipeline());
+    if (graphicsPipeline && vertextCount) {
+        commandBuffer->bindPipeline(graphicsPipeline.data());
 
-    commandBuffer->addViewport(viewport);
-    commandBuffer->setViewport(0);
+        commandBuffer->addViewport(viewport);
+        commandBuffer->setViewport(0);
 
-    commandBuffer->addScissor(scissor);
-    commandBuffer->setScissor(0);
+        commandBuffer->addScissor(scissor);
+        commandBuffer->setScissor(0);
 
-    //    commandBuffer->addVertexBuffer(vertexBuffer->getHandle());
-    //    commandBuffer->bindVertexBuffers();
-    //    commandBuffer->bindIndexBuffer(indexBuffer->getHandle(), indexBuffer->getIndexType());
+        commandBuffer->addVertexBuffer(vertexBuffer->getHandle());
+        commandBuffer->bindVertexBuffers();
 
-    //    for (int i = 0; i < shaderProgram.getDescriptorSets()->getCount(); i++) {
-    //        commandBuffer->addDescriptorSet(shaderProgram.getDescriptorSets()->at(i));
-    //    }
-    //    commandBuffer->bindDescriptorSets(shaderProgram.getGraphicsPipeline()->getBindPoint(), shaderProgram.getPipelineLayout()->getHandle());
-    //    commandBuffer->drawIndexed(MAX_CHAR_COUNT, 1, 0, 0, 0);
+        for (size_t i = 0; i < shaderProgram->getDescriptorSets()->getCount(); i++) {
+            commandBuffer->addDescriptorSet(shaderProgram->getDescriptorSets()->at(i));
+        }
+        commandBuffer->bindDescriptorSets(graphicsPipeline->getBindPoint(), shaderProgram->getPipelineLayout()->getHandle());
+
+        commandBuffer->draw(vertextCount, 1, 0, 0);
+    }
 
     commandBuffer->endRenderPass();
 }
--- Tools/OctreeFarm/RenderEngine.h
+++ Tools/OctreeFarm/RenderEngine.h
@@ -1,6 +1,14 @@
 #pragma once
 #include "Vulkan/Renderer.h"
 #include <QObject>
+#include <QScopedPointer>
+#include <glm/glm.hpp>
+
+namespace Vulkan {
+    class GpuBuffer;
+    class ShaderProgram;
+    class GraphicsPipeline;
+}
 
 namespace OctreeFarm {
 
@@ -8,12 +16,24 @@ class RenderEngine : public QObject, public Vulkan::Renderer {
     Q_OBJECT
 
 public:
+
+    struct Vertex {
+        glm::vec4 pos;
+        glm::vec4 color;
+    };
+
     RenderEngine(void* platformHandle, void* platformWindow, QObject* parent = nullptr);
     virtual ~RenderEngine();
 
 private:
     void init() override;
     void writeCommandBuffers(Vulkan::CommandBuffer* commandBuffer, Vulkan::Framebuffer* framebuffer) override;
+
+    QScopedPointer<Vulkan::GpuBuffer> vertexBuffer;
+    QScopedPointer<Vulkan::GpuBuffer> uboBuffer;
+    QScopedPointer<Vulkan::ShaderProgram> shaderProgram;
+    QScopedPointer<Vulkan::GraphicsPipeline> graphicsPipeline;
+    uint32_t vertextCount = 0;
 };
 
 } // OctreeFarm

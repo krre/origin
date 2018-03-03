370e04b590c9164d0782b7c4783475c7f5f1caca
Vladimir Zarypov <krre31@gmail.com>
2018-03-03 12:04:44 +0200

    Add winit 0.11.1 to Cargo dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+extern crate winit;
+
 fn main() {
-    println!("Hello, world!");
+    let mut events_loop = winit::EventsLoop::new();
+    let _window = winit::Window::new(&events_loop).unwrap();
+
+    events_loop.run_forever(|event| {
+        match event {
+            winit::Event::WindowEvent { event: winit::WindowEvent::Closed, .. } => {
+                winit::ControlFlow::Break
+            },
+            _ => winit::ControlFlow::Continue,
+        }
+    });
 }

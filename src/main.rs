dcbd53ba85dd89165ea971db62f043716d986572
Vladimir Zarypov <krre31@gmail.com>
2018-03-03 20:59:42 +0200

    Restore window postion and size from settings

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ extern crate serde_json;
 
 use std::fs::File;
 use std::io::Write;
+use std::io::Read;
+use std::io::BufReader;
 use std::env;
 
 fn main() {
@@ -19,8 +21,25 @@ fn main() {
     let (monitor_width, monitor_height) = events_loop.get_primary_monitor().get_dimensions();
     let (window_width, window_height) = window.get_outer_size().unwrap();
 
-    let x = (monitor_width - window_width) / 2;
-    let y = (monitor_height - window_height) / 2;
+    let mut x: i64 = (monitor_width - window_width) as i64 / 2;
+    let mut y: i64 = (monitor_height - window_height) as i64 / 2;
+
+    // Read settings
+    match File::open(&settings_path) {
+        Ok(file) => {
+            let mut buf = BufReader::new(file);
+            let mut data = String::new();
+            buf.read_to_string(&mut data).unwrap();
+
+            let settings: serde_json::Value = serde_json::from_str(data.as_str()).unwrap();
+            x = settings["window"]["x"].as_i64().unwrap();
+            y = settings["window"]["y"].as_i64().unwrap();
+            let width = settings["window"]["width"].as_i64().unwrap();
+            let height = settings["window"]["height"].as_i64().unwrap();
+            window.set_inner_size(width as u32, height as u32);
+        },
+        Err(_) => {}
+    };
 
     window.set_position(x as i32, y as i32);
 

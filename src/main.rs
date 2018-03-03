73bf3e26832e1373ba4be521f3aa74e283b77eca
Vladimir Zarypov <krre31@gmail.com>
2018-03-03 13:56:15 +0200

    Move window on center screen after creation

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,14 @@ fn main() {
     let window = winit::Window::new(&events_loop).unwrap();
     window.set_title("Origin");
 
+    let (monitor_width, monitor_height) = events_loop.get_primary_monitor().get_dimensions();
+    let (window_width, window_height) = window.get_outer_size().unwrap();
+
+    let x = (monitor_width - window_width) / 2;
+    let y = (monitor_height - window_height) / 2;
+
+    window.set_position(x as i32, y as i32);
+
     events_loop.run_forever(|event| {
         match event {
             winit::Event::WindowEvent { event: winit::WindowEvent::Closed, .. } => {

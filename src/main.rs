837738082f836ab24e62981dbd07cbe394c05a08
Vladimir Zarypov <krre31@gmail.com>
2018-03-04 08:13:01 +0200

    Create window by winit::WindowBuilder

--- src/main.rs
+++ src/main.rs
@@ -10,19 +10,17 @@ use std::io::BufReader;
 use std::env;
 
 fn main() {
+    // Get path to settings file
     let current_exe = env::current_exe().unwrap();
     let mut settings_path = current_exe.parent().unwrap().to_path_buf();
     settings_path.push("origin.ini");
 
+    // Create window
     let mut events_loop = winit::EventsLoop::new();
-    let window = winit::Window::new(&events_loop).unwrap();
-    window.set_title("Origin");
+    let mut builder = winit::WindowBuilder::new().with_title("Origin");
 
-    let (monitor_width, monitor_height) = events_loop.get_primary_monitor().get_dimensions();
-    let (window_width, window_height) = window.get_outer_size().unwrap();
-
-    let mut x: i64 = (monitor_width - window_width) as i64 / 2;
-    let mut y: i64 = (monitor_height - window_height) as i64 / 2;
+    let mut x = i32::max_value();
+    let mut y = i32::max_value();
 
     // Read settings
     match File::open(&settings_path) {
@@ -32,16 +30,27 @@ fn main() {
             buf.read_to_string(&mut data).unwrap();
 
             let settings: serde_json::Value = serde_json::from_str(data.as_str()).unwrap();
-            x = settings["window"]["x"].as_i64().unwrap();
-            y = settings["window"]["y"].as_i64().unwrap();
+            x = settings["window"]["x"].as_i64().unwrap() as i32;
+            y = settings["window"]["y"].as_i64().unwrap() as i32;
             let width = settings["window"]["width"].as_i64().unwrap();
             let height = settings["window"]["height"].as_i64().unwrap();
-            window.set_inner_size(width as u32, height as u32);
+            builder = builder.with_dimensions(width as u32, height as u32);
         },
         Err(_) => {}
     };
 
-    window.set_position(x as i32, y as i32);
+    let window = builder.build(&events_loop).unwrap();
+
+    if x == i32::max_value() || y == i32::max_value() {
+        // Move window on center of screen
+        let (monitor_width, monitor_height) = events_loop.get_primary_monitor().get_dimensions();
+        let (window_width, window_height) = window.get_outer_size().unwrap();
+
+        x = (monitor_width - window_width) as i32 / 2;
+        y = (monitor_height - window_height) as i32 / 2;
+    }
+
+    window.set_position(x, y);
 
     events_loop.run_forever(|event| {
         match event {

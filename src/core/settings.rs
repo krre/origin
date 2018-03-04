348a3b197e946a4f3b6a9f83dbcecea819790e70
Vladimir Zarypov <krre31@gmail.com>
2018-03-04 16:18:50 +0200

    Serialize settings json object by serde_json::to_string_pretty()

--- src/core/settings.rs
+++ src/core/settings.rs
@@ -59,7 +59,7 @@ impl Settings {
     }
 
     pub fn flush(&self) {
-        let _file = File::create(&self.settings_path).unwrap().write_all(self.storage.to_string().as_bytes());
+        let _file = File::create(&self.settings_path).unwrap().write_all( serde_json::to_string_pretty(&self.storage).unwrap().as_bytes());
     }
 }
 

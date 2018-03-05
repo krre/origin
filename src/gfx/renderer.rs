e58143dd69f9421c3a89837186bea50b41f3f622
Vladimir Zarypov <krre31@gmail.com>
2018-03-05 14:35:32 +0200

    Add vulkano-0.7.2 to crate dependencies section

--- src/gfx/renderer.rs
+++ src/gfx/renderer.rs
@@ -1,13 +1,35 @@
-pub struct Renderer {
+use vulkano_win;
+use vulkano::instance::Instance;
 
+use std::sync::Arc;
+
+pub struct Renderer {
+    vulkan_backend: VulkanBackend
+}
+
+struct VulkanBackend {
+    instance: Arc<Instance>
 }
 
 impl Renderer {
     pub fn new() -> Self {
-        Renderer {}
+        let vulkan_backend = VulkanBackend::new();
+
+        Renderer { vulkan_backend }
     }
 
     pub fn render(&self) {
 //        println!("render")
+    }
+}
+
+impl VulkanBackend {
+    pub fn new() -> Self {
+        let instance = {
+            let extensions = vulkano_win::required_extensions();
+            Instance::new(None, &extensions, None).expect("failed to create Vulkan instance")
+        };
+
+        VulkanBackend { instance }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #[macro_use]
 extern crate serde_json;
+#[macro_use]
+extern crate vulkano;
+extern crate vulkano_win;
 
 mod core;
 mod gfx;

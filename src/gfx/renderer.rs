cbcba72362610599023e83e75b4a1b1cbea33f80
Vladimir Zarypov <krre31@gmail.com>
2018-03-06 18:52:06 +0200

    Create Vulkan swapchain

--- src/gfx/renderer.rs
+++ src/gfx/renderer.rs
@@ -5,6 +5,16 @@ use vulkano::instance::PhysicalDevice;
 use vulkano::swapchain::Surface;
 use vulkano::device::DeviceExtensions;
 use vulkano::device::Device;
+use vulkano::swapchain;
+use vulkano::swapchain::PresentMode;
+use vulkano::swapchain::SurfaceTransform;
+use vulkano::swapchain::Swapchain;
+use vulkano::swapchain::AcquireError;
+use vulkano::swapchain::SwapchainCreationError;
+use vulkano::sync::now;
+use vulkano::sync::SharingMode;
+use vulkano::sync::GpuFuture;
+use vulkano::image::ImageUsage;
 
 use std::sync::Arc;
 
@@ -54,6 +64,31 @@ impl VulkanBackend {
 
             Device::new(physical, physical.supported_features(), &device_ext,
                         [(queue, 0.5)].iter().cloned()).expect("Failed to create device")
+        };
+
+        let queue = queues.next().unwrap();
+
+        let (mut swapchain, mut images) = {
+            let caps = surface.capabilities(physical)
+                .expect("Failed to get surface capabilities");
+
+            let alpha = caps.supported_composite_alpha.iter().next().unwrap();
+            let format = caps.supported_formats[0].0;
+
+            let mut dimensions = {
+                let (width, height) = window.get_inner_size().unwrap();
+                [width, height]
+            };
+
+            let usage = ImageUsage {
+                color_attachment: true,
+                .. ImageUsage::none()
+            };
+
+            Swapchain::new(device.clone(), surface.clone(), caps.min_image_count, format,
+                           dimensions, 1, usage, &queue,
+                           SurfaceTransform::Identity, alpha, PresentMode::Fifo, true, None)
+                .expect("Failed to create swapchain")
         };
 
         VulkanBackend {

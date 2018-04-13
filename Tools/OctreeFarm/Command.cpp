3512e86dc9f56e98d963299147b5ece94f24eeca
Vladimir Zarypov <krre31@gmail.com>
2018-04-13 11:09:09 +0300

    OctreeFarm: Rename octree member to octreeEditor in command classes

--- Tools/OctreeFarm/Command.cpp
+++ Tools/OctreeFarm/Command.cpp
@@ -4,119 +4,119 @@
 
 namespace OctreeFarm {
 
-DeleteCommand::DeleteCommand(OctreeEditor* octree) : octree(octree) {
-    nodes = octree->getSelection();
+DeleteCommand::DeleteCommand(OctreeEditor* octreeEditor) : octreeEditor(octreeEditor) {
+    nodes = octreeEditor->getSelection();
 }
 
 void DeleteCommand::undo() {
     bool result = false;
     for (auto node: nodes) {
-        result = octree->getSource()->addNode(*node.data());
+        result = octreeEditor->getSource()->addNode(*node.data());
     }
 
     if (result) {
-        octree->confirmUpdate();
+        octreeEditor->confirmUpdate();
     } else {
         qDebug() << "Failure add node";
     }
 }
 
 void DeleteCommand::redo() {
-    if (octree->getSource()->deleteNode(nodes)) {
-        octree->confirmUpdate();
+    if (octreeEditor->getSource()->deleteNode(nodes)) {
+        octreeEditor->confirmUpdate();
     } else {
         qDebug() << "Failure delete node";
     }
 }
 
-AddCommand::AddCommand(OctreeEditor* octree, bool back) : octree(octree), back(back) {
-    nodes = octree->getSelection();
+AddCommand::AddCommand(OctreeEditor* octreeEditor, bool back) : octreeEditor(octreeEditor), back(back) {
+    nodes = octreeEditor->getSelection();
 }
 
 void AddCommand::undo() {
-    if (octree->getSource()->deleteNode(node)) {
-        octree->confirmUpdate();
+    if (octreeEditor->getSource()->deleteNode(node)) {
+        octreeEditor->confirmUpdate();
     } else {
         qDebug() << "Failure delete node";
     }
 }
 
 void AddCommand::redo() {
-    if (octree->getSource()->addNode(nodes, back, node)) {
-        octree->confirmUpdate();
+    if (octreeEditor->getSource()->addNode(nodes, back, node)) {
+        octreeEditor->confirmUpdate();
     } else {
         qDebug() << "Failure add node";
     }
 }
 
-SplitCommand::SplitCommand(OctreeEditor* octree) : octree(octree) {
-    nodes = octree->getSelection();
+SplitCommand::SplitCommand(OctreeEditor* octreeEditor) : octreeEditor(octreeEditor) {
+    nodes = octreeEditor->getSelection();
 }
 
 void SplitCommand::undo() {
     bool result = false;
     for (auto node: nodes) {
-        result = octree->getSource()->mergeNode(*node.data());
+        result = octreeEditor->getSource()->mergeNode(*node.data());
     }
     if (result) {
-        octree->confirmUpdate();
+        octreeEditor->confirmUpdate();
     } else {
         qDebug() << "Failure merge node";
     }
 }
 
 void SplitCommand::redo() {
-    if (octree->getSource()->splitNode(nodes)) {
-        octree->confirmUpdate();
+    if (octreeEditor->getSource()->splitNode(nodes)) {
+        octreeEditor->confirmUpdate();
     } else {
         qDebug() << "Failure split node";
     }
 }
 
-MergeCommand::MergeCommand(OctreeEditor* octree) : octree(octree) {
-    nodes = octree->getSelection();
+MergeCommand::MergeCommand(OctreeEditor* octreeEditor) : octreeEditor(octreeEditor) {
+    nodes = octreeEditor->getSelection();
 }
 
 void MergeCommand::undo() {
-    octree->getSource()->createChildren(newNode);
+    octreeEditor->getSource()->createChildren(newNode);
     bool result = false;
     for (auto node: mergedNodes) {
-        result = octree->getSource()->addNode(node);
+        result = octreeEditor->getSource()->addNode(node);
     }
     if (result) {
-        octree->confirmUpdate();
+        octreeEditor->confirmUpdate();
     } else {
         qDebug() << "Failure merge node";
     }
 }
 
 void MergeCommand::redo() {
-    if (octree->getSource()->mergeNode(nodes, newNode, mergedNodes)) {
-        octree->confirmUpdate();
+    if (octreeEditor->getSource()->mergeNode(nodes, newNode, mergedNodes)) {
+        octreeEditor->confirmUpdate();
     } else {
         qDebug() << "Failure merge node";
     }
 }
 
-ChangeColorCommand::ChangeColorCommand(OctreeEditor* octree, QColor color) : octree(octree), color(color) {
-    nodes = octree->getSelection();
+ChangeColorCommand::ChangeColorCommand(OctreeEditor* octreeEditor, QColor color) : octreeEditor(octreeEditor), color(color) {
+    nodes = octreeEditor->getSelection();
 }
 
 void ChangeColorCommand::undo() {
     bool result = false;
     for (auto node: nodes) {
-        result = octree->getSource()->changeNodeColor(*node.data());
+        result = octreeEditor->getSource()->changeNodeColor(*node.data());
     }
     if (result) {
-        octree->confirmUpdate();
+        octreeEditor->confirmUpdate();
     } else {
         qDebug() << "Failure change node color";
     }
 }
 
 void ChangeColorCommand::redo() {
-    if (octree->getSource()->changeNodeColor(nodes, color)) {
-        octree->confirmUpdate();
+    if (octreeEditor->getSource()->changeNodeColor(nodes, color)) {
+        octreeEditor->confirmUpdate();
     } else {
         qDebug() << "Failure change node color";
     }
--- Tools/OctreeFarm/Command.h
+++ Tools/OctreeFarm/Command.h
@@ -7,24 +7,24 @@ namespace OctreeFarm {
 class DeleteCommand : public QUndoCommand {
 
 public:
-    explicit DeleteCommand(OctreeEditor* octree);
+    explicit DeleteCommand(OctreeEditor* octreeEditor);
     void undo() override;
     void redo() override;
 
 private:
-    OctreeEditor* octree;
+    OctreeEditor* octreeEditor;
     QVector<QSharedPointer<Node>> nodes;
 };
 
 class AddCommand : public QUndoCommand {
 
 public:
-    explicit AddCommand(OctreeEditor* octree, bool back = false);
+    explicit AddCommand(OctreeEditor* octreeEditor, bool back = false);
     void undo() override;
     void redo() override;
 
 private:
-    OctreeEditor* octree;
+    OctreeEditor* octreeEditor;
     bool back;
     QVector<QSharedPointer<Node>> nodes;
     Node node;
@@ -33,24 +33,24 @@ private:
 class SplitCommand : public QUndoCommand {
 
 public:
-    explicit SplitCommand(OctreeEditor* octree);
+    explicit SplitCommand(OctreeEditor* octreeEditor);
     void undo() override;
     void redo() override;
 
 private:
-    OctreeEditor* octree;
+    OctreeEditor* octreeEditor;
     QVector<QSharedPointer<Node>> nodes;
 };
 
 class MergeCommand : public QUndoCommand {
 
 public:
-    explicit MergeCommand(OctreeEditor* octree);
+    explicit MergeCommand(OctreeEditor* octreeEditor);
     void undo() override;
     void redo() override;
 
 private:
-    OctreeEditor* octree;
+    OctreeEditor* octreeEditor;
     QVector<QSharedPointer<Node>> nodes;
     QVector<Node> mergedNodes;
     Node newNode;
@@ -59,12 +59,12 @@ private:
 class ChangeColorCommand : public QUndoCommand {
 
 public:
-    explicit ChangeColorCommand(OctreeEditor* octree, QColor color);
+    explicit ChangeColorCommand(OctreeEditor* octreeEditor, QColor color);
     void undo() override;
     void redo() override;
 
 private:
-    OctreeEditor* octree;
+    OctreeEditor* octreeEditor;
     QVector<QSharedPointer<Node>> nodes;
     QColor color;
 };

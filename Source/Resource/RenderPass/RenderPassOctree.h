ba02b788ea815279efbbd7c9aca2a3bf6a3c0d3a
Vladimir Zarypov <krre31@gmail.com>
2018-06-22 13:56:43 +0300

    Add RenderPassOctree class

---
+++ Source/Resource/RenderPass/RenderPassOctree.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "RenderPassResource.h"
+#include <glm/glm.hpp>
+
+namespace Vulkan {
+    class GpuBuffer;
+    class ShaderProgram;
+    class GraphicsPipeline;
+}
+
+namespace Origin {
+
+class RenderPassOctree : public RenderPassResource {
+
+public:
+    RenderPassOctree(Vulkan::Device* device, Object* parent = nullptr);
+    ~RenderPassOctree();
+    void write(Vulkan::CommandBuffer* commandBuffer, Vulkan::Framebuffer* framebuffer) override;
+    Vulkan::GpuBuffer* getVertexBuffer() const { return vertexBuffer.get(); }
+    void resizeVertexBuffer(uint32_t size);
+    void setVertexCount(uint32_t vertextCount);
+
+    void updateMvp(const glm::mat4& mvp);
+
+private:
+    std::unique_ptr<Vulkan::GpuBuffer> vertexBuffer;
+    std::unique_ptr<Vulkan::GpuBuffer> uboBuffer;
+    std::unique_ptr<Vulkan::ShaderProgram> shaderProgram;
+    std::unique_ptr<Vulkan::GraphicsPipeline> graphicsPipeline;
+    uint32_t vertextCount = 0;
+};
+
+} // Origin

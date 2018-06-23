2bd14184805968a692ddd585f7c5b092ea05b421
Vladimir Zarypov <krre31@gmail.com>
2018-06-23 06:51:36 +0300

    Add Renderer class

---
+++ Source/Graphics/Render/Renderer.cpp
@@ -0,0 +1,13 @@
+#include "Renderer.h"
+
+namespace Origin {
+
+Renderer::Renderer(Object* parent) : Object(parent) {
+
+}
+
+Renderer::~Renderer() {
+
+}
+
+} // Origin
---
+++ Source/Graphics/Render/Renderer.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "Core/Object.h"
+
+namespace Origin {
+
+class Renderer : public Object {
+
+public:
+    Renderer(Object* parent = nullptr);
+    ~Renderer();
+
+private:
+
+};
+
+} // Origin

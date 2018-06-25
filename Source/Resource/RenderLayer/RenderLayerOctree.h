609f856244feb1c83d9dee1eae0e4e3cf66d22aa
Vladimir Zarypov <krre31@gmail.com>
2018-06-25 08:38:43 +0300

    Move code from RenderPassOctree class to RenderLayerOctree

--- Source/Resource/RenderLayer/RenderLayerOctree.h
+++ Source/Resource/RenderLayer/RenderLayerOctree.h
@@ -1,6 +1,12 @@
 #pragma once
 #include "RenderLayer.h"
 
+namespace Vulkan {
+    class GpuBuffer;
+    class ShaderProgram;
+    class GraphicsPipeline;
+}
+
 namespace Origin {
 
 class RenderLayerOctree : public RenderLayer {
@@ -9,7 +15,14 @@ public:
     RenderLayerOctree(Vulkan::Device* device, Object* parent = nullptr);
     ~RenderLayerOctree();
 
+    void write(Vulkan::CommandBuffer* commandBuffer, Vulkan::Framebuffer* framebuffer) override;
+    Vulkan::GpuBuffer* getVertexBuffer() const { return vertexBuffer.get(); }
+
 private:
+    std::unique_ptr<Vulkan::GpuBuffer> vertexBuffer;
+    std::unique_ptr<Vulkan::ShaderProgram> shaderProgram;
+    std::unique_ptr<Vulkan::GraphicsPipeline> graphicsPipeline;
+    uint32_t vertextCount = 0;
 
 };
 

b5281769593d1a583314f8305c2f07b6cf001e45
Vladimir Zarypov <krre31@gmail.com>
2018-06-25 07:45:12 +0300

    Add RenderLayerOctree class

---
+++ Source/Resource/RenderLayer/RenderLayerOctree.cpp
@@ -0,0 +1,13 @@
+#include "RenderLayerOctree.h"
+
+namespace Origin {
+
+RenderLayerOctree::RenderLayerOctree(Object* parent) : RenderLayer(parent) {
+
+}
+
+RenderLayerOctree::~RenderLayerOctree() {
+
+}
+
+} // Origin
---
+++ Source/Resource/RenderLayer/RenderLayerOctree.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "RenderLayer.h"
+
+namespace Origin {
+
+class RenderLayerOctree : public RenderLayer {
+
+public:
+    RenderLayerOctree(Object* parent = nullptr);
+    ~RenderLayerOctree();
+
+private:
+
+};
+
+} // Origin

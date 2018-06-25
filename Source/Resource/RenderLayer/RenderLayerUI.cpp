defab05bc658a3c5e0b43a9508b9b767bffbfc1b
Vladimir Zarypov <krre31@gmail.com>
2018-06-25 07:47:20 +0300

    Add RenderLayerUI class

---
+++ Source/Resource/RenderLayer/RenderLayerUI.cpp
@@ -0,0 +1,13 @@
+#include "RenderLayerUI.h"
+
+namespace Origin {
+
+RenderLayerUI::RenderLayerUI(Object* parent) : RenderLayer(parent) {
+
+}
+
+RenderLayerUI::~RenderLayerUI() {
+
+}
+
+} // Origin
---
+++ Source/Resource/RenderLayer/RenderLayerUI.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "RenderLayer.h"
+
+namespace Origin {
+
+class RenderLayerUI : public RenderLayer {
+
+public:
+    RenderLayerUI(Object* parent = nullptr);
+    ~RenderLayerUI();
+
+private:
+
+};
+
+} // Origin

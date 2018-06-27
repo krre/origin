e8111af1aaecdf6e28b7cb1d83f0586f68849f79
Vladimir Zarypov <krre31@gmail.com>
2018-06-27 08:31:19 +0300

    Vulkan: Fix debug report format

--- Libs/Vulkan/API/DebugReportCallback.h
+++ Libs/Vulkan/API/DebugReportCallback.h
@@ -11,25 +11,25 @@ static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallbackDefault(VkDebugReportFlagsEXT
                                                            size_t location, int32_t messageCode, const char* pLayerPrefix, const char* pMessage, void* userData) {
     std::string log;
     switch (flags) {
-    case VK_DEBUG_REPORT_INFORMATION_BIT_EXT:
-        log = "INFO";
-        break;
-    case VK_DEBUG_REPORT_WARNING_BIT_EXT:
-        log = "WARN";
-        break;
-    case VK_DEBUG_REPORT_PERFORMANCE_WARNING_BIT_EXT:
-        log = "PERF";
-        break;
-    case VK_DEBUG_REPORT_ERROR_BIT_EXT:
-        log = "ERROR";
-        break;
-    case VK_DEBUG_REPORT_DEBUG_BIT_EXT:
-        log = "DEBUG";
-        break;
+        case VK_DEBUG_REPORT_INFORMATION_BIT_EXT:
+            log = "INFO";
+            break;
+        case VK_DEBUG_REPORT_WARNING_BIT_EXT:
+            log = "WARN";
+            break;
+        case VK_DEBUG_REPORT_PERFORMANCE_WARNING_BIT_EXT:
+            log = "PERF";
+            break;
+        case VK_DEBUG_REPORT_ERROR_BIT_EXT:
+            log = "ERROR";
+            break;
+        case VK_DEBUG_REPORT_DEBUG_BIT_EXT:
+            log = "DEBUG";
+            break;
     }
 
     std::cout << "[" << log << "] " << pLayerPrefix << " | object: " << std::hex << "0x" << object << std::dec << " | type: " << objectType
-              << " | location: " << location << " | code: " << messageCode << std::endl << pMessage << std::endl;
+              << " | location: " << location << " | code: " << messageCode << " | message: " << pMessage << std::endl;
 
     return VK_FALSE;
 }

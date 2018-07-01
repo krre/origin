91795c4d252995ce0ba4c6d1ca328871fee947e7
Vladimir Zarypov <krre31@gmail.com>
2018-07-01 15:35:31 +0300

    Fix code format

--- Source/ECS/Components/TransformComponent.h
+++ Source/ECS/Components/TransformComponent.h
@@ -8,7 +8,7 @@ class TransformComponent : public Component {
 
 public:
     TransformComponent() = default;
-    Type getType( )const override { return Component::Type::Transform; }
+    Type getType() const override { return Component::Type::Transform; }
 
     glm::mat4 objectToWorld = glm::mat4(1.0);
     glm::mat4 worldToObject = glm::mat4(1.0);

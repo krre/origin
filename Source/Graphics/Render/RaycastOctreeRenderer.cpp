c0f516431d62342bc897057d7cfbc638bf287a5b
Vladimir Zarypov <krre31@gmail.com>
2018-07-02 17:31:20 +0300

    Add RaycastOctreeRenderer class

---
+++ Source/Graphics/Render/RaycastOctreeRenderer.cpp
@@ -0,0 +1,17 @@
+#include "RaycastOctreeRenderer.h"
+
+namespace Origin {
+
+RaycastOctreeRenderer::RaycastOctreeRenderer(Object* parent) : OctreeRenderer(parent) {
+
+}
+
+RaycastOctreeRenderer::~RaycastOctreeRenderer() {
+
+}
+
+void RaycastOctreeRenderer::writeCommandBuffer(Vulkan::CommandBuffer* commandBuffer, Vulkan::Framebuffer* framebuffer) {
+
+}
+
+} // Origin
---
+++ Source/Graphics/Render/RaycastOctreeRenderer.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "OctreeRenderer.h"
+
+namespace Origin {
+
+class RaycastOctreeRenderer : public OctreeRenderer {
+
+public:
+    RaycastOctreeRenderer(Object* parent = nullptr);
+    ~RaycastOctreeRenderer();
+
+    void writeCommandBuffer(Vulkan::CommandBuffer* commandBuffer, Vulkan::Framebuffer* framebuffer) override;
+
+private:
+
+};
+
+} // Origin

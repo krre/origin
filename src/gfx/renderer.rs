e42ac3b82fc078799929da8fda9d59c675fdd6a5
Vladimir Zarypov <krre31@gmail.com>
2020-05-23 14:32:03 +0300

    Fix compile warning

--- src/gfx/renderer.rs
+++ src/gfx/renderer.rs
@@ -1,30 +1,30 @@
-use winit;
 use gfx::surface_adapter;
-use vulkano::instance::Instance;
-use vulkano::instance::PhysicalDevice;
-use vulkano::swapchain::Surface;
-use vulkano::device::DeviceExtensions;
+use vulkano::command_buffer::AutoCommandBufferBuilder;
 use vulkano::device::Device;
+use vulkano::device::DeviceExtensions;
 use vulkano::device::Queue;
-use vulkano::swapchain;
-use vulkano::swapchain::PresentMode;
-use vulkano::swapchain::SurfaceTransform;
-use vulkano::swapchain::Swapchain;
-use vulkano::swapchain::AcquireError;
-use vulkano::image::ImageUsage;
-use vulkano::image::swapchain::SwapchainImage;
 use vulkano::framebuffer::Framebuffer;
 use vulkano::framebuffer::FramebufferAbstract;
 use vulkano::framebuffer::RenderPassAbstract;
-use vulkano::sync::GpuFuture;
+use vulkano::image::swapchain::SwapchainImage;
+use vulkano::image::ImageUsage;
+use vulkano::instance::Instance;
+use vulkano::instance::PhysicalDevice;
+use vulkano::swapchain;
+use vulkano::swapchain::AcquireError;
+use vulkano::swapchain::PresentMode;
+use vulkano::swapchain::Surface;
+use vulkano::swapchain::SurfaceTransform;
+use vulkano::swapchain::Swapchain;
 use vulkano::sync::now;
-use vulkano::command_buffer::AutoCommandBufferBuilder;
+use vulkano::sync::GpuFuture;
+use winit;
 
-use std::sync::Arc;
 use std::mem;
+use std::sync::Arc;
 
 pub struct Renderer {
-    vulkan_backend: VulkanBackend
+    vulkan_backend: VulkanBackend,
 }
 
 struct VulkanBackend {
@@ -34,10 +34,10 @@ struct VulkanBackend {
     queue: Arc<Queue>,
     swapchain: Arc<Swapchain>,
     swapchain_images: Vec<Arc<SwapchainImage>>,
-    framebuffers: Option<Vec<Arc<FramebufferAbstract + Send + Sync>>>,
-    render_pass: Arc<RenderPassAbstract + Send + Sync>,
+    framebuffers: Option<Vec<Arc<dyn FramebufferAbstract + Send + Sync>>>,
+    render_pass: Arc<dyn RenderPassAbstract + Send + Sync>,
     recreate_swapchain: bool,
-    previous_frame_end: Box<GpuFuture>
+    previous_frame_end: Box<dyn GpuFuture>,
 }
 
 impl Renderer {
@@ -58,32 +58,39 @@ impl Renderer {
 
         self.vulkan_backend.previous_frame_end.cleanup_finished();
 
-        let (image_num, acquire_future) = match swapchain::acquire_next_image(self.vulkan_backend.swapchain.clone(), None) {
-            Ok(r) => r,
-            Err(AcquireError::OutOfDate) => {
-                self.vulkan_backend.recreate_swapchain = true;
-                return;
-            },
-            Err(err) => panic!("{:?}", err)
-        };
+        let (image_num, acquire_future) =
+            match swapchain::acquire_next_image(self.vulkan_backend.swapchain.clone(), None) {
+                Ok(r) => r,
+                Err(AcquireError::OutOfDate) => {
+                    self.vulkan_backend.recreate_swapchain = true;
+                    return;
+                }
+                Err(err) => panic!("{:?}", err),
+            };
 
-        let command_buffer = AutoCommandBufferBuilder::primary_one_time_submit(self.vulkan_backend.device.clone(), self.vulkan_backend.queue.family()).unwrap()
-            .begin_render_pass(self.vulkan_backend.framebuffers.as_ref().unwrap()[image_num].clone(), false,
-                               vec![[0.0, 0.0, 1.0, 1.0].into()])
-            .unwrap()
+        let command_buffer = AutoCommandBufferBuilder::primary_one_time_submit(
+            self.vulkan_backend.device.clone(),
+            self.vulkan_backend.queue.family(),
+        )
+        .unwrap()
+        .begin_render_pass(
+            self.vulkan_backend.framebuffers.as_ref().unwrap()[image_num].clone(),
+            false,
+            vec![[0.0, 0.0, 1.0, 1.0].into()],
+        )
+        .unwrap()
+        .end_render_pass()
+        .unwrap()
+        .build()
+        .unwrap();
 
-            .end_render_pass()
-            .unwrap()
+        //        let future = self.vulkan_backend.previous_frame_end.join(acquire_future)
+        //            .then_execute(self.vulkan_backend.queue.clone(), command_buffer).unwrap()
+        //            .then_swapchain_present(self.vulkan_backend.queue.clone(), self.vulkan_backend.swapchain.clone(), image_num)
+        //            .then_signal_fence_and_flush().unwrap();
+        //        self.vulkan_backend.previous_frame_end = Box::new(future) as Box<_>;
 
-            .build().unwrap();
-
-//        let future = self.vulkan_backend.previous_frame_end.join(acquire_future)
-//            .then_execute(self.vulkan_backend.queue.clone(), command_buffer).unwrap()
-//            .then_swapchain_present(self.vulkan_backend.queue.clone(), self.vulkan_backend.swapchain.clone(), image_num)
-//            .then_signal_fence_and_flush().unwrap();
-//        self.vulkan_backend.previous_frame_end = Box::new(future) as Box<_>;
-
-//        println!("render")
+        //        println!("render")
     }
 }
 
@@ -94,69 +101,95 @@ impl VulkanBackend {
 
         let surface = surface_adapter::build_surface(&window, instance.clone()).unwrap();
 
-        let physical = PhysicalDevice::enumerate(&instance).next()
+        let physical = PhysicalDevice::enumerate(&instance)
+            .next()
             .expect("No device available");
         // Some little debug infos.
-        println!("Using device: {} (type: {:?})", physical.name(), physical.ty());
+        println!(
+            "Using device: {} (type: {:?})",
+            physical.name(),
+            physical.ty()
+        );
 
-        let queue = physical.queue_families().find(|&q| {
-            q.supports_graphics() && surface.is_supported(q).unwrap_or(false)
-        }).expect("Couldn't find a graphical queue family");
+        let queue = physical
+            .queue_families()
+            .find(|&q| q.supports_graphics() && surface.is_supported(q).unwrap_or(false))
+            .expect("Couldn't find a graphical queue family");
 
         let (device, mut queues) = {
             let device_ext = DeviceExtensions {
                 khr_swapchain: true,
-                .. DeviceExtensions::none()
+                ..DeviceExtensions::none()
             };
 
-            Device::new(physical, physical.supported_features(), &device_ext,
-                        [(queue, 0.5)].iter().cloned()).expect("Failed to create device")
+            Device::new(
+                physical,
+                physical.supported_features(),
+                &device_ext,
+                [(queue, 0.5)].iter().cloned(),
+            )
+            .expect("Failed to create device")
         };
 
         let queue = queues.next().unwrap();
 
         let (swapchain, swapchain_images) = {
-            let caps = surface.capabilities(physical)
+            let caps = surface
+                .capabilities(physical)
                 .expect("Failed to get surface capabilities");
 
             let alpha = caps.supported_composite_alpha.iter().next().unwrap();
             let format = caps.supported_formats[0].0;
 
-            let mut dimensions = {
+            let dimensions = {
                 let (width, height) = window.get_inner_size().unwrap();
                 [width, height]
             };
 
             let usage = ImageUsage {
                 color_attachment: true,
-                .. ImageUsage::none()
+                ..ImageUsage::none()
             };
 
-            Swapchain::new(device.clone(), surface.clone(), caps.min_image_count, format,
-                           dimensions, 1, usage, &queue,
-                           SurfaceTransform::Identity, alpha, PresentMode::Fifo, true, None)
-                .expect("Failed to create swapchain")
-
+            Swapchain::new(
+                device.clone(),
+                surface.clone(),
+                caps.min_image_count,
+                format,
+                dimensions,
+                1,
+                usage,
+                &queue,
+                SurfaceTransform::Identity,
+                alpha,
+                PresentMode::Fifo,
+                true,
+                None,
+            )
+            .expect("Failed to create swapchain")
         };
 
-        let framebuffers: Option<Vec<Arc<FramebufferAbstract + Send + Sync>>> = None;
+        let framebuffers: Option<Vec<Arc<dyn FramebufferAbstract + Send + Sync>>> = None;
 
-        let render_pass = Arc::new(single_pass_renderpass!(device.clone(),
-            attachments: {
-                color: {
-                    load: Clear,
-                    store: Store,
-                    format: swapchain.format(),
-                    samples: 1,
+        let render_pass = Arc::new(
+            single_pass_renderpass!(device.clone(),
+                attachments: {
+                    color: {
+                        load: Clear,
+                        store: Store,
+                        format: swapchain.format(),
+                        samples: 1,
+                    }
+                },
+                pass: {
+                    color: [color],
+                    depth_stencil: {}
                 }
-            },
-            pass: {
-                color: [color],
-                depth_stencil: {}
-            }
-        ).unwrap());
+            )
+            .unwrap(),
+        );
 
-        let previous_frame_end = Box::new(now(device.clone())) as Box<GpuFuture>;
+        let previous_frame_end = Box::new(now(device.clone())) as Box<dyn GpuFuture>;
 
         VulkanBackend {
             instance: instance.clone(),
@@ -168,28 +201,40 @@ impl VulkanBackend {
             framebuffers,
             render_pass,
             recreate_swapchain: true,
-            previous_frame_end
+            previous_frame_end,
         }
     }
 
     fn resize_swapchain(&mut self) {
-        let capabilites = self.surface.capabilities(self.device.physical_device()).unwrap();
+        let capabilites = self
+            .surface
+            .capabilities(self.device.physical_device())
+            .unwrap();
         let dimensions = capabilites.current_extent.unwrap();
         println!("{} {}", dimensions[0], dimensions[1]);
 
         let (new_swapchain, new_images) = match self.swapchain.recreate_with_dimension(dimensions) {
             Ok(r) => r,
-            Err(err) => panic!("{:?}", err)
+            Err(err) => panic!("{:?}", err),
         };
 
         mem::replace(&mut self.swapchain, new_swapchain);
         mem::replace(&mut self.swapchain_images, new_images);
 
-        let new_framebuffers = Some(self.swapchain_images.iter().map(|image| {
-            Arc::new(Framebuffer::start(self.render_pass.clone())
-                .add(image.clone()).unwrap()
-                .build().unwrap()) as Arc<FramebufferAbstract + Send + Sync>
-        }).collect::<Vec<_>>());
+        let new_framebuffers = Some(
+            self.swapchain_images
+                .iter()
+                .map(|image| {
+                    Arc::new(
+                        Framebuffer::start(self.render_pass.clone())
+                            .add(image.clone())
+                            .unwrap()
+                            .build()
+                            .unwrap(),
+                    ) as Arc<dyn FramebufferAbstract + Send + Sync>
+                })
+                .collect::<Vec<_>>(),
+        );
         mem::replace(&mut self.framebuffers, new_framebuffers);
     }
 }

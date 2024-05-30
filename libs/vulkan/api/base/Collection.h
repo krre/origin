671f1e55b9d99a4dd2792a10587ba776b6c44182
Vladimir Zarypov <krre31@gmail.com>
2024-05-30 09:34:00 +0300

    Remove isOwner() method from Collection and Handle classes

--- libs/vulkan/api/base/Collection.h
+++ libs/vulkan/api/base/Collection.h
@@ -15,7 +15,6 @@ template <typename T>
 class Collection {
 public:
     Collection() = default;
-    bool isOwner() const { return owner; }
     size_t getCount() const { return collection.size(); }
     T at(size_t i) const { return collection.at(i); }
     const T* getData() const { return collection.data(); }
@@ -23,7 +22,6 @@ public:
 
 protected:
     std::vector<T> collection;
-    bool owner = true; // Always true for collections
 };
 
 } // Vulkan
--- libs/vulkan/api/base/Handle.h
+++ libs/vulkan/api/base/Handle.h
@@ -27,17 +27,14 @@ public:
     Handle() = default;
     Handle(T handle) : handle(handle) {
         assert(handle != VK_NULL_HANDLE);
-        owner = false;
     }
     bool isValid() const { return handle != VK_NULL_HANDLE; }
-    bool isOwner() const { return owner; }
     T getHandle() const { return handle; }
     virtual void create() = 0;
     virtual void destroy() = 0;
 
 protected:
     T handle = VK_NULL_HANDLE;
-    bool owner = true;
 };
 
 } // Vulkan

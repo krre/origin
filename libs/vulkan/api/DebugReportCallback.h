e2a5c01ab1b991c575d7ca80a2939d5d22e4d736
Vladimir Zarypov <krre31@gmail.com>
2024-07-29 08:04:20 +0300

    Vulkan: Add [[maybe_unused]] attribute to function parameter

--- libs/vulkan/api/DebugReportCallback.h
+++ libs/vulkan/api/DebugReportCallback.h
@@ -7,7 +7,7 @@ namespace Vulkan {
 class Instance;
 
 static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallbackDefault(VkDebugReportFlagsEXT flags, VkDebugReportObjectTypeEXT objectType, uint64_t object,
-                                                           size_t location, int32_t messageCode, const char* pLayerPrefix, const char* pMessage, void* userData) {
+                                                           size_t location, int32_t messageCode, const char* pLayerPrefix, const char* pMessage, void* userData [[maybe_unused]]) {
     std::string log;
     switch (flags) {
         case VK_DEBUG_REPORT_INFORMATION_BIT_EXT: log = "INFO"; break;

9ea548b56669a882ab9e82c9fdb25168fd891373
Vladimir Zarypov <krre31@gmail.com>
2024-08-19 16:21:49 +0300

    Remove gagarin-libgdx project

--- README.md
+++
@@ -1,5 +0,0 @@
-# Gagarin
-Procedural world game
-
-## Dependencies
-LibGDX 1.9.2
--- src/core/src/main/kotlin/event/KeyEvent.kt
+++
@@ -1,7 +0,0 @@
-package org.gagarin.event
-
-interface KeyEvent {
-    fun keyPressed(key: Int): Boolean {
-        return  false
-    }
-}

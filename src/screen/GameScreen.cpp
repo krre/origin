57caf739d5c670ec8d9cda1aaab8801be3363f95
Vladimir Zarypov <krre31@gmail.com>
2025-05-29 09:51:21 +0300

    Use `auto` keyword to remove duplicate code

--- src/screen/GameScreen.cpp
+++ src/screen/GameScreen.cpp
@@ -9,14 +9,14 @@
 GameScreen::GameScreen(const std::string& worldName) {
     World::load(worldName);
 
-    View3D* gameView = new View3D(this);
+    auto gameView = new View3D(this);
     std::shared_ptr<GameScene> gameScene = std::make_shared<GameScene>();
     gameView->setScene(gameScene);
 }
 
 void GameScreen::keyPressed(const SDL_KeyboardEvent& event) {
     if (event.keysym.sym == SDLK_ESCAPE) {
-        PauseDialog* dialog = new PauseDialog;
+        auto dialog = new PauseDialog;
         Overlay::get()->showDialog(dialog);
     }
 }

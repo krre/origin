9ea7682ff4583cbf1fc0d0989b3cdce6371c9462
Vladimir Zarypov <krre31@gmail.com>
2026-09-10 06:31:58 +0300

    AssemblyBox: Wrap `About` message to C++11 raw string

--- tools/assemblybox/MainWindow.cpp
+++ tools/assemblybox/MainWindow.cpp
@@ -18,14 +18,14 @@ void MainWindow::closeEvent(QCloseEvent* event) {
 
 void MainWindow::about() {
     QMessageBox::about(this, tr("About %1").arg(Application::Name),
-        tr("<h3>%1 %2</h3> \
-           Universe editor for Origin game<br><br> \
-           Based on Qt %3<br> \
-           Build on %4 %5<br><br> \
-           <a href=%6>%6</a><br><br>Copyright © %7, %8").
-           arg(Application::Name, Application::Version, QT_VERSION_STR,
-            Application::BuildDate, Application::BuildTime, Application::Url,
-            Application::Years, Application::Author));
+tr(R"(<h3>%1 %2</h3>
+Universe editor for Origin game<br><br>
+Based on Qt %3<br>
+Build on %4 %5<br><br>
+<a href=%6>%6</a><br><br>Copyright © %7, %8)").
+        arg(Application::Name, Application::Version, QT_VERSION_STR,
+        Application::BuildDate, Application::BuildTime, Application::Url,
+        Application::Years, Application::Author));
 }
 
 void MainWindow::createActions() {

2782a58effc9b94e4a9670b6eff0547903cdc4c0
Vladimir Zarypov <krre31@gmail.com>
2026-09-12 13:46:50 +0300

    DSE: Wrap `About` message to C++11 raw string

--- tools/debugsettingseditor/MainWindow.cpp
+++ tools/debugsettingseditor/MainWindow.cpp
@@ -39,14 +39,15 @@ void MainWindow::closeEvent(QCloseEvent* event) {
 
 void MainWindow::about() {
     QMessageBox::about(this, tr("About %1").arg(Application::Title),
-        tr("<h3>%1 %2</h3> \
-           Debug settings editor for Origin game<br><br> \
-           Based on Qt %3<br> \
-           Build on %4 %5<br><br> \
-           <a href=%6>%6</a><br><br>Copyright © %7, %8").
-           arg(Application::Title, Application::Version, QT_VERSION_STR,
-            Application::BuildDate, Application::BuildTime, Application::Url,
-            Application::Years, Application::Author));
+        tr(R"(
+<h3>%1 %2</h3>
+Debug settings editor for Origin game<br><br>
+Based on Qt %3<br>
+Build on %4 %5<br><br>
+<a href=%6>%6</a><br><br>Copyright © %7, %8)").
+        arg(Application::Title, Application::Version, QT_VERSION_STR,
+        Application::BuildDate, Application::BuildTime, Application::Url,
+        Application::Years, Application::Author));
 }
 
 void MainWindow::createActions() {
